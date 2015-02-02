ab7ccc5cf6eee9ea4cae20a2a893ccd4522d4edf
Martin Zurowietz <martin@zurowietz.de>
2015-02-02 15:47:24 +0100

    Implements database migrations. The migrations are now final for the currently planned database schema.

--- app/database/migrations/2015_01_27_154541_initialize.php
+++ app/database/migrations/2015_01_27_154541_initialize.php
@@ -8,29 +8,68 @@ class Initialize extends Migration {
 	/**
 	 * Run the migrations.
 	 *
+	 * MIND THE ORDERING BECAUSE OF FOREIGN KEY RELATIONS
+	 *
 	 * @return void
 	 */
 	public function up()
 	{
+		/*
+		| The user. Each user belongs to one or many projects and has one role
+		| for each project. A user can be creator of different objects
+		| (transects, projects, ...).
+		*/
 		Schema::create('users', function(Blueprint $table) {
 			$table->increments('id');
 			$table->string('firstname', 128);
 			$table->string('lastname', 128);
+			// hashing uses bcrypt so the password is always 60 chars long
 			$table->string('password', 60);
 			$table->string('email', 256);
+			// token for the "stay logged in" session
 			$table->rememberToken();
 			$table->timestamps();
 			$table->timestamp('login_at')->nullable();
 
+			// email is used for login and thus unique
 			$table->unique('email');
 		});
 
+		/*
+		| The roles users can have. For example a user can be 'admin' in one
+		| project and 'guest' un another. Roles are not restricted to projects.
+		*/
 		Schema::create('roles', function(Blueprint $table) {
 			$table->increments('id');
 			$table->string('name', 128);
+
+			// NO timestamps
 		});
 
-		Schema::create('role_user', function(Blueprint $table) {
+		/*
+		| Users and transects are grouped into projects.
+		*/
+		Schema::create('projects', function(Blueprint $table) {
+			$table->increments('id');
+			$table->string('name', 512);
+			$table->text('description');
+
+			// creator of the project
+			$table->integer('user_id')->unsigned()->nullable();
+			$table->foreign('user_id')
+			      ->references('id')
+			      ->on('users')
+			      // if the creator is deleted, the project should still exist
+			      ->onDelete('set null');
+
+			$table->timestamps();
+		});
+
+		/*
+		| Users can participate in multiple projects and have one specific role
+		| in each project.
+		*/
+		Schema::create('project_user', function(Blueprint $table) {
 			$table->integer('role_id')->unsigned();
 			$table->foreign('role_id')
 			      ->references('id')
@@ -44,124 +83,69 @@ class Initialize extends Migration {
 			      ->on('users')
 			      ->onDelete('cascade');
 
-			$table->unique(array('role_id', 'user_id'));
+			$table->integer('project_id')->unsigned();
+			$table->foreign('project_id')
+			      ->references('id')
+			      ->on('projects')
+			      ->onDelete('cascade');
+
+			// users shouldn't have multiple roles in the same project
+			$table->unique(array('role_id', 'user_id', 'project_id'));
 		});
 
+		/*
+		| Media type of a transect. E.g. time-series of a stationary camera or
+		| images from different locations of a moving camera.
+		*/
 		Schema::create('media_types', function(Blueprint $table) {
 			$table->increments('id');
 			$table->string('name', 512);
+
+			// NO timestamps
 		});
 
-		Schema::create('projects', function(Blueprint $table) {
-			$table->increments('id');
-			$table->string('name', 512);
-			$table->text('description');
-
-			// creator
-			$table->integer('user_id')->unsigned()->nullable();
-			$table->foreign('user_id')
-			      ->references('id')
-			      ->on('users')
-			      ->onDelete('set null');
-
-			$table->timestamps();
-		});
-
-		Schema::create('project_admin_user', function(Blueprint $table) {
-			$table->integer('user_id')->unsigned();
-			$table->foreign('user_id')
-			      ->references('id')
-			      ->on('users')
-			      ->onDelete('cascade');
-
-			$table->integer('project_id')->unsigned();
-			$table->foreign('project_id')
-			      ->references('id')
-			      ->on('projects')
-			      ->onDelete('cascade');
-
-			$table->unique(array('user_id', 'project_id'));
-		});
-
-		Schema::create('project_editor_user', function(Blueprint $table) {
-			$table->integer('user_id')->unsigned();
-			$table->foreign('user_id')
-			      ->references('id')
-			      ->on('users')
-			      ->onDelete('cascade');
-
-			$table->integer('project_id')->unsigned();
-			$table->foreign('project_id')
-			      ->references('id')
-			      ->on('projects')
-			      ->onDelete('cascade');
-
-			$table->unique(array('user_id', 'project_id'));
-		});
-
-		Schema::create('project_guest_user', function(Blueprint $table) {
-			$table->integer('user_id')->unsigned();
-			$table->foreign('user_id')
-			      ->references('id')
-			      ->on('users')
-			      ->onDelete('cascade');
-
-			$table->integer('project_id')->unsigned();
-			$table->foreign('project_id')
-			      ->references('id')
-			      ->on('projects')
-			      ->onDelete('cascade');
-
-			$table->unique(array('user_id', 'project_id'));
-		});
-
-		Schema::create('labels', function(Blueprint $table) {
-			$table->increments('id');
-			$table->string('name', 512);
-
-			$table->integer('parent_id')->unsigned()->nullable();
-			$table->foreign('parent_id')
-			      ->references('id')
-			      ->on('labels')
-			      ->onDelete('cascade');
-
-			$table->integer('aphia_id')->nullable();
-
-			// TODO what is this for???
-			$table->integer('project_id')->nullable();
-		});
-
-		Schema::create('attributes', function(Blueprint $table) {
-			$table->increments('id');
-			$table->string('name', 512);
-
-			// TODO what is this for? make a new table attribute_type?
-			$table->integer('type');
-		});
-
+		/*
+		| A transect is a series of images. Each transect belongs to one or more
+		| projects.
+		|
+		| A transect may reference to a remote location where the images are
+		| stored. This may be a remote server or even a directory other than
+		| the default data directory on the same machine (e.g. another volume).
+		|
+		| If the transect references to a remote location, only the image
+		| thumbnails are stored locally; the original images are loaded from 
+		| remote.
+		*/
 		Schema::create('transects', function(Blueprint $table) {
 			$table->increments('id');
 			$table->string('name', 512);
 
+			// which kind of data is stored in this transect?
 			$table->integer('media_type_id')->unsigned();
 			$table->foreign('media_type_id')
 			      ->references('id')
 			      ->on('media_types')
-			      // media types in use shouldnt be deleted
+			      // media types in use shouldn't be deleted
 			      ->onDelete('restrict');
 
+			// the creator of the transect
 			$table->integer('user_id')->unsigned()->nullable();
 			$table->foreign('user_id')
 			      ->references('id')
 			      ->on('users')
+			      // the transect should still exist if the creator was deleted
 			      ->onDelete('set null');
 
 			$table->timestamps();
 
-			$table->string('image_path', 512);
+			// stores external URL (e.g. http: or file:) if the images of the
+			// transect are not stored in the default path
 			$table->string('url', 256)->nullable();
 		});
 
+		/*
+		| Transects belong to one or many projects.
+		*/
 		Schema::create('project_transect', function(Blueprint $table) {
 			$table->integer('transect_id')->unsigned();
 			$table->foreign('transect_id')
@@ -176,11 +160,18 @@ class Initialize extends Migration {
 			      // projects with transects shouldn't be deleted
 			      ->onDelete('restrict');
 
+			// each transect may belont to each project only once
 			$table->unique(array('transect_id', 'project_id'));
 		});
 
+		/*
+		| Transects consist of multiple images. Each image belongs to a single
+		| transect.
+		*/
 		Schema::create('images', function(Blueprint $table) {
 			$table->increments('id');
+			// if the transect references to a remote location, the images may
+			// have non-standard filenames
 			$table->string('filename', 512);
 
 			$table->integer('transect_id')->unsigned();
@@ -190,14 +181,48 @@ class Initialize extends Migration {
 			      // delete all images belonging to a deleted transect
 			      ->onDelete('cascade');
 
-			$table->string('hash', 32)->default('');
+			// NO timestamps (same as transect)
 		});
 
+		/*
+		| An annotation can have multiple labels. Each label is set by a user.
+		| Each label may have a 'parent' so labels can be ordered in a tree-like
+		| structure.
+		*/
+		Schema::create('labels', function(Blueprint $table) {
+			$table->increments('id');
+			$table->string('name', 512);
+
+			// parent label. if the parent is deleted, ask the user if all
+			// children should be deleted as well
+			$table->integer('label_id')->unsigned()->nullable();
+			$table->foreign('label_id')
+			      ->references('id')
+			      ->on('labels')
+			      // ask user if all children should be deleted!
+			      ->onDelete('set null');
+
+			// id for the World Register of Marine Species (WoRMS)
+			$table->integer('aphia_id')->nullable();
+
+			// NO timestamps
+		});
+
+		/*
+		| An annotation can be of a certain shape, e.g. circle, line, polygon.
+		*/
 		Schema::create('annotation_shapes', function(Blueprint $table) {
 			$table->increments('id');
 			$table->string('name', 256);
+
+			// NO timestamps
 		});
 
+		/*
+		| Annotations mark regions or distances on an image. These annotations
+		| then can be labeled with multiple labels describing what the annotation
+		| (probably) shows.
+		*/
 		Schema::create('annotations', function(Blueprint $table) {
 			$table->increments('id');
 
@@ -212,12 +237,16 @@ class Initialize extends Migration {
 			$table->foreign('annotation_shape_id')
 			      ->references('id')
 			      ->on('annotation_shapes')
-			      // don't delete shapes in use
+			      // don't delete shapes that are used
 			      ->onDelete('restrict');
 
 			$table->timestamps();
 		});
 
+		/*
+		| Each annotation consists of one or multiple points (e.g. two points of
+		| a line). Each point belongs to a single annotation.
+		*/
 		Schema::create('annotation_points', function(Blueprint $table) {
 			$table->integer('annotation_id')->unsigned();
 			$table->foreign('annotation_id')
@@ -226,14 +255,20 @@ class Initialize extends Migration {
 			      // delete all points of a deleted annotation
 			      ->onDelete('cascade');
 
+			// for e.g. polygons the ordering of the points is essential, so the
+			// polygon can be correctly reconstructed
 			$table->integer('point_index');
-
 			$table->unique(array('annotation_id', 'point_index'));
 
 			$table->integer('x');
 			$table->integer('y');
 		});
 
+		/*
+		| Each annotation may get labels by the users. Each user may set multiple
+		| labels to the same annotation e.g. with different levels of confidence
+		| ('i'm not sure, it is 90% this type or 10% this').
+		*/
 		Schema::create('annotation_label', function(Blueprint $table) {
 			$table->integer('annotation_id')->unsigned();
 			$table->foreign('annotation_id')
@@ -245,177 +280,43 @@ class Initialize extends Migration {
 			$table->foreign('label_id')
 			      ->references('id')
 			      ->on('labels')
-			      // TODO cascade??
+			      // don't delete labels in use
 			      ->onDelete('restrict');
 
 			$table->integer('user_id')->unsigned()->nullable();
 			$table->foreign('user_id')
 			      ->references('id')
 			      ->on('users')
+			      // don't delete labels if the creator is deleted
 			      ->onDelete('set null');
 
 			$table->double('confidence');
-		});
 
-		//TODO INSERT OTHER TABLES HERE
-
-		Schema::create('attribute_project', function(Blueprint $table) {
-			$table->integer('attribute_id')->unsigned();
-			$table->foreign('attribute_id')
-			      ->references('id')
-			      ->on('attributes')
-			      // TODO restrict?
-			      ->onDelete('cascade');
-
-			$table->integer('project_id')->unsigned();
-			$table->foreign('project_id')
-			      ->references('id')
-			      ->on('projects')
-			      ->onDelete('cascade');
-
-			// TODO unique?
-			$table->unique(array('attribute_id', 'project_id'));
-
-			$table->integer('value_int')->nullable();
-			$table->double('value_double')->nullable();
-			$table->string('value_string', 512)->nullable();
-		});
-
-		Schema::create('attribute_user', function(Blueprint $table) {
-			$table->integer('attribute_id')->unsigned();
-			$table->foreign('attribute_id')
-			      ->references('id')
-			      ->on('attributes')
-			      ->onDelete('cascade');
-
-			$table->integer('user_id')->unsigned();
-			$table->foreign('user_id')
-			      ->references('id')
-			      ->on('users')
-			      ->onDelete('cascade');
-
-			// TODO unique?
-			$table->unique(array('attribute_id', 'user_id'));
-
-			$table->integer('value_int')->nullable();
-			$table->double('value_double')->nullable();
-			$table->string('value_string', 512)->nullable();
-		});
-
-		Schema::create('attribute_transect', function(Blueprint $table) {
-			$table->integer('attribute_id')->unsigned();
-			$table->foreign('attribute_id')
-			      ->references('id')
-			      ->on('attributes')
-			      ->onDelete('cascade');
-
-			$table->integer('transect_id')->unsigned();
-			$table->foreign('transect_id')
-			      ->references('id')
-			      ->on('transects')
-			      ->onDelete('cascade');
-
-			// TODO unique?
-			$table->unique(array('attribute_id', 'transect_id'));
-
-			$table->integer('value_int')->nullable();
-			$table->double('value_double')->nullable();
-			$table->string('value_string', 512)->nullable();
-		});
-
-		Schema::create('attribute_image', function(Blueprint $table) {
-			$table->integer('attribute_id')->unsigned();
-			$table->foreign('attribute_id')
-			      ->references('id')
-			      ->on('attributes')
-			      ->onDelete('cascade');
-
-			$table->integer('image_id')->unsigned();
-			$table->foreign('image_id')
-			      ->references('id')
-			      ->on('images')
-			      ->onDelete('cascade');
-
-			// TODO unique?
-			$table->unique(array('attribute_id', 'image_id'));
-
-			$table->integer('value_int')->nullable();
-			$table->double('value_double')->nullable();
-			$table->string('value_string', 512)->nullable();
-		});
-
-		Schema::create('attribute_label', function(Blueprint $table) {
-			$table->integer('attribute_id')->unsigned();
-			$table->foreign('attribute_id')
-			      ->references('id')
-			      ->on('attributes')
-			      ->onDelete('cascade');
-
-			$table->integer('label_id')->unsigned();
-			$table->foreign('label_id')
-			      ->references('id')
-			      ->on('labels')
-			      ->onDelete('cascade');
-
-			// TODO unique?
-			$table->unique(array('attribute_id', 'label_id'));
-
-			$table->integer('value_int')->nullable();
-			$table->double('value_double')->nullable();
-			$table->string('value_string', 512)->nullable();
-		});
-
-		Schema::create('annotation_attribute', function(Blueprint $table) {
-			$table->integer('attribute_id')->unsigned();
-			$table->foreign('attribute_id')
-			      ->references('id')
-			      ->on('attributes')
-			      ->onDelete('cascade');
-
-			$table->integer('annotation_id')->unsigned();
-			$table->foreign('annotation_id')
-			      ->references('id')
-			      ->on('annotations')
-			      ->onDelete('cascade');
-
-			// TODO unique?
-			$table->unique(array('attribute_id', 'annotation_id'));
-
-			$table->integer('value_int')->nullable();
-			$table->double('value_double')->nullable();
-			$table->string('value_string', 512)->nullable();
+			// each user may set the same label only once for each annotation
+			$table->unique('annotation_id', 'label_id', 'user_id');
 		});
 	}
 
 	/**
 	 * Reverse the migrations.
 	 *
+	 * MIND THE ORDERING BECAUSE OF FOREIGN KEY RELATIONS
+	 *
 	 * @return void
 	 */
 	public function down()
 	{
-		Schema::drop('attribute_project');
-		Schema::drop('attribute_user');
-		Schema::drop('attribute_transect');
-		Schema::drop('attribute_image');
-		Schema::drop('attribute_label');
-		Schema::drop('annotation_attribute');
-
 		Schema::drop('annotation_label');
 		Schema::drop('annotation_points');
 		Schema::drop('annotations');
 		Schema::drop('annotation_shapes');
+		Schema::drop('labels');
 		Schema::drop('images');
 		Schema::drop('project_transect');
 		Schema::drop('transects');
-		Schema::drop('attributes');
-		Schema::drop('labels');
-		Schema::drop('project_admin_user');
-		Schema::drop('project_editor_user');
-		Schema::drop('project_guest_user');
-		Schema::drop('projects');
 		Schema::drop('media_types');
-		Schema::drop('role_user');
+		Schema::drop('project_user');
+		Schema::drop('projects');
 		Schema::drop('roles');
 		Schema::drop('users');
 	}
