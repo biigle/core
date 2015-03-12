278ba474eb82ccfe7db1fe2c7d5e382764cd7276
Martin Zurowietz <martin@zurowietz.de>
2015-03-12 15:52:45 +0100

    Adds the User resource.

---
+++ resources/assets/js/factories/User.js
@@ -0,0 +1,56 @@
+/**
+ * @ngdoc factory
+ * @name User
+ * @memberOf dias.core
+ * @description Provides the resource for users.
+ * @requires $resource
+ * @returns {Object} A new [ngResource](https://docs.angularjs.org/api/ngResource/service/$resource) object
+ * @example
+// get a list of all users
+var users = User.query(function () {
+   console.log(users); // [{id: 1, firstname: "Jane", ...}, ...]
+});
+
+// retrieving the username
+var user = User.get({id: 1}, function () {
+   console.log(user.firstname);
+});
+
+// creating a new user
+var user = User.add(
+   {
+      email: 'my@mail.com',
+      password: '123456pw',
+      password_confirmation: '123456pw',
+      firstname: 'jane',
+      lastname: 'user'
+   },
+   function () {
+      console.log(user); // {id: 1, firstname: 'jane', ...}
+   }
+);
+
+// changing the username
+var user = User.get({id: 1}, function () {
+   user.firstname == 'Joel';
+   user.$save();
+});
+// or directly
+User.save({id: 1, firstname: 'Joel'});
+
+// deleting the user
+var user = User.get({id: 1}, function () {
+   user.$delete();
+});
+// or directly
+User.delete({id: 1});
+ * 
+ */
+angular.module('dias.core').factory('User', function ($resource) {
+	"use strict";
+
+	return $resource('/api/v1/users/:id', {id: '@id'}, {
+		save: { method: 'PUT' },
+		add: { method: 'POST' }
+	});
+});
