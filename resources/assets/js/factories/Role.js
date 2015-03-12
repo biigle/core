422ae4c5a1ca675702162f9422dceee6ff702653
Martin Zurowietz <martin@zurowietz.de>
2015-03-12 12:24:55 +0100

    Adds the Role resource.

---
+++ resources/assets/js/factories/Role.js
@@ -0,0 +1,24 @@
+/**
+ * @ngdoc factory
+ * @name Role
+ * @memberOf dias.core
+ * @description Provides the resource for roles.
+ * @requires $resource
+ * @returns {Object} A new [ngResource](https://docs.angularjs.org/api/ngResource/service/$resource) object
+ * @example
+// get all roles
+var roles = Role.query(function () {
+   console.log(roles); // [{id: 1, name: "admin"}, ...]
+});
+
+// get one media type
+var role = Role.get({id: 1}, function () {
+   console.log(role); // {id: 1, name: "admin"}
+});
+ *
+ */
+angular.module('dias.core').factory('Role', function ($resource) {
+	"use strict";
+
+	return $resource('/api/v1/roles/:id', { id: '@id' });
+});
---
+++ tests/js/factories/Role.js
@@ -0,0 +1,44 @@
+describe('The Role resource factory', function () {
+	var $httpBackend;
+
+	beforeEach(module('dias.core'));
+
+	beforeEach(inject(function($injector) {
+		var role = {
+			id: 1,
+			name: "admin"
+		};
+
+		// Set up the mock http service responses
+		$httpBackend = $injector.get('$httpBackend');
+
+		$httpBackend.when('GET', '/api/v1/roles')
+		            .respond([role]);
+		
+		$httpBackend.when('GET', '/api/v1/roles/1')
+		            .respond(role);
+	}));
+
+	afterEach(function() {
+		$httpBackend.verifyNoOutstandingExpectation();
+		$httpBackend.verifyNoOutstandingRequest();
+	});
+
+	it('should query roles', inject(function (Role) {
+		$httpBackend.expectGET('/api/v1/roles');
+		var roles = Role.query(function () {
+			var role = roles[0];
+			expect(role instanceof Role).toBe(true);
+			expect(role.name).toEqual('admin');
+		});
+		$httpBackend.flush();
+	}));
+
+	it('should show a role', inject(function (Role) {
+		$httpBackend.expectGET('/api/v1/roles/1');
+		var role = Role.get({id: 1}, function () {
+			expect(role.name).toEqual('admin');
+		});
+		$httpBackend.flush();
+	}));
+});
