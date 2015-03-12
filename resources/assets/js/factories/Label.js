d29b2f738076535907d0105578adabaaafe3942e
Martin Zurowietz <martin@zurowietz.de>
2015-03-12 10:36:28 +0100

    Adds the Label resource.

---
+++ resources/assets/js/factories/Label.js
@@ -0,0 +1,49 @@
+/**
+ * @ngdoc factory
+ * @name Label
+ * @memberOf dias.core
+ * @description Provides the resource for labels.
+ * @requires $resource
+ * @returns {Object} A new [ngResource](https://docs.angularjs.org/api/ngResource/service/$resource) object
+ * @example
+// get all labels
+var labels = Label.query(function () {
+   console.log(labels); // [{id: 1, name: "Benthic Object", ...}, ...]
+});
+
+// get one label
+var label = Label.get({id: 1}, function () {
+   console.log(label); // {id: 1, name: "Benthic Object", ...}
+});
+
+// create a new label
+var label = Label.add({name: "Trash", parent_id: 1}, function () {
+   console.log(label); // {id: 2, name: "Trash", parent_id: 1, ...}
+});
+
+// update a label
+var label = Label.get({id: 1}, function () {
+   label.name = 'Trash';
+   label.$save();
+});
+// or directly
+Label.save({id: 1, name: 'Trash'});
+
+// delete a label
+var label = Label.get({id: 1}, function () {
+   label.$delete();
+});
+// or directly
+Label.delete({id: 1});
+ *
+ */
+angular.module('dias.core').factory('Label', function ($resource) {
+	"use strict";
+
+	return $resource('/api/v1/labels/:id', { id: '@id' },
+		{
+			add: {method: 'POST' },
+			save: { method: 'PUT' }
+		}
+	);
+});
---
+++ tests/js/factories/Label.js
@@ -0,0 +1,89 @@
+describe('The Label resource factory', function () {
+	var $httpBackend;
+
+	beforeEach(module('dias.core'));
+
+	beforeEach(inject(function($injector) {
+		var label = {
+			id: 1,
+			name: "Benthic Object"
+		};
+
+		// Set up the mock http service responses
+		$httpBackend = $injector.get('$httpBackend');
+
+		$httpBackend.when('GET', '/api/v1/labels')
+		            .respond([label]);
+		
+		$httpBackend.when('GET', '/api/v1/labels/1')
+		            .respond(label);
+
+		$httpBackend.when('POST', '/api/v1/labels')
+		            .respond(label);
+
+		$httpBackend.when('PUT', '/api/v1/labels/1')
+		            .respond(200);
+	
+		$httpBackend.when('DELETE', '/api/v1/labels/1')
+		            .respond(200);
+	}));
+
+	afterEach(function() {
+		$httpBackend.verifyNoOutstandingExpectation();
+		$httpBackend.verifyNoOutstandingRequest();
+	});
+
+	it('should query labels', inject(function (Label) {
+		$httpBackend.expectGET('/api/v1/labels');
+		var labels = Label.query(function () {
+			var label = labels[0];
+			expect(label instanceof Label).toBe(true);
+			expect(label.name).toEqual('Benthic Object');
+		});
+		$httpBackend.flush();
+	}));
+
+	it('should show a label', inject(function (Label) {
+		$httpBackend.expectGET('/api/v1/labels/1');
+		var label = Label.get({id: 1}, function () {
+			expect(label.name).toEqual('Benthic Object');
+		});
+		$httpBackend.flush();
+	}));
+
+	it('should add new labels', inject(function (Label) {
+		$httpBackend.expectPOST('/api/v1/labels', {name: "Benthic Object"});
+		var label = Label.add({name: "Benthic Object"}, function () {
+			expect(label.name).toEqual('Benthic Object');
+			expect(label.id).toBeDefined();
+		});
+		$httpBackend.flush();
+	}));
+
+	it('should update labels', inject(function (Label) {
+		$httpBackend.expectPUT('/api/v1/labels/1',
+			{id:1, name: "Trash"}
+		);
+		var label = Label.get({id: 1}, function () {
+			label.name = 'Trash';
+			label.$save();
+		});
+
+		$httpBackend.expectPUT('/api/v1/labels/1', {id:1, name: "Trash"});
+		var label = Label.save({id: 1, name: 'Trash'}, function () {
+			expect(label.name).toEqual('Trash');
+		});
+		$httpBackend.flush();
+	}));
+
+	it('should delete labels', inject(function (Label) {
+		$httpBackend.expectDELETE('/api/v1/labels/1');
+		var label = Label.get({id: 1}, function () {
+			label.$delete();
+		});
+
+		$httpBackend.expectDELETE('/api/v1/labels/1');
+		Label.delete({id: 1});
+		$httpBackend.flush();
+	}));
+});
