785314ba6057c1fe612c0746f5f00c6d5cc72e7a
Martin Zurowietz <martin@zurowietz.de>
2015-03-12 11:30:28 +0100

    Adds the ProjectTransect resource.

---
+++ resources/assets/js/factories/ProjectTransect.js
@@ -0,0 +1,56 @@
+/**
+ * @ngdoc factory
+ * @name ProjectTransect
+ * @memberOf dias.core
+ * @description Provides the resource for transects belonging to a project.
+ * @requires $resource
+ * @returns {Object} A new [ngResource](https://docs.angularjs.org/api/ngResource/service/$resource) object
+ * @example
+// get all transects of the project with ID 1
+var transects = ProjectTransect.query({ project_id: 1 }, function () {
+   console.log(transects); // [{id: 1, name: "transect 1", ...}, ...]
+});
+
+// add a new transect to the project with ID 1
+var transect = ProjectTransect.add({project_id: 1},
+   {
+      name: "transect 1",
+      url: "/vol/transects/1",
+      media_type_id: 1,
+      images: ["1.jpg", "2.jpg"]
+   },
+   function () {
+      console.log(transect); // {id: 1, name: "transect 1", ...}
+   }
+);
+
+// attach an existing transect to another project
+var transects = ProjectTransect.query({ project_id: 1 }, function () {
+   var transect = transects[0];
+   // transect is now attached to project 1 *and* 2
+   transect.$attach({project_id: 2});
+});
+// or directly (transect 1 will be attached to project 2)
+ProjectTransect.attach({project_id: 2}, {id: 1});
+
+// detach a transect from the project with ID 1
+var transects = ProjectTransect.query({ project_id: 1 }, function () {
+   var transect = transects[0];
+   transect.$detach({project_id: 1});
+});
+// or directly
+ProjectTransect.detach({project_id: 1}, {id: 1});
+ *
+ */
+angular.module('dias.core').factory('ProjectTransect', function ($resource) {
+	"use strict";
+
+	return $resource('/api/v1/projects/:project_id/transects/:id',
+		{ id: '@id' },
+		{
+			add: { method: 'POST' },
+			attach: { method: 'POST' },
+			detach: { method: 'DELETE' }
+		}
+	);
+});
