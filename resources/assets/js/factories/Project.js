c70fcddd813fbf712af90f0b7847ddf6fa25ab84
Martin Zurowietz <martin@zurowietz.de>
2015-03-12 10:57:48 +0100

    Adds the Project resource.

---
+++ resources/assets/js/factories/Project.js
@@ -0,0 +1,53 @@
+/**
+ * @ngdoc factory
+ * @name Project
+ * @memberOf dias.core
+ * @description Provides the resource for projects.
+ * @requires $resource
+ * @returns {Object} A new [ngResource](https://docs.angularjs.org/api/ngResource/service/$resource) object
+ * @example
+// get all projects, the current user belongs to
+var projects = Project.query(function () {
+   console.log(projects); // [{id: 1, name: "Test Project", ...}, ...]
+});
+
+// get one project
+var project = Project.get({id: 1}, function () {
+   console.log(project); // {id: 1, name: "Test Project", ...}
+});
+
+// create a new project
+var project = Project.add({name: "My Project", description: "my project"},
+   function () {
+      console.log(project); // {id: 2, name: "My Project", ...}
+   }
+);
+
+// update a project
+var project = Project.get({id: 1}, function () {
+   project.name = 'New Project';
+   project.$save();
+});
+// or directly
+Project.save({id: 1, name: 'New Project'});
+
+// delete a project
+var project = Project.get({id: 1}, function () {
+   project.$delete();
+});
+// or directly
+Project.delete({id: 1});
+ *
+ */
+angular.module('dias.core').factory('Project', function ($resource) {
+	"use strict";
+
+	return $resource('/api/v1/projects/:id', { id: '@id' },
+		{
+			// a user can only query their own projects
+			query: { method: 'GET', params: { id: 'my' }, isArray: true },
+			add: { method: 'POST' },
+			save: { method: 'PUT' }
+		}
+	);
+});
