98b90eab07e3cedfc41612faee805078908e2adf
Martin Zurowietz <martin@zurowietz.de>
2015-03-16 17:22:27 +0100

    Adds the dashboard mixin to the module registry.

--- src/ProjectsServiceProvider.php
+++ src/ProjectsServiceProvider.php
@@ -1,16 +1,19 @@
 <?php namespace Dias\Modules\Projects;
 
 use Illuminate\Support\ServiceProvider;
+use Dias\Services\Modules;
 
 class ProjectsServiceProvider extends ServiceProvider {
+
 	/**
 	* Bootstrap the application events.
 	*
 	* @return void
 	*/
-	public function boot()
+	public function boot(Modules $modules)
 	{
 		$this->loadViewsFrom(__DIR__.'/resources/views', 'projects');
+		$modules->addMixin('projects', 'dashboard');
 	}
 
 	/**
