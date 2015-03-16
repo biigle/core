98a47022cdf356e8f7806f65ed286ab7b5883695
Martin Zurowietz <martin@zurowietz.de>
2015-03-16 20:42:34 +0100

    Implements working dashboard mixin.

--- src/ProjectsServiceProvider.php
+++ src/ProjectsServiceProvider.php
@@ -23,6 +23,6 @@ class ProjectsServiceProvider extends ServiceProvider {
 	*/
 	public function register()
 	{
-		$app = $this->app;
+		//
 	}
 }
--- src/resources/views/dashboard.blade.php
+++ src/resources/views/dashboard.blade.php
@@ -1,1 +1,21 @@
-This is the projects dashboard mixin.
+<?php $user = Auth::user() ?>
+<h2>Projects</h2>
+
+@forelse($user->projects as $project)
+	<div class="panel panel-default">
+		<div class="panel-heading">
+			<h3 class="panel-title">{{ $project->name }}</h3>
+		</div>
+		<div class="panel-body">
+			@foreach ($mixins as $module => $nestedMixins)
+				@include($module.'::dashboard.projects', array('mixins' => $nestedMixins, 'project' => $project))
+			@endforeach
+		</div>
+	</div>
+@empty
+	<div class="panel panel-default">
+		<p class="text-muted">
+			You do not belong to any projects.
+		</p>
+	</div>
+@endforelse
