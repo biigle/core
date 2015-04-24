439c1b9c117ef8a670102a3149b15f76e9ff30b7
Martin Zurowietz <martin@zurowietz.de>
2015-04-24 09:52:51 +0200

    Includes the ImageAnnotation Angular resource in the Annotation resource.

--- public/assets/scripts/main.js
+++ public/assets/scripts/main.js
@@ -68,12 +68,50 @@ var annotation = Annotation.get({id: 123}, function () {
 });
 // or directly
 Annotation.delete({id: 123});
+
+// get all annotations of an image
+// note, that the `id` is now the image ID and not the annotation ID for the
+// query!
+var annotations = Annotation.query({id: 1}, function () {
+   console.log(annotations); // [{id: 1, shape_id: 1, ...}, ...]
+});
+
+// add a new annotation to an image
+// note, that the `id` is now the image ID and not the annotation ID for the
+// query!
+var annotation = Annotation.add({
+   id: 1,
+   shape_id: 1,
+   points: [
+      { x: 10, y: 20 }
+   ]
+});
  * 
  */
 angular.module('dias.api').factory('Annotation', ["$resource", "URL", function ($resource, URL) {
 	"use strict";
 
-	return $resource(URL + '/api/v1/annotations/:id/', { id: '@id'	});
+	return $resource(URL + '/api/v1/:endpoint/:id/:slug',
+		{ id: '@id'	},
+		{
+			get: {
+				method: 'GET',
+				params: { endpoint: 'annotations' }
+			},
+			delete: {
+				method: 'DELETE',
+				params: { endpoint: 'annotations' }
+			},
+			query: {
+				method: 'GET',
+				params: { endpoint: 'images', slug: 'annotations' },
+				isArray: true
+			},
+			add: {
+				method: 'POST',
+				params: { endpoint: 'images', slug: 'annotations' }
+			}
+		});
 }]);
 /**
  * @ngdoc factory
@@ -224,38 +262,6 @@ angular.module('dias.api').factory('Image', ["$resource", "URL", function ($reso
 	"use strict";
 
 	return $resource(URL + '/api/v1/images/:id');
-}]);
-/**
- * @ngdoc factory
- * @name ImageAnnotation
- * @memberOf dias.api
- * @description Provides the resource for annotations of an image.
- * @requires $resource
- * @returns {Object} A new [ngResource](https://docs.angularjs.org/api/ngResource/service/$resource) object
- * @example
-// get all annotations of an image
-var annotations = ImageAnnotation.query({image_id: 1}, function () {
-   console.log(annotations); // [{id: 1, shape_id: 1, ...}, ...]
-});
-
-// add a new annotation to an image
-var annotation = ImageAnnotation.add({
-   image_id: 1,
-   shape_id: 1,
-   points: [
-      { x: 10, y: 20 }
-   ]
-});
- *
- */
-angular.module('dias.api').factory('ImageAnnotation', ["$resource", "URL", function ($resource, URL) {
-	"use strict";
-
-	return $resource(
-		URL + '/api/v1/images/:image_id/annotations',
-		{ image_id: '@image_id' },
-		{ add: { method: 'POST' } }
-	);
 }]);
 /**
  * @ngdoc factory
--- resources/assets/js/api/factories/Annotation.js
+++ resources/assets/js/api/factories/Annotation.js
@@ -17,10 +17,48 @@ var annotation = Annotation.get({id: 123}, function () {
 });
 // or directly
 Annotation.delete({id: 123});
+
+// get all annotations of an image
+// note, that the `id` is now the image ID and not the annotation ID for the
+// query!
+var annotations = Annotation.query({id: 1}, function () {
+   console.log(annotations); // [{id: 1, shape_id: 1, ...}, ...]
+});
+
+// add a new annotation to an image
+// note, that the `id` is now the image ID and not the annotation ID for the
+// query!
+var annotation = Annotation.add({
+   id: 1,
+   shape_id: 1,
+   points: [
+      { x: 10, y: 20 }
+   ]
+});
  * 
  */
 angular.module('dias.api').factory('Annotation', function ($resource, URL) {
 	"use strict";
 
-	return $resource(URL + '/api/v1/annotations/:id/', { id: '@id'	});
+	return $resource(URL + '/api/v1/:endpoint/:id/:slug',
+		{ id: '@id'	},
+		{
+			get: {
+				method: 'GET',
+				params: { endpoint: 'annotations' }
+			},
+			delete: {
+				method: 'DELETE',
+				params: { endpoint: 'annotations' }
+			},
+			query: {
+				method: 'GET',
+				params: { endpoint: 'images', slug: 'annotations' },
+				isArray: true
+			},
+			add: {
+				method: 'POST',
+				params: { endpoint: 'images', slug: 'annotations' }
+			}
+		});
 });
