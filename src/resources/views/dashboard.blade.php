f7d3d9e930d4b96906d20b6e0da566edfa5e78dc
Martin Zurowietz <martin@zurowietz.de>
2015-09-09 12:41:38 +0200

    Some styling of the dashboard.

--- src/resources/views/dashboard.blade.php
+++ src/resources/views/dashboard.blade.php
@@ -2,19 +2,23 @@
 
 <h2 class="clearfix">Projects <a href="{{ route('create-project') }}" class="btn btn-default pull-right" title="Create a new project">New Project</a></h2>
 
-@forelse($user->projects as $project)
-	<div class="panel panel-default">
-		<div class="panel-heading">
-			<a href="{{ route('project', $project->id) }}" title="Show {{ $project->name }}"><h3 class="panel-title">{{ $project->name }}</h3></a>
-		</div>
-		<div class="panel-body">
-			@foreach ($mixins as $module => $nestedMixins)
-				@include($module.'::dashboard.projects', array('mixins' => $nestedMixins, 'project' => $project))
-			@endforeach
-		</div>
-	</div>
-@empty
-	<div class="alert alert-info">
-		You do not belong to any projects yet. Try <a href="{{ route('create-project') }}" class="alert-link">creating</a> one.
-	</div>
-@endforelse
+<div class="row">
+    @forelse($user->projects as $project)
+        <div class="col-lg-6">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    <a href="{{ route('project', $project->id) }}" title="Show {{ $project->name }}"><h3 class="panel-title">{{ $project->name }}</h3></a>
+                </div>
+                <div class="panel-body">
+                    @foreach ($mixins as $module => $nestedMixins)
+                        @include($module.'::dashboard.projects', array('mixins' => $nestedMixins, 'project' => $project))
+                    @endforeach
+                </div>
+            </div>
+        </div>
+    @empty
+        <div class="alert alert-info">
+            You do not belong to any projects yet. Try <a href="{{ route('create-project') }}" class="alert-link">creating</a> one.
+        </div>
+    @endforelse
+</div>
