b030a5efea5d0f40ef5dab95301869ffb99b9d1c
Martin Zurowietz <martin@zurowietz.de>
2015-09-16 10:54:58 +0200

    Improve asset pipeline
    The `artisan vendor:publish` command is now called automatically
    by Gulp.

--- gulpfile.js
+++ gulpfile.js
@@ -4,9 +4,15 @@ process.env.DISABLE_NOTIFIER = true;
 var gulp    = require('gulp');
 var elixir  = require('laravel-elixir');
 var angular = require('laravel-elixir-angular');
+var shell   = require('gulp-shell');
 
 elixir(function (mix) {
 	process.chdir('src');
 	mix.sass('main.scss', 'public/assets/styles')
-	   .angular('resources/assets/js/', 'public/assets/scripts', 'main.js');
+	   .angular('resources/assets/js/projects/', 'public/assets/scripts', 'main.js');
+    mix.task('publish', 'resources/assets/**/*');
+});
+
+gulp.task('publish', function () {
+    gulp.src('').pipe(shell('php ../../../../artisan vendor:publish --provider="Dias\\Modules\\Projects\\ProjectsServiceProvider" --force'));
 });
