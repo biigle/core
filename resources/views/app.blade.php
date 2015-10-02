6366913eb98f5ba0b3fd5ce94af944220c3490b9
Martin Zurowietz <martin@zurowietz.de>
2015-10-02 09:40:50 +0200

    Add favicon tag to base blade template

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,35 +1,36 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>@yield('title')</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" type="image/x-icon" href="{{ asset('favicon.ico') }}">
+    <title>@yield('title')</title>
 
-	<link href="{{ asset('assets/styles/main.css') }}" rel="stylesheet">
-	@yield('styles')
+    <link href="{{ asset('assets/styles/main.css') }}" rel="stylesheet">
+    @yield('styles')
 
-	<!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-	<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-	<!--[if lt IE 9]>
-		<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
-		<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-	<![endif]-->
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
 </head>
 <body>
 @if(auth()->check())
-	@include('partials.navbar')
+    @include('partials.navbar')
 @endif
-	@include('partials.messages')
-	@yield('content')
+    @include('partials.messages')
+    @yield('content')
 
-	<script type="text/javascript">
-		window.$diasBaseUrl = '{{ url() }}';
-	</script>
-	<script src="{{ asset('assets/scripts/angular.min.js') }}"></script>
-	<script src="{{ asset('assets/scripts/angular-resource.min.js') }}"></script>
-	<script src="{{ asset('assets/scripts/ui-bootstrap-tpls.min.js') }}"></script>
-	<script src="{{ asset('assets/scripts/main.js') }}"></script>
-	@yield('scripts')
+    <script type="text/javascript">
+        window.$diasBaseUrl = '{{ url() }}';
+    </script>
+    <script src="{{ asset('assets/scripts/angular.min.js') }}"></script>
+    <script src="{{ asset('assets/scripts/angular-resource.min.js') }}"></script>
+    <script src="{{ asset('assets/scripts/ui-bootstrap-tpls.min.js') }}"></script>
+    <script src="{{ asset('assets/scripts/main.js') }}"></script>
+    @yield('scripts')
 </body>
 </html>
