67514e0342ab66519020a7e393c6a5e997dabc31
Martin Zurowietz <martin@zurowietz.de>
2015-11-05 11:42:51 +0100

    Update to Angular UI Bootstrap 0.14.3

--- src/resources/views/index/sidebar/categories.blade.php
+++ src/resources/views/index/sidebar/categories.blade.php
@@ -1,6 +1,6 @@
 <div class="sidebar__categories" data-ng-class="{open:(foldout=='categories')}" data-ng-controller="CategoriesController">
     <div class="categories-search">
-        <input class="form-control" type="text" data-ng-model="searchCategory" data-typeahead="category.name for category in categories | filter:$viewValue | limitTo:10" data-typeahead-on-select="selectItem($item)" placeholder="Find category" />
+        <input class="form-control" type="text" data-ng-model="searchCategory" data-uib-typeahead="category.name for category in categories | filter:$viewValue | limitTo:10" data-typeahead-on-select="selectItem($item)" placeholder="Find category" />
 
         <button class="btn btn-inverse pull-right" data-ng-click="toggleFoldout('categories')" title="Collapse this foldout"><span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span></button>
     </div>
