e78eb5536d27a5f70054e1e574f30b641d810700
Martin Zurowietz <martin@zurowietz.de>
2015-11-12 14:17:14 +0100

    Umprove frontent msg service

--- resources/assets/js/core/ui/messages/services/msg.js
+++ resources/assets/js/core/ui/messages/services/msg.js
@@ -17,49 +17,49 @@ var label = AnnotationLabel.attach({ ... });
 label.$promise.catch(msg.responseError);
  */
 angular.module('dias.ui.messages').service('msg', function () {
-		"use strict";
-		var _this = this;
+        "use strict";
+        var _this = this;
 
-		this.post = function (type, message) {
-			message = message || type;
-			window.$diasPostMessage(type, message);
-		};
+        this.post = function (type, message) {
+            message = message || type;
+            window.$diasPostMessage(type, message);
+        };
 
-		this.danger = function (message) {
-			_this.post('danger', message);
-		};
+        this.danger = function (message) {
+            _this.post('danger', message);
+        };
 
-		this.warning = function (message) {
-			_this.post('warning', message);
-		};
+        this.warning = function (message) {
+            _this.post('warning', message);
+        };
 
-		this.success = function (message) {
-			_this.post('success', message);
-		};
+        this.success = function (message) {
+            _this.post('success', message);
+        };
 
-		this.info = function (message) {
-			_this.post('info', message);
-		};
+        this.info = function (message) {
+            _this.post('info', message);
+        };
 
-		this.responseError = function (response) {
-			var data = response.data;
+        this.responseError = function (response) {
+            var data = response.data;
 
-			if (response.status === 401) {
-				_this.danger("Please log in (again).");
-			} else if (!data) {
-				_this.danger("The server didn't respond, sorry.");
-			} else if (data.message) {
-				// error response
-				_this.danger(data.message);
-			} else if (data) {
-				// validation response
-				for (var key in data) {
-					_this.danger(data[key][0]);
-				}
-			} else {
-				// unknown error response
-				_this.danger("There was an error, sorry.");
-			}
-		};
-	}
+            if (!data) {
+                _this.danger("The server didn't respond, sorry.");
+            } else if (data.message) {
+                // error response
+                _this.danger(data.message);
+            } else if (response.status === 401) {
+                _this.danger("Please log in (again).");
+            } else if (typeof data === 'string') {
+                // unknown error response
+                _this.danger(data);
+            } else {
+                // validation response
+                for (var key in data) {
+                    _this.danger(data[key][0]);
+                }
+            }
+        };
+    }
 );
