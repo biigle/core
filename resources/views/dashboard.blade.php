44d86ac39e7740038162e30d9d14a85ca786361d
Martin Zurowietz <martin@zurowietz.de>
2015-11-12 09:59:24 +0100

    Fix style of 'no dashboard modules' alert

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -21,7 +21,7 @@
 		@forelse ($modules->getMixins('dashboard') as $module => $nestedMixins)
 			@include($module.'::dashboard', array('mixins' => $nestedMixins))
 		@empty
-			<p class="alert alert-info">
+			<p class="alert alert-info col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2">
 				You don't have any dashboard modules activated yet.
 			</p>
 		@endforelse
