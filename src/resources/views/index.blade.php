34b179d70a7ad1751b980eb6e27003e1fadb2c51
Martin Zurowietz <martin@zurowietz.de>
2015-12-07 10:54:18 +0100

    Change appearance of image flags to a simple dot

--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -36,7 +36,7 @@
     <div class="transect__images" data-ng-controller="ImagesController">
         <figure class="transect-figure" data-ng-repeat="id in images.sequence | limitTo: images.limit">
             <div class="transect-figure__flags">
-                <span class="figure-flag glyphicon glyphicon-bookmark" data-ng-repeat="flag in getFlagsFor(id) track by $index" title="@{{flag.title}}" data-ng-class="flag.name"></span>
+                <span class="figure-flag" data-ng-repeat="flag in getFlagsFor(id) track by $index" title="@{{flag.title}}" data-ng-class="flag.name"></span>
             </div>
             <img src="{{ asset(config('thumbnails.empty_url')) }}" data-lazy-image="{{ url('api/v1/images/') }}/@{{ id }}/thumb">
             @foreach ($modules->getMixins('transects') as $module => $nestedMixins)
