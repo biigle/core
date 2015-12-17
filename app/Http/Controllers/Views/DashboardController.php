ffdf414f221d1bb8debfba0d9fb6c9e43e9bbe16
Martin Zurowietz <martin@zurowietz.de>
2015-12-17 13:05:44 +0100

    Add the user variable to the dashboard view

--- app/Http/Controllers/Views/DashboardController.php
+++ app/Http/Controllers/Views/DashboardController.php
@@ -11,6 +11,7 @@ class DashboardController extends Controller
      */
     public function index()
     {
-        return view('dashboard');
+        return view('dashboard')
+            ->with('user', auth()->user());
     }
 }
