9d20c125eb9c11c6678fc3f2f6616d3724d36480
Martin Zurowietz <martin@zurowietz.de>
2016-02-12 11:24:53 +0100

    Add initial label to StoreImageAnnotations API endpoint output

--- tests/unit/Http/Controllers/Api/ApiImageAnnotationControllerTest.php
+++ tests/unit/Http/Controllers/Api/ApiImageAnnotationControllerTest.php
@@ -182,6 +182,14 @@ class ApiImageAnnotationControllerTest extends ApiTestCase
             ]);
         }
 
+        $this->seeJson([
+            'name' => $this->labelRoot->name
+        ]);
+
+        $this->seeJson([
+            'color' => $this->labelRoot->color
+        ]);
+
         $annotation = $this->image->annotations->first();
         $this->assertNotNull($annotation);
         $this->assertEquals(1, $annotation->points()->count());
