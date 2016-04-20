216bad8464f90282d4fea67b94966318ef8216d1
Martin Zurowietz <martin@zurowietz.de>
2016-04-20 16:09:51 +0200

    Upgrade to Laravel 5.2

--- src/Http/routes.php
+++ src/Http/routes.php
@@ -1,37 +1,23 @@
 <?php
 
-Route::group([
-        'namespace' => '\Dias\Modules\Transects\Http\Controllers',
+$router->group([
         'middleware' => 'auth',
-        'prefix' => 'transects',
     ], function ($router) {
-    $router->get('/create', [
+
+    $router->get('transects/create', [
         'as'   => 'create-transect',
         'uses' => 'TransectController@create',
     ]);
 
-    $router->get('/{id}', [
+    $router->get('transects/{id}', [
         'as'   => 'transect',
         'uses' => 'TransectController@index',
     ]);
-});
 
-Route::group([
-        'namespace' => '\Dias\Modules\Transects\Http\Controllers',
-        'middleware' => 'auth',
-        'prefix' => 'images',
-    ], function ($router) {
-
-    $router->get('/{id}', [
+    $router->get('images/{id}', [
         'as'   => 'image',
         'uses' => 'ImageController@index',
     ]);
-});
-
-Route::group([
-        'namespace' => '\Dias\Modules\Transects\Http\Controllers',
-        'middleware' => 'auth'
-    ], function ($router) {
 
     $router->get('admin/transects', [
         'as' => 'admin-transects',
--- src/TransectsServiceProvider.php
+++ src/TransectsServiceProvider.php
@@ -3,6 +3,7 @@
 namespace Dias\Modules\Transects;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Routing\Router;
 use Dias\Services\Modules;
 
 class TransectsServiceProvider extends ServiceProvider
@@ -10,9 +11,12 @@ class TransectsServiceProvider extends ServiceProvider
     /**
      * Bootstrap the application events.
      *
+     * @param  \Dias\Services\Modules  $modules
+     * @param  \Illuminate\Routing\Router  $router
+     *
      * @return void
      */
-    public function boot(Modules $modules)
+    public function boot(Modules $modules, Router $router)
     {
         $this->loadViewsFrom(__DIR__.'/resources/views', 'transects');
 
@@ -20,7 +24,12 @@ class TransectsServiceProvider extends ServiceProvider
             __DIR__.'/public/assets' => public_path('vendor/transects'),
         ], 'public');
 
-        include __DIR__.'/Http/routes.php';
+        $router->group([
+            'namespace' => 'Dias\Modules\Transects\Http\Controllers',
+            'middleware' => 'web',
+        ], function ($router) {
+            require __DIR__.'/Http/routes.php';
+        });
 
         $modules->addMixin('transects', 'dashboard.projects');
         $modules->addMixin('transects', 'dashboardStyles');
