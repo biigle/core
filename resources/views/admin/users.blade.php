5a55abcfe2506c621e9412a7e638b6be609a9f18
Martin Zurowietz <martin@zurowietz.de>
2016-05-03 14:56:32 +0200

    Improve admin area users view

--- resources/views/admin/users.blade.php
+++ resources/views/admin/users.blade.php
@@ -9,13 +9,14 @@
     </div>
 @endif
 <a href="{{route('admin-users-new')}}" class="btn btn-default" title="Create a new user"><span class="glyphicon glyphicon-plus" aria-hidden="true"></span> New user</a>
-<table class="table">
+<table class="table table-hover">
     <thead>
         <tr>
             <th>Name</th>
             <th>Email</th>
             <th>Role</th>
-            <th>Last activity</th>
+            <th>Activity</th>
+            <th></th>
         </tr>
     </thead>
     <tbody>
@@ -24,7 +25,18 @@
                 <td>{{$user->firstname}} {{$user->lastname}}</td>
                 <td><a href="mailto:{{$user->email}}">{{$user->email}}</a></td>
                 <td>{{ucfirst($user->role->name)}}</td>
-                <td>@if ($user->login_at) <time datetime="{{$user->login_at->toAtomString()}}" title="{{$user->login_at->toDateTimeString()}}">{{$user->login_at->diffForHumans()}}</time> @else <span class="text-muted">no login yet</span> @endif @if ($user->id !== auth()->user()->id) <a class="pull-right text-muted" href="{{route('admin-users-edit', $user->id)}}" title="Edit {{$user->firstname}} {{$user->lastname}}"><span class="glyphicon glyphicon-pencil" aria-hidden="true"></span></a>@endif </td>
+                <td>
+                    @if ($user->login_at)
+                        <time datetime="{{$user->login_at->toAtomString()}}" title="{{$user->login_at->toDateTimeString()}}">{{$user->login_at->diffForHumans()}}</time>
+                    @else
+                        <span class="text-muted">none</span>
+                    @endif
+                </td>
+                <td>
+                    @if ($user->id !== auth()->user()->id)
+                        <a class="btn btn-default btn-xs" href="{{route('admin-users-edit', $user->id)}}" title="Edit {{$user->firstname}} {{$user->lastname}}"><span class="glyphicon glyphicon-pencil" aria-hidden="true"></span></a>
+                    @endif
+                </td>
             </tr>
         @endforeach
     </tbody>
