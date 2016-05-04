1f663c819839026752966c6fc3d025c79f07222b
Martin Zurowietz <martin@zurowietz.de>
2016-05-04 09:12:37 +0200

    Disable Angular debugging info for better performance

--- src/resources/assets/js/transects/main.js
+++ src/resources/assets/js/transects/main.js
@@ -3,3 +3,12 @@
  * @description The DIAS transects module.
  */
 angular.module('dias.transects', ['dias.api', 'dias.ui']);
+
+/*
+ * Disable debug info in production for better performance.
+ */
+angular.module('dias.transects').config(function ($compileProvider) {
+    "use strict";
+
+    $compileProvider.debugInfoEnabled(false);
+});
