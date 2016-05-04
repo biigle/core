59b5e11537a40ca1c03e584a72a15fcf1526b3ff
Martin Zurowietz <martin@zurowietz.de>
2016-05-04 09:11:34 +0200

    Disable Angular debugging info for better performance

--- src/resources/assets/js/annotations/main.js
+++ src/resources/assets/js/annotations/main.js
@@ -3,3 +3,12 @@
  * @description The DIAS annotations module.
  */
 angular.module('dias.annotations', ['dias.api', 'dias.ui']);
+
+/*
+ * Disable debug info in production for better performance.
+ */
+angular.module('dias.annotations').config(function ($compileProvider) {
+    "use strict";
+
+    $compileProvider.debugInfoEnabled(false);
+});
