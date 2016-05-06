e07f2c3c8625beb3a32a27993f687baa18726d6b
Martin Zurowietz <martin@zurowietz.de>
2016-05-06 10:19:46 +0200

    Update docs

--- public/doc/api/api_project.js
+++ public/doc/api/api_project.js
@@ -15,7 +15,7 @@ define({
   "apidoc": "0.2.0",
   "generator": {
     "name": "apidoc",
-    "time": "2016-04-29T08:08:44.928Z",
+    "time": "2016-05-06T08:19:27.823Z",
     "url": "http://apidocjs.com",
     "version": "0.13.2"
   }
