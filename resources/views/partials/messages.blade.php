8cea7a6486bc7a175ac5bb33dd71335c58112322
Martin Zurowietz <martin@zurowietz.de>
2016-05-10 10:51:59 +0200

    Update static alert directive to uib-alert

--- resources/views/partials/messages.blade.php
+++ resources/views/partials/messages.blade.php
@@ -1,6 +1,6 @@
 <div data-ng-controller="MessagesController" class="messages container-fluid">
 	<div data-uib-alert="" data-ng-repeat="alert in alerts" data-type="@{{alert.type}}" data-close="close($index)" class="messages__message"><span data-ng-bind="alert.message"></span></div>
 	@if (session()->has('message'))
-		<div data-alert="" data-type="@if (session()->has('messageType')) {{session('messageType')}} @endif" class="messages__message" data-ng-hide="hide" data-close="hide = true">{{ session('message') }}</div>
+		<div data-uib-alert="" data-type="@if (session()->has('messageType')) {{session('messageType')}} @endif" class="messages__message" data-ng-hide="hide" data-close="hide = true">{{ session('message') }}</div>
 	@endif
 </div>
