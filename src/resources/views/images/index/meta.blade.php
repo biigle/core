157417f5a2faf8df0860f2674c2a5e2e5b973312
Martin Zurowietz <martin@zurowietz.de>
2016-05-11 11:24:19 +0200

    Fix image info view if no EXIF data is present

--- src/resources/views/images/index/meta.blade.php
+++ src/resources/views/images/index/meta.blade.php
@@ -1,25 +1,27 @@
 <div class="col-sm-6 col-lg-4">
-	<div class="panel panel-default">
-		<div class="panel-heading">
-			<h3 class="panel-title">Meta info</h3>
-		</div>
-		<table class="table">
-			<tr>
-				<th>Transect</th>
-				<td>{{ $image->transect->name }}</td>
-			</tr>
-			<tr>
-				<th>Filename</th>
-				<td>{{ $image->filename }}</td>
-			</tr>
-			<tr>
-				<th>Dimensions</th>
-				<td>{{ $image->width }} &times; {{ $image->height }} px</td>
-			</tr>
-			<tr>
-				<th>Size</th>
-				<td>{{ round($image->exif['FileSize'] / 1e6, 2) }} MByte</td>
-			</tr>
-		</table>
-	</div>
+    <div class="panel panel-default">
+        <div class="panel-heading">
+            <h3 class="panel-title">Meta info</h3>
+        </div>
+        <table class="table">
+            <tr>
+                <th>Transect</th>
+                <td>{{ $image->transect->name }}</td>
+            </tr>
+            <tr>
+                <th>Filename</th>
+                <td>{{ $image->filename }}</td>
+            </tr>
+            <tr>
+                <th>Dimensions</th>
+                <td>{{ $image->width }} &times; {{ $image->height }} px</td>
+            </tr>
+            @if (is_array($image->exif) && array_key_exists('FileSize', $image->exif))
+                <tr>
+                    <th>Size</th>
+                    <td>{{ round($image->exif['FileSize'] / 1e6, 2) }} MByte</td>
+                </tr>
+            @endif
+        </table>
+    </div>
 </div>
