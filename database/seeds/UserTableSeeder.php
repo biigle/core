3fb28f979deab0183f94be7125cfc80c922613a1
Daniel Langenkämper <dlangenk@gmail.com>
2016-05-12 10:07:41 +0200

    Delete unneeded seeders and updated user seeder.

--- database/seeds/UserTableSeeder.php
+++ database/seeds/UserTableSeeder.php
@@ -8,24 +8,12 @@ class UserTableSeeder extends Seeder
     public function run()
     {
         DB::table('users')->delete();
-
-        $joe = User::create([
-            'firstname' => 'Joe',
-            'lastname'  => 'User',
-            'email'     => 'joe@user.com',
-            'password'  => Hash::make('joespassword'),
-        ]);
-
-        $joe->generateApiKey();
-        $joe->save();
-
         $jane = User::create([
             'firstname' => 'Jane',
             'lastname'  => 'User',
             'email'     => 'jane@user.com',
             'password'  => Hash::make('janespassword'),
         ]);
-
         $jane->role()->associate(Dias\Role::$admin);
         $jane->save();
     }
