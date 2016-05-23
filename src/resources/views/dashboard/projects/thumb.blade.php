82425f053b0bf04c92c3bd5bd52d59cfee4e7adb
Martin Zurowietz <martin@zurowietz.de>
2016-05-23 10:35:04 +0200

    Update dashboard thumbnail for new API behavior
    The API now responds with a 404 instead of the fallback image
    if the thumbnail is not present.

--- src/resources/views/dashboard/projects/thumb.blade.php
+++ src/resources/views/dashboard/projects/thumb.blade.php
@@ -1,7 +1,12 @@
 <figure class="transects-dashboard__thumb">
 	<a href="{{ route('transect', $transect->id) }}">
         @if ($transect->images()->exists())
-		  <img src="{{ url('api/v1/images/'.$transect->images()->first()->id.'/thumb') }}">
+            <?php $image = $transect->images()->first() ?>
+            @if (File::exists($image->thumbPath))
+                <img src="{{ url('api/v1/images/'.$image->id.'/thumb') }}">
+            @else
+                <img src="{{ asset(config('thumbnails.empty_url')) }}">
+            @endif
         @endif
 		<figcaption class="caption">{{ $transect->name }}</figcaption>
 	</a>
