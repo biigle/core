99e7b09554046646eed23f9b6cbdd165dca2d847
Martin Zurowietz <martin@zurowietz.de>
2016-05-24 10:38:36 +0200

    Improve urlParams service of dias.ui.utils with an unset function

--- resources/assets/js/core/ui/utils/services/urlParams.js
+++ resources/assets/js/core/ui/utils/services/urlParams.js
@@ -6,60 +6,75 @@
  * @description Manages URL parameters after a #
  */
 angular.module('dias.ui.utils').service('urlParams', function () {
-      "use strict";
+        "use strict";
 
-      var state = {};
-      var slug = location.pathname.split('/');
-      slug = slug[slug.length - 1];
+        var state = {};
+        var slug = location.pathname.split('/');
+        slug = slug[slug.length - 1];
 
-      // transforms a URL parameter string like #a=1&b=2 to an object
-      var decodeState = function () {
-         var params = location.hash.replace('#', '').split('&');
+        // transforms a URL parameter string like #a=1&b=2 to an object
+        var decodeState = function () {
+            var params = location.hash.replace('#', '').split('&');
 
-         var state = {};
+            var state = {};
 
-         params.forEach(function (param) {
-            // capture key-value pairs
-            var capture = param.match(/(.+)\=(.+)/);
-            if (capture && capture.length === 3) {
-               state[capture[1]] = decodeURIComponent(capture[2]);
+            params.forEach(function (param) {
+                // capture key-value pairs
+                var capture = param.match(/(.+)\=(.+)/);
+                if (capture && capture.length === 3) {
+                    state[capture[1]] = decodeURIComponent(capture[2]);
+                }
+            });
+
+            return state;
+        };
+
+        // transforms an object to a URL parameter string
+        var encodeState = function (state) {
+            var params = '';
+            for (var key in state) {
+                params += key + '=' + encodeURIComponent(state[key]) + '&';
             }
-         });
+            return params.substring(0, params.length - 1);
+        };
 
-         return state;
-      };
+        var pushState = function () {
+            var hash = encodeState(state);
+            history.pushState(state, '', slug + (hash) ? ('#' + hash) : '');
+        };
 
-      // transforms an object to a URL parameter string
-      var encodeState = function (state) {
-         var params = '';
-         for (var key in state) {
-            params += key + '=' + encodeURIComponent(state[key]) + '&';
-         }
-         return params.substring(0, params.length - 1);
-      };
+        var replaceState = function () {
+            var hash = encodeState(state);
+            history.replaceState(state, '', slug + (hash) ? ('#' + hash) : '');
+        };
 
-      this.pushState = function (s) {
-         slug = s;
-         history.pushState(state, '', slug + '#' + encodeState(state));
-      };
+        this.pushState = function (s) {
+            slug = s;
+            pushState();
+        };
 
-      // sets a URL parameter and updates the history state
-      this.set = function (params) {
-         for (var key in params) {
-            state[key] = params[key];
-         }
-         history.replaceState(state, '', slug + '#' + encodeState(state));
-      };
+        // sets a URL parameter and updates the history state
+        this.set = function (params) {
+            for (var key in params) {
+                state[key] = params[key];
+            }
+            replaceState();
+        };
 
-      // returns a URL parameter
-      this.get = function (key) {
-         return state[key];
-      };
+        this.unset = function (key) {
+            delete state[key];
+            replaceState();
+        };
 
-      state = history.state;
+        // returns a URL parameter
+        this.get = function (key) {
+            return state[key];
+        };
 
-      if (!state) {
-         state = decodeState();
-      }
-   }
+        state = history.state;
+
+        if (!state) {
+            state = decodeState();
+        }
+    }
 );
