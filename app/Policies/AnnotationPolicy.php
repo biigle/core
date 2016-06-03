0d75ca86d011b95160826eccc9bad26e49c03bf0
Martin Zurowietz <martin@zurowietz.de>
2016-06-03 22:10:53 +0200

    Improve annotation attach-label policy

--- app/Policies/AnnotationPolicy.php
+++ app/Policies/AnnotationPolicy.php
@@ -37,12 +37,22 @@ class AnnotationPolicy
      */
     public function attachLabel(User $user, Annotation $annotation, Label $label)
     {
+        // the projects, the annotation belongs to
         $projectIds = DB::table('project_transect')
             ->join('images', 'project_transect.transect_id', '=', 'images.transect_id')
             ->where('images.id', $annotation->image_id)
             ->pluck('project_transect.project_id');
 
-        return $user->projects()->whereIn('id', $projectIds)->whereIn('project_user.project_role_id', [Role::$editor->id, Role::$admin->id])->exists()
-            && DB::table('label_tree_project')->whereIn('project_id', $projectIds)->where('label_tree_id', $label->label_tree_id)->exists();
+        // user must be editor or admin in one of the projects
+        return DB::table('project_user')
+                ->where('user_id', $user->id)
+                ->whereIn('project_id', $projectIds)
+                ->whereIn('project_role_id', [Role::$editor->id, Role::$admin->id])
+                ->exists()
+            // label must belong to a label tree that is used by one of the projects
+            && DB::table('label_tree_project')
+                ->whereIn('project_id', $projectIds)
+                ->where('label_tree_id', $label->label_tree_id)
+                ->exists();
     }
 }
