ff6f1487ee325eeb32b87350d86a8d423c146ead
Martin Zurowietz <martin@zurowietz.de>
2016-06-13 14:15:54 +0200

    Implement 100% zoom button
    The button will zoom to the zoom level that is nearest to the
    original resolution.
    
    This commit enhances the styling of custom icons in OL controls,
    too.
    
    Closes #15

--- src/resources/assets/js/annotations/factories/map.js
+++ src/resources/assets/js/annotations/factories/map.js
@@ -5,22 +5,33 @@
  * @memberOf dias.annotations
  * @description Wrapper factory handling OpenLayers map
  */
-angular.module('dias.annotations').factory('map', function () {
-		"use strict";
+angular.module('dias.annotations').factory('map', function (ZoomToNativeControl) {
+        "use strict";
 
-		var map = new ol.Map({
-			target: 'canvas',
+        var map = new ol.Map({
+            target: 'canvas',
             renderer: 'canvas',
-			controls: [
-				new ol.control.Zoom(),
-				new ol.control.ZoomToExtent(),
-				new ol.control.FullScreen()
-			],
+            controls: [
+                new ol.control.Zoom(),
+                new ol.control.ZoomToExtent({
+                    tipLabel: 'Zoom to show whole image',
+                    // bootstrap glyphicons resize-small icon
+                    label: '\ue097'
+                }),
+                new ol.control.FullScreen({
+                    // bootstrap glyphicons fullscreen icon
+                    label: '\ue140'
+                }),
+                new ZoomToNativeControl({
+                    // bootstrap glyphicons resize-full icon
+                    label: '\ue096'
+                })
+            ],
             interactions: ol.interaction.defaults({
                 keyboard: false
             })
-		});
+        });
 
-		return map;
-	}
+        return map;
+    }
 );
