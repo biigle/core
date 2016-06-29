50ec7fe69d95d5c1f3ee888328ce6bca909ee0cc
Martin Zurowietz <martin@zurowietz.de>
2016-06-29 13:13:50 +0200

    Add missing row wrapper to tutorial

--- resources/views/manual/tutorials/whole-playlist.blade.php
+++ resources/views/manual/tutorials/whole-playlist.blade.php
@@ -3,12 +3,14 @@
 @section('manual-title') Biigle Dias Tutorial Playlist @stop
 
 @section('manual-content')
-    <p class="lead">
-        Learn how to use Biigle Dias.
-    </p>
+    <div class="row">
+        <p class="lead">
+            Learn how to use Biigle Dias.
+        </p>
 
-    <div class="embed-responsive embed-responsive-16by9">
-        <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/videoseries?list=PLwRp4qE1V-1Nwfr2LH68sbtH80pnAntNX" allowfullscreen></iframe>
+        <div class="embed-responsive embed-responsive-16by9">
+            <iframe class="embed-responsive-item" src="https://www.youtube.com/embed/videoseries?list=PLwRp4qE1V-1Nwfr2LH68sbtH80pnAntNX" allowfullscreen></iframe>
+        </div>
     </div>
 @endsection
 
