291640033025f6d4c04cf18b9b7c1370ee1fa94c
Martin Zurowietz <martin@zurowietz.de>
2016-07-25 10:11:26 +0200

    Add annotator sidebar view mixin location

--- src/resources/views/index/sidebar.blade.php
+++ src/resources/views/index/sidebar.blade.php
@@ -1,16 +1,19 @@
 @inject('modules', 'Dias\Services\Modules')
 <div class="annotator__sidebar" data-ng-controller="SidebarController">
-    @if ($editMode)
-	   @include('annotations::index.sidebar.categories')
-       @include('annotations::index.sidebar.settings')
-    @endif
-   @include('annotations::index.sidebar.filters')
-   @foreach ($modules->getMixins('annotationsSidebarFoldouts') as $module => $nestedMixins)
-        @include($module.'::annotationsSidebarFoldouts')
-    @endforeach
-    <div class="sidebar__permanent">
-		<div id="minimap" class="sidebar__minimap" data-ng-controller="MinimapController"></div>
-		@include('annotations::index.sidebar.controls')
-		@include('annotations::index.sidebar.annotations')
-	</div>
+      @if ($editMode)
+         @include('annotations::index.sidebar.categories')
+         @include('annotations::index.sidebar.settings')
+      @endif
+    @include('annotations::index.sidebar.filters')
+    @foreach ($modules->getMixins('annotationsSidebarFoldouts') as $module => $nestedMixins)
+            @include($module.'::annotationsSidebarFoldouts')
+      @endforeach
+      <div class="sidebar__permanent">
+      <div id="minimap" class="sidebar__minimap" data-ng-controller="MinimapController"></div>
+      @include('annotations::index.sidebar.controls')
+      @foreach ($modules->getMixins('annotationsSidebar') as $module => $nestedMixins)
+            @include($module.'::annotationsSidebar')
+      @endforeach
+      @include('annotations::index.sidebar.annotations')
+   </div>
 </div>
