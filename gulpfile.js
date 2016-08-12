06c8fcdfd2749d10f3376b2f44eb0f1594df3613
Martin Zurowietz <martin@zurowietz.de>
2016-08-12 10:42:39 +0200

    Switch from Laravel Elixir to gulp-helpers

--- gulpfile.js
+++ gulpfile.js
@@ -1,40 +1,27 @@
 "use strict"
-process.env.DISABLE_NOTIFIER = true;
 
-var gulp    = require('gulp');
-var elixir  = require('laravel-elixir');
-var angular = require('laravel-elixir-angular')
-
-/*
- |--------------------------------------------------------------------------
- | Elixir Asset Management
- |--------------------------------------------------------------------------
- |
- | Elixir provides a clean, fluent API for defining some basic Gulp tasks
- | for your Laravel application. By default, we are compiling the Sass
- | file for our application, as well as publishing vendor resources.
- |
- */
-
-elixir(function (mix) {
-	mix.sass('main.scss', 'public/assets/styles/main.css')
-	   .angular('resources/assets/js/core/', 'public/assets/scripts', 'main.js');
-});
-
-var shell = require('gulp-shell');
-gulp.task('docs', shell.task([
-	'node_modules/jsdoc/jsdoc.js '+
-	'-c node_modules/angular-jsdoc/conf.json '+   // config file
-	'-t node_modules/angular-jsdoc/template '+    // template file
-	'-d public/doc/client '+                      // output directory
-	'-r resources/assets/js'                      // source code directory
-]));
-
+var gulp = require('gulp');
+var h = require('gulp-helpers');
 var apidoc = require('gulp-apidoc');
-gulp.task('apidoc', function (cb) {
-	apidoc.exec({
-		src: 'app/Http/Controllers/Api/',
-		dest: 'public/doc/api'
-	}, cb);
+
+gulp.task('sass', function () {
+   h.sass('main.scss', 'main.css');
 });
 
+gulp.task('js', function (cb) {
+   h.angular('core/**/*.js', 'main.js', cb);
+});
+
+gulp.task('apidoc', function (cb) {
+    apidoc.exec({
+        src: 'app/Http/Controllers/Api/',
+        dest: 'public/doc/api'
+    }, cb);
+});
+
+gulp.task('watch', function () {
+    gulp.watch(h.paths.sass + '**/*.scss', ['sass']);
+    gulp.watch(h.paths.js + 'core/**/*.js', ['js']);
+});
+
+gulp.task('default', ['sass', 'js'])
