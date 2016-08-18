d4e38def6ecb82b5f731ad35de88ef1dc2ce9c3d
Martin Zurowietz <martin@zurowietz.de>
2016-08-18 21:02:00 +0200

    Add migration to update the failed_jobs table
    The table now requires an additional 'exception' column.

---
+++ database/migrations/2016_08_18_120658_update_failed_jobs_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class UpdateFailedJobsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        // update for Laravel 5.3
+        Schema::table('failed_jobs', function (Blueprint $table) {
+            $table->text('exception')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('failed_jobs', function (Blueprint $table) {
+            $table->dropColumn('exception');
+        });
+    }
+}
