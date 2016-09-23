2a97dfb53ba7fba765bb82307a6aa88e08b87226
Martin Zurowietz <martin@zurowietz.de>
2016-09-23 15:57:53 +0200

    Add various indexes to the annotation_labels table

---
+++ database/migrations/2016_09_23_135248_create_annotation_label_index.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAnnotationLabelIndex extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('annotation_labels', function (Blueprint $table) {
+            $table->index('annotation_id');
+            $table->index('label_id');
+            $table->index('user_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('annotation_labels', function (Blueprint $table) {
+            $table->dropIndex('annotation_labels_annotation_id_index');
+            $table->dropIndex('annotation_labels_label_id_index');
+            $table->dropIndex('annotation_labels_user_id_index');
+        });
+    }
+}
