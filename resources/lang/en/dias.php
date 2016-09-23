3a943bd1f2c23c53c1db117d3f59019a1d7087d4
Martin Zurowietz <martin@zurowietz.de>
2016-09-23 13:14:52 +0200

    Update login page info text

--- resources/lang/en/dias.php
+++ resources/lang/en/dias.php
@@ -2,7 +2,7 @@
 
 return array(
 
-	'info' => 'DIAS (or Discol Image Annotation Software) is a browser-based software for the most efficient and effective rapid manual annotation of still images. It focuses generally on underwater images and more specifically on benthic images taken in the manganese nodule fields of the Pacific Ocean.',
+	'info' => 'The BIIGLE Discol Image Annotation Software is a browser-based application for the most efficient and effective rapid manual annotation of still images with a focus on underwater images.',
 	'new_acc' => 'Create an account',
 	'back' => 'back',
 
