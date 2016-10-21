e42f44bdbcf03040c5c4d0fe7493afd7799ff216
Martin Zurowietz <martin@zurowietz.de>
2016-10-21 11:07:26 +0200

    Implement new permissions in annotator settings foldout
    References BiodataMiningGroup/dias-core#47

--- src/resources/views/annotationsSettings.blade.php
+++ src/resources/views/annotationsSettings.blade.php
@@ -1,8 +1,10 @@
-<h4>Example annotations</h4>
+@if ($editMode)
+    <h4>Example annotations</h4>
 
-<div class="settings-foldout__item" data-ng-controller="AteExamplePatchesSettingsController">
-    <div class="btn-group">
-        <button type="button" class="btn btn-default" data-ng-class="{active: shown()}" data-ng-click="show()" title="Show example annotations">show</button>
-        <button type="button" class="btn btn-default" data-ng-class="{active: !shown()}" data-ng-click="hide()" title="Don't show example annotations">hide</button>
+    <div class="settings-foldout__item" data-ng-controller="AteExamplePatchesSettingsController">
+        <div class="btn-group">
+            <button type="button" class="btn btn-default" data-ng-class="{active: shown()}" data-ng-click="show()" title="Show example annotations">show</button>
+            <button type="button" class="btn btn-default" data-ng-class="{active: !shown()}" data-ng-click="hide()" title="Don't show example annotations">hide</button>
+        </div>
     </div>
-</div>
+@endif
--- src/resources/views/annotationsSidebar.blade.php
+++ src/resources/views/annotationsSidebar.blade.php
@@ -1,13 +1,15 @@
-<div class="ate-example-patches" data-ng-controller="AteExamplePatchesController">
-    <div class="patch-container ng-cloak" data-ng-switch="isLoading()" data-ng-if="isEnabled() && hasLabel()">
-        <div class="alert alert-info" data-ng-switch-when="true">
-            Loading example annotations...
-        </div>
-        <div data-ng-switch-default="" data-ng-switch="hasPatches()">
-            <img class="patch" src="{{ asset(config('thumbnails.empty_url')) }}" data-ng-src="{{ url('api/v1/annotations/') }}/@{{ patch }}/patch" data-fallback-src="{{ asset(config('thumbnails.empty_url')) }}" data-ng-switch-when="true" data-ng-repeat="patch in getPatches()" title="Example annotation for label @{{getLabelName()}}">
-            <div class="alert alert-info" data-ng-switch-default="">
-                No example annotations available.
+@if ($editMode)
+    <div class="ate-example-patches" data-ng-controller="AteExamplePatchesController">
+        <div class="patch-container ng-cloak" data-ng-switch="isLoading()" data-ng-if="isEnabled() && hasLabel()">
+            <div class="alert alert-info" data-ng-switch-when="true">
+                Loading example annotations...
+            </div>
+            <div data-ng-switch-default="" data-ng-switch="hasPatches()">
+                <img class="patch" src="{{ asset(config('thumbnails.empty_url')) }}" data-ng-src="{{ url('api/v1/annotations/') }}/@{{ patch }}/patch" data-fallback-src="{{ asset(config('thumbnails.empty_url')) }}" data-ng-switch-when="true" data-ng-repeat="patch in getPatches()" title="Example annotation for label @{{getLabelName()}}">
+                <div class="alert alert-info" data-ng-switch-default="">
+                    No example annotations available.
+                </div>
             </div>
         </div>
     </div>
-</div>
+@endif
