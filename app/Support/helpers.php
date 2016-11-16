bad84a909f0a3f411408b8913c314df555a52c5f
Martin Zurowietz <martin@zurowietz.de>
2016-11-16 09:11:08 +0100

    Implement cachebust_asset helper function
    This function adds a cachebusting query string (filemtime) to the
    asset path. Although using a cachebusting filename is preferred
    over a query string (because some proxies don't recognize the
    query strings for caching in their default configuration) and
    Laravel supports cachebusting filenames out of the box, this
    approach works better with our setup of modules and their own
    assets.
    
    References BiodataMiningGroup/dias-core#35

---
+++ app/Support/helpers.php
@@ -0,0 +1,20 @@
+<?php
+
+if (! function_exists('cachebust_asset')) {
+    /**
+     * Generate an asset path with a cachbusting query string for the application.
+     *
+     * @param  string  $path
+     * @param  bool    $secure
+     * @return string
+     */
+    function cachebust_asset($path, $secure = null)
+    {
+        $publicPath = public_path($path);
+        if (file_exists($publicPath)) {
+            $path .= '?'.filemtime($publicPath);
+        }
+
+        return asset($path, $secure);
+    }
+}
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,10 +4,10 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="icon" type="image/x-icon" href="{{ asset('favicon.ico') }}">
+    <link rel="icon" type="image/x-icon" href="{{ cachebust_asset('favicon.ico') }}">
     <title>@yield('title')</title>
 
-    <link href="{{ asset('assets/styles/main.css') }}" rel="stylesheet">
+    <link href="{{ cachebust_asset('assets/styles/main.css') }}" rel="stylesheet">
     @stack('styles')
 </head>
 <body>
@@ -18,22 +18,22 @@
     @yield('content')
 
     @if (app()->environment('local'))
-        <script src="{{ asset('assets/scripts/vue.js') }}"></script>
+        <script src="{{ cachebust_asset('assets/scripts/vue.js') }}"></script>
     @else
-        <script src="{{ asset('assets/scripts/vue.min.js') }}"></script>
+        <script src="{{ cachebust_asset('assets/scripts/vue.min.js') }}"></script>
     @endif
-    <script src="{{ asset('assets/scripts/vue-resource.min.js') }}"></script>
+    <script src="{{ cachebust_asset('assets/scripts/vue-resource.min.js') }}"></script>
 
     <script type="text/javascript">
         Vue.http.options.root = '{{url('/')}}';
         Vue.http.headers.common['X-CSRF-TOKEN'] = '{{csrf_token()}}';
     </script>
 
-    <script src="{{ asset('assets/scripts/angular.min.js') }}"></script>
-    <script src="{{ asset('assets/scripts/angular-resource.min.js') }}"></script>
-    <script src="{{ asset('assets/scripts/angular-animate.min.js') }}"></script>
-    <script src="{{ asset('assets/scripts/ui-bootstrap-tpls.min.js') }}"></script>
-    <script src="{{ asset('assets/scripts/main.js') }}"></script>
+    <script src="{{ cachebust_asset('assets/scripts/angular.min.js') }}"></script>
+    <script src="{{ cachebust_asset('assets/scripts/angular-resource.min.js') }}"></script>
+    <script src="{{ cachebust_asset('assets/scripts/angular-animate.min.js') }}"></script>
+    <script src="{{ cachebust_asset('assets/scripts/ui-bootstrap-tpls.min.js') }}"></script>
+    <script src="{{ cachebust_asset('assets/scripts/main.js') }}"></script>
     <script type="text/javascript">
         angular.module('dias.api').constant('URL', '{{ url('/') }}');
     </script>
