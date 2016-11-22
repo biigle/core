3009b7e6f5cb95aa4dc726de390e93956f8ee59b
Martin Zurowietz <martin@zurowietz.de>
2016-11-22 13:11:09 +0100

    Implement more efficient method for a fallback thumbnail
    This makes the fallback-src directive obsolete.

--- src/resources/views/annotationsSidebar.blade.php
+++ src/resources/views/annotationsSidebar.blade.php
@@ -5,7 +5,7 @@
                 Loading example annotations...
             </div>
             <div data-ng-switch-default="" data-ng-switch="hasPatches()">
-                <img class="patch" src="{{ asset(config('thumbnails.empty_url')) }}" data-ng-src="{{ url('api/v1/annotations/') }}/@{{ patch }}/patch" data-fallback-src="{{ asset(config('thumbnails.empty_url')) }}" data-ng-switch-when="true" data-ng-repeat="patch in getPatches()" title="Example annotation for label @{{getLabelName()}}">
+                <img class="patch" src="{{ asset(config('thumbnails.empty_url')) }}" data-ng-src="{{ url('api/v1/annotations/') }}/@{{ patch }}/patch" onerror="this.src='{{ asset(config('thumbnails.empty_url')) }}'" data-ng-switch-when="true" data-ng-repeat="patch in getPatches()" title="Example annotation for label @{{getLabelName()}}">
                 <div class="alert alert-info" data-ng-switch-default="">
                     No example annotations available.
                 </div>
