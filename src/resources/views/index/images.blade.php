2f532b0632427e288fab4c1682ef54d6f2589753
Martin Zurowietz <martin@zurowietz.de>
2016-11-22 13:14:34 +0100

    Remove last use of fallback-src directive

--- src/resources/views/index/images.blade.php
+++ src/resources/views/index/images.blade.php
@@ -1,7 +1,7 @@
 <div class="transect__images label-mode" data-ng-controller="ImagesController">
     <figure class="transect-figure ng-cloak" data-ate-figure="" data-ng-repeat="id in getImageIds()" data-ng-class="getClass()">
         <div class ="image-wrapper" title="@{{getTitle()}}" data-ng-click="handleClick($event)">
-            <img src="{{ asset(config('thumbnails.empty_url')) }}" data-ng-src="{{ url('api/v1/annotations/') }}/@{{ id }}/patch" data-fallback-src="{{ asset(config('thumbnails.empty_url')) }}">
+            <img src="{{ asset(config('thumbnails.empty_url')) }}" data-ng-src="{{ url('api/v1/annotations/') }}/@{{ id }}/patch" onerror="this.src='{{ asset(config('thumbnails.empty_url')) }}'">
         </div>
         <div class="changed-label ng-cloak" data-ng-if="isInReLabellingMode() && isChanged()">
             <span class="changed-label-color" data-ng-style="{'background-color': '#' + changedLabel.color}"></span>
