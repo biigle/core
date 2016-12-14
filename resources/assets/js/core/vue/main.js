187e33e2d7b7c53cb9e9fd6df3b2f7275d7e9873
Martin Zurowietz <martin@zurowietz.de>
2016-12-14 11:21:57 +0100

    Implement biigle.$component and change $declare helper functions
    $component works as $declare worked before. Since $declare didn't
    copy the object prototype, declaring things like Vue instances
    (new Vue) resulted in erroneous objects where e.g. obj.$on was
    missing.
    
    Now $declare can be used with anything but the ordering of
    declaration and reference still matters (it's just a helper to
    resolve the namespace).
    
    $component can be used as $declare was intended before since Vue
    component declarations are plain JS objects. This way components
    can be referenced even *before* they are declared.

--- resources/assets/js/core/vue/main.js
+++ resources/assets/js/core/vue/main.js
@@ -13,46 +13,68 @@ biigle = {};
  * @param  {Function} callback Function that instanciates the view model.
  */
 biigle.$viewModel = function (id, callback) {
-    var element = document.getElementById(id);
     window.addEventListener('load', function () {
+        var element = document.getElementById(id);
         if (element) callback(element);
     });
 };
 
 /**
- * Require an object under a specific namespace
+ * Require an object under a specific namespace.
+ * If it doesn't exist yet, it will be created.
  *
- * If it doesn't exist yet, it will be created. This can be used to declare
- * interdependent objects (like Vue components) in arbitrary ordering.
- *
- * @param  String namespace Namespace like 'messages.message'
+ * @param  String|Array namespace Namespace like 'messages.message' or
+ *                                ['messages', 'message']
  * @return Object
  */
 biigle.$require = function (namespace) {
+    namespace = Array.isArray(namespace) ? namespace : namespace.split('.');
     var object = biigle;
-    var namepsace = namespace.split('.');
-    for (var i = 0, j = namepsace.length; i < j; i++) {
-        object[namepsace[i]] = object[namepsace[i]] || {};
-        object = object[namepsace[i]];
+    for (var i = 0, j = namespace.length; i < j; i++) {
+        object[namespace[i]] = object[namespace[i]] || {};
+        object = object[namespace[i]];
     }
 
     return object;
 };
 
+
 /**
- * Register a (local Vue) component that can be biigle.$require()'d
- *
- * This works for any object, not just Vue components.
+ * Declare an object in the namespace even if the namespace doesn't exist yet
  *
  * @param  String namespace Namespace like 'messages.message' where the last part
  *                          should be the name of the component. All components will be
  *                          added to the global biigle object.
- * @param  Object declaration (Vue) component declaration. If it is a function, it will
- *                            be called and expected to return the declaration.
- * @return Object Reference to the component object returned by biigle.$require
+ * @param  Object object Object to declare. If it is a function, the function will be
+ *                       called and expected to return the object.
+ * @return Object
  */
-biigle.$declare = function (namespace, declaration) {
+biigle.$declare = function (namespace, object) {
+    namespace = namespace.split('.');
+    var name = namespace.pop();
+    var parent = biigle.$require(namespace);
+    parent[name] = (typeof object === 'function') ? object() : object;
+
+    return object;
+};
+
+/**
+ * Declare a Vue Component in a way that biigle.$require() is resolved correctly even
+ * if it is called *before* the Component was declared.
+ *
+ * ATTENTION: Only use this function for Vue Component declarations (plain objects).
+ * Not for Vue instances (new Vue), for example, because the prototype functions will
+ * not be resolved!
+ *
+ * @param  String namespace Namespace like 'messages.message' where the last part
+ *                          should be the name of the component. All components will be
+ *                          added to the global biigle object.
+ * @param  Object declaration Vue Component declaration object
+ * @return Object
+ */
+biigle.$component = function (namespace, declaration) {
     var object = biigle.$require(namespace);
+
     if (typeof declaration === 'function') {
         declaration = declaration();
     }
