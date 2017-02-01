270bcdf3e06c67d0346e2050dd465a07be11fcab
Martin Zurowietz <martin@zurowietz.de>
2017-02-01 09:43:25 +0100

    Update volume list with new mixins, components, resources

--- src/resources/assets/js/vue/showVolumeList.js
+++ src/resources/assets/js/vue/showVolumeList.js
@@ -2,73 +2,59 @@
  * The volume list on the project show view.
  */
 biigle.$viewModel('projects-show-volume-list', function (element) {
-    var projectVolumes = biigle.$require('api.projectVolumes');
+    var projectsApi = biigle.$require('api.projects');
     var attachableVolumes = biigle.$require('api.attachableVolumes');
-    var messageStore = biigle.$require('messages.store');
+    var messages = biigle.$require('messages.store');
 
     new Vue({
         el: element,
+        mixins: [
+            biigle.$require('core.mixins.loader'),
+            biigle.$require('core.mixins.editor'),
+        ],
         data: {
             project: biigle.$require('projects.project'),
             volumes: biigle.$require('projects.volumes'),
-            editing: false,
-            loading: false,
-            fetchedAttachableVolumes: false,
             attachableVolumes: [],
-            // emplate for the attachable volumes typeahead
-            template: '<span v-text="item.name"></span>'
         },
         components: {
             volumeThumbnail: biigle.$require('projects.components.volumeThumbnail'),
-            typeahead: VueStrap.typeahead
+            typeahead: biigle.$require('core.components.typeahead')
         },
         methods: {
             removeVolume: function (id) {
                 var self = this;
                 this.startLoading();
-                projectVolumes.detach({pid: this.project.id, id: id})
-                    .then(function (response) {
-                        self.spliceVolume(id);
+                projectsApi.detachVolume({id: this.project.id, volume_id: id})
+                    .then(function () {
+                        self.volumeRemoved(id);
                     }, function (response) {
                         if (response.status === 400) {
                             if (confirm('The volume you are about to remove belongs only to this project and will be deleted. Are you sure you want to delete this volume?')) {
                                 self.forceRemoveVolume(id);
                             }
                         } else {
-                            messageStore.handleErrorResponse(response);
+                            messages.handleErrorResponse(response);
                         }
                     })
-                    .finally(this.quitLoading);
+                    .finally(this.finishLoading);
             },
             forceRemoveVolume: function (id) {
                 var self = this;
                 this.startLoading();
-                projectVolumes.detach({pid: this.project.id, id: id}, {force: true})
-                    .then(function (response) {
-                        self.spliceVolume(id);
-                    }, messageStore.handleErrorResponse)
-                    .finally(this.quitLoading);
+                projectsApi.detachVolume({id: this.project.id, volume_id: id}, {force: true})
+                    .then(function () {
+                        self.volumeRemoved(id);
+                    }, messages.handleErrorResponse)
+                    .finally(this.finishLoading);
             },
-            spliceVolume: function (id) {
+            volumeRemoved: function (id) {
                 for (var i = this.volumes.length - 1; i >= 0; i--) {
                     if (this.volumes[i].id === id) {
                         this.attachableVolumes.unshift(this.volumes[i]);
                         this.volumes.splice(i, 1);
                     }
                 }
-            },
-            edit: function () {
-                this.editing = !this.editing;
-                if (!this.fetchedAttachableVolumes) {
-                    this.fetchedAttachableVolumes = true;
-                    this.fetchAttachableVolumes();
-                }
-            },
-            startLoading: function () {
-                this.loading = true;
-            },
-            quitLoading: function () {
-                this.loading = false;
             },
             hasVolume: function (id) {
                 for (var i = this.volumes.length - 1; i >= 0; i--) {
@@ -79,30 +65,35 @@ biigle.$viewModel('projects-show-volume-list', function (element) {
 
                 return false;
             },
-            attachVolume: function (volume, typeahead) {
-                typeahead.reset();
+            attachVolume: function (volume) {
                 if (volume && !this.hasVolume(volume.id)) {
                     var self = this;
                     this.startLoading();
-                    projectVolumes.attach({pid: this.project.id, id: volume.id}, {})
+                    projectsApi.attachVolume({id: this.project.id, volume_id: volume.id}, {})
                         .then(function () {
-                            self.volumes.unshift(volume);
-                            for (var i = self.attachableVolumes.length - 1; i >= 0; i--) {
-                                if (self.attachableVolumes[i].id === volume.id) {
-                                    self.attachableVolumes.splice(i, 1);
-                                }
-                            }
-                        }, messageStore.handleErrorResponse)
-                        .finally(this.quitLoading);
+                            self.volumeAttached(volume);
+                        }, messages.handleErrorResponse)
+                        .finally(this.finishLoading);
+                }
+            },
+            volumeAttached: function (volume) {
+                this.volumes.unshift(volume);
+                for (var i = this.attachableVolumes.length - 1; i >= 0; i--) {
+                    if (this.attachableVolumes[i].id === volume.id) {
+                        this.attachableVolumes.splice(i, 1);
+                    }
                 }
             },
             fetchAttachableVolumes: function () {
-                var self = this;
                 attachableVolumes.get({id: this.project.id})
-                    .then(function (response) {
-                        self.attachableVolumes = response.data;
-                    }, messageStore.handleErrorResponse);
-            }
-        }
+                    .then(this.attachableVolumesFetched, messages.handleErrorResponse);
+            },
+            attachableVolumesFetched: function (response) {
+                this.attachableVolumes = response.data;
+            },
+        },
+        created: function () {
+            this.$once('editing.start', this.fetchAttachableVolumes);
+        },
     });
 });
