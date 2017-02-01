ed9b9f96284696e516680413560831330fb8704d
Martin Zurowietz <martin@zurowietz.de>
2017-02-01 09:27:44 +0100

    Merge projectVolumes resource with projects resource

--- resources/assets/js/core/vue/api/projects.js
+++ resources/assets/js/core/vue/api/projects.js
@@ -18,11 +18,44 @@
  * Delete a project:
  * resource.delete({id: 1}).then(...);
  *
+ * Get all volumes attached to a project:
+ * resource.queryVolumes({id: 1}).then(...);
+ *
+ * Create a new volume for for a project:
+ * resource.saveVolume({id: 1}, {
+ *     name: "volume 1",
+ *     url: "/vol/volumes/1",
+ *     media_type_id: 1,
+ *     images: ["1.jpg", "2.jpg"]
+ * }).then(...);
+ *
+ * Attach an existing volume to a project:
+ * resource.attachVolume({id: 1, volume_id: 12}).then(...);
+ *
+ * Detach a volume from a project:
+ * resource.detachVolume({id: 1, volume_id: 12}).then(...);
+ *
  * @type {Vue.resource}
  */
 biigle.$declare('api.projects', Vue.resource('/api/v1/projects{/id}', {}, {
     query: {
         // a user can only query their own projects
         url: '/api/v1/projects/my',
+    },
+    queryVolumes: {
+        method: 'GET',
+        url: '/api/v1/projects{/id}/volumes',
+    },
+    saveVolume: {
+        method: 'POST',
+        url: '/api/v1/projects{/id}/volumes',
+    },
+    attachVolume: {
+        method: 'POST',
+        url: '/api/v1/projects{/id}/volumes{/volume_id}',
+    },
+    detachVolume: {
+        method: 'DELETE',
+        url: '/api/v1/projects{/id}/volumes{/volume_id}',
     }
 }));
