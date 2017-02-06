d7f7079b7812b0c486f8d1e2078fd7d03571d946
Martin Zurowietz <martin@zurowietz.de>
2017-02-06 13:25:39 +0100

    Fix gloabl label tree message

--- src/resources/views/show/members.blade.php
+++ src/resources/views/show/members.blade.php
@@ -1,1 +1,3 @@
-<members-panel id="label-trees-members" :members="members" :roles="roles" :default-role="defaultRole" :own-id="userId" :loading="loading" v-on:attach="attachMember" v-on:update="updateMember" v-on:remove="removeMember"></members-panel>
+<members-panel id="label-trees-members" :members="members" :roles="roles" :default-role="defaultRole" :own-id="userId" :loading="loading" v-on:attach="attachMember" v-on:update="updateMember" v-on:remove="removeMember">
+    <span>This tree has no members and therefore is a global label tree.<br>Add members to make it an ordinary label tree.</span>
+</members-panel>
