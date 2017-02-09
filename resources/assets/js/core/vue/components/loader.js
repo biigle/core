91acd1052ed1c3b8fa02df518e56be1885fcc269
Martin Zurowietz <martin@zurowietz.de>
2017-02-09 10:08:46 +0100

    Implement fancy loader variant

--- resources/assets/js/core/vue/components/loader.js
+++ resources/assets/js/core/vue/components/loader.js
@@ -4,11 +4,15 @@
  * @type {Object}
  */
 biigle.$component('core.components.loader', {
-    template: '<span class="loader" :class="{\'loader--active\': active}"></span>',
+    template: '<span class="loader" :class="{\'loader--active\': active, \'loader--fancy\': fancy}"></span>',
     props: {
         active: {
             type: Boolean,
             required: true,
+        },
+        fancy: {
+            type: Boolean,
+            default: false,
         }
     }
 });
