0fbc16b7201058181ef1aa59a328f3628758945a
Martin Zurowietz <martin@zurowietz.de>
2017-02-09 10:41:58 +0100

    Add loader-block component to loader mixin

--- resources/assets/js/core/vue/mixins/loader.js
+++ resources/assets/js/core/vue/mixins/loader.js
@@ -6,20 +6,24 @@
 biigle.$component('core.mixins.loader', {
     components: {
         loader: biigle.$require('core.components.loader'),
+        loaderBlock: biigle.$require('core.components.loaderBlock'),
     },
     data: function () {
         return {
             loading: false,
         };
     },
+    watch: {
+        loading: function (loading) {
+            this.$emit('loading', loading);
+        },
+    },
     methods: {
         startLoading: function () {
             this.loading = true;
-            this.$emit('loading', true);
         },
         finishLoading: function () {
             this.loading = false;
-            this.$emit('loading', false);
         },
     },
 });
