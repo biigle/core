f3695ef115c4443555adfbe19f3cd5feb1eaf3ad
Martin Zurowietz <martin@zurowietz.de>
2017-04-03 09:42:14 +0200

    Fix project largo view with image-grid of volumes module

--- src/resources/views/project.blade.php
+++ src/resources/views/project.blade.php
@@ -4,6 +4,7 @@
 
 @push('scripts')
     <script src="{{ cachebust_asset('vendor/label-trees/scripts/main.js') }}"></script>
+    <script src="{{ cachebust_asset('vendor/volumes/scripts/main.js') }}"></script>
     <script src="{{ cachebust_asset('vendor/largo/scripts/main.js') }}"></script>
     <script type="text/javascript">
         biigle.$declare('largo.projectId', {!! $project->id !!});
@@ -14,13 +15,13 @@
 
 @push('styles')
     <link href="{{ cachebust_asset('vendor/label-trees/styles/main.css') }}" rel="stylesheet">
+    <link href="{{ cachebust_asset('vendor/volumes/styles/main.css') }}" rel="stylesheet">
     <link href="{{ cachebust_asset('vendor/largo/styles/main.css') }}" rel="stylesheet">
 @endpush
 
 @section('navbar')
 <div class="navbar-text navbar-largo-breadcrumbs">
-    <a href="{{route('project', $project->id)}}" class="navbar-link" title="Show project {{$project->name}}">{{$project->name}}</a>
-    / <span id="largo-title">Largo / <strong v-if="isInDismissStep">dismiss existing annotations</strong><strong v-cloak v-else>re-label dismissed annotations</strong> <small>(<span v-text="shownCount">0</span>&nbsp;annotations)</small></span>
+    <a href="{{route('project', $project->id)}}" class="navbar-link" title="Show project {{$project->name}}">{{$project->name}}</a> / <span id="largo-title">Largo / <strong v-if="isInDismissStep">dismiss existing annotations</strong><strong v-cloak v-else>re-label dismissed annotations</strong> <small>(<span v-text="shownCount">0</span>&nbsp;annotations)</small></span>
 </div>
 @endsection
 
