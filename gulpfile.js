39b2a883ef5173adf6fa2ab07bc48cc43eeb2c47
Martin Zurowietz <martin@zurowietz.de>
2017-04-18 13:53:35 +0200

    Simplify directory structure

--- gulpfile.js
+++ gulpfile.js
@@ -23,14 +23,14 @@ gulp.task('sass-dashboard', function () {
 gulp.task('sass', ['sass-main', 'sass-edit', 'sass-dashboard']);
 
 gulp.task('js-main', function (cb) {
-    h.angular('volumes/**/*.js', 'main.js', cb);
+    h.angular('**/*.js', 'main.js', cb);
 });
 
 gulp.task('js', ['js-main']);
 
 gulp.task('watch', function () {
     gulp.watch(h.paths.sass + '**/*.scss', ['sass']);
-    gulp.watch(h.paths.js + 'volumes/**/*.js', ['js-main']);
+    gulp.watch(h.paths.js + '**/*.js', ['js-main']);
     gulp.watch(h.paths.public + '**/*', publish);
 });
 
