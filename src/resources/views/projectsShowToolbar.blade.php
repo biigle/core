40dbc4582b5522f85b9f08c2a8972ed13cf9e58a
Martin Zurowietz <martin@zurowietz.de>
2017-04-18 16:12:53 +0200

    Hide project largo button for project guests

--- src/resources/views/projectsShowToolbar.blade.php
+++ src/resources/views/projectsShowToolbar.blade.php
@@ -1,1 +1,3 @@
-<a href="{{route('projectsLargo', $project->id)}}" class="btn btn-default" title="Perform Largo re-evaluation of annotations for this project"><span class="glyphicon glyphicon-check" aria-hidden="true"></span> Largo re-evaluation</a>
+@can('edit-in', $project)
+    <a href="{{route('projectsLargo', $project->id)}}" class="btn btn-default" title="Perform Largo re-evaluation of annotations for this project"><span class="glyphicon glyphicon-check" aria-hidden="true"></span> Largo re-evaluation</a>
+@endcan
