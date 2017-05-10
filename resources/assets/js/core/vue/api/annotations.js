a64182245ec1a248dfdbd72a13eb18beaf58a2ef
Martin Zurowietz <martin@zurowietz.de>
2017-05-10 11:07:16 +0200

    Add attachLabel and detachLabel endpoints to annotations resource

--- resources/assets/js/core/vue/api/annotations.js
+++ resources/assets/js/core/vue/api/annotations.js
@@ -9,6 +9,22 @@
  * Delete an annotation:
  * resource.delete({id: 1}).then(...);
  *
+ * Attach a new label to an annotation:
+ * resource.attachLabel({id: 1}, {label_id: 1, confidence: 0.5}).then(...);
+ *
+ * Detach a label from an annotation:
+ * resource.detachLabel({annotation_label_id: id}).then(...);
+ * Note that the annotation label ID is required for this and not the annotation ID!
+ *
  * @type {Vue.resource}
  */
-biigle.$declare('api.annotations', Vue.resource('api/v1/annotations{/id}'));
+biigle.$declare('api.annotations', Vue.resource('api/v1/annotations{/id}', {}, {
+    attachLabel: {
+        method: 'POST',
+        url: 'api/v1/annotations{/id}/labels',
+    },
+    detachLabel: {
+        method: 'DELETE',
+        url: 'api/v1/annotation-labels{/annotation_label_id}',
+    },
+}));
