094479b9aec96e6ebfd82e517fe1720760491b18
Martin Zurowietz <martin@zurowietz.de>
2017-05-17 16:08:34 +0200

    Implement "seen all images" message

--- src/resources/assets/js/vue/annotationsNavbar.js
+++ src/resources/assets/js/vue/annotationsNavbar.js
@@ -11,8 +11,23 @@ biigle.$viewModel('annotations-navbar', function (element) {
             // loaded until the image id has been set. Otherwise the filename would
             // vanish and appear again what we don't want.
             defaultFilename: element.innerHTML,
+            imageIdsToSee: biigle.$require('annotations.imagesIds').slice(),
         },
         watch: {
+            currentImageId: function (id) {
+                var ids = this.imageIdsToSee;
+                for (var i = ids.length - 1; i >= 0; i--) {
+                    if (ids[i] === id) {
+                        ids.splice(i, 1);
+                        break;
+                    }
+                }
+            },
+            imageIdsToSee: function (ids) {
+                if (ids.length === 0) {
+                    biigle.$require('messages.store').info('You have now seen all images of this batch.');
+                }
+            },
             currentImageFilename: function (filename) {
                 document.title = 'Annotate ' + filename;
             },
