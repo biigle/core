c61ca302864ac9ff72aef050279c2a4c00d98398
Martin Zurowietz <martin@zurowietz.de>
2017-05-19 13:17:59 +0200

    Make annotator settings tab label ucfirst

--- src/resources/views/annotationsSettingsTab.blade.php
+++ src/resources/views/annotationsSettingsTab.blade.php
@@ -1,7 +1,7 @@
 @can ('add-annotations', $volume)
     <component :is="plugins.exampleAnnotations" :settings="settings" inline-template>
         <div class="settings-tab__section">
-            <label>Example annotations</label>
+            <label>Example Annotations</label>
             <div class="btn-group">
                 <button type="button" class="btn btn-default" :class="{active: isShown}" v-on:click="show" title="Show example annotations">show</button>
                 <button type="button" class="btn btn-default" :class="{active: !isShown}" v-on:click="hide" title="Don't show example annotations">hide</button>
