c2a96d940a1c0ecc255382b9e5bf820b60ee04b5
Martin Zurowietz <martin@zurowietz.de>
2017-05-22 10:44:12 +0200

    Update gulp-helpers, angular command is no longer required

--- gulpfile.js
+++ gulpfile.js
@@ -8,25 +8,20 @@ h.paths.sass = 'src/resources/assets/sass/';
 h.paths.js = 'src/resources/assets/js/';
 h.paths.public = 'src/public/assets/';
 
-gulp.task('sass', function () {
-    // h.sass('main.scss', 'main.css');
-});
-
 gulp.task('js-main', function (cb) {
-    h.angular('export/**/*.js', 'main.js', cb);
+    h.js('export/**/*.js', 'main.js', cb);
 });
 
 gulp.task('js-annotations', function (cb) {
-    h.angular('annotations/**/*.js', 'annotations.js', cb);
+    h.js('annotations/**/*.js', 'annotations.js', cb);
 });
 
 gulp.task('js', ['js-main', 'js-annotations']);
 
 gulp.task('watch', function () {
-    gulp.watch(h.paths.sass + '**/*.scss', ['sass']);
     gulp.watch(h.paths.js + 'export/**/*.js', ['js-main']);
     gulp.watch(h.paths.js + 'annotations/**/*.js', ['js-annotations']);
     gulp.watch(h.paths.public + '**/*', publish);
 });
 
-gulp.task('default', ['sass', 'js'], publish)
+gulp.task('default', ['js'], publish)
