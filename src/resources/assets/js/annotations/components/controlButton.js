3d0f98e0eb100a6ac6cd1bf52fe1e05cf042d4e5
Martin Zurowietz <martin@zurowietz.de>
2017-05-30 16:16:42 +0200

    Implement nestable control button components
    If a control button contains one or more control buttons, a new
    toolbar with these buttons will appear above the parent button on
    mouseenter.

--- src/resources/assets/js/annotations/components/controlButton.js
+++ src/resources/assets/js/annotations/components/controlButton.js
@@ -4,7 +4,12 @@
  * @type {Object}
  */
 biigle.$component('annotations.components.controlButton', {
-    template: '<button class="control-button btn" :title="title" :class="buttonClass" @click="handleClick"><i :class="iconClass" aria-hidden="true"></i></button>',
+    template: '<span class="control-button btn" :title="title" :class="classObject" @click="handleClick" @mouseenter="handleMouseEnter" @mouseleave="handleMouseLeave">' +
+        '<i :class="iconClass" aria-hidden="true"></i>' +
+        '<span v-if="hasSubControls" @click.stop class="control-button__sub-controls btn-group">' +
+            '<slot></slot>' +
+        '</span>' +
+    '</span>',
     props: {
         title: {
             type: String,
@@ -19,10 +24,18 @@ biigle.$component('annotations.components.controlButton', {
             default: false,
         },
     },
+    data: function () {
+        return {
+            mouseOver: false,
+            timeout: null,
+            activeSubControls: 0,
+        };
+    },
     computed: {
-        buttonClass: function () {
+        classObject: function () {
             return {
                 active: this.active,
+                'control-button--open': this.showSubControls,
             };
         },
         iconClass: function () {
@@ -34,10 +47,45 @@ biigle.$component('annotations.components.controlButton', {
 
             return 'icon icon-white ' + this.icon;
         },
+        hasSubControls: function () {
+            return this.$slots.hasOwnProperty('default');
+        },
+        showSubControls: function () {
+            return this.mouseOver || this.hasActiveSubControl;
+        },
+        hasActiveSubControl: function () {
+            return this.activeSubControls > 0;
+        },
     },
     methods: {
         handleClick: function () {
             this.$emit('click');
-       },
+        },
+        handleMouseEnter: function () {
+            this.mouseOver = true;
+            window.clearTimeout(this.timeout);
+        },
+        handleMouseLeave: function () {
+            var self = this;
+            window.clearTimeout(this.timeout);
+            this.timeout = window.setTimeout(function () {
+                self.mouseOver = false;
+            }, 200);
+        },
+        updateActiveSubControls: function (active) {
+            if (active) {
+                this.activeSubControls++;
+            } else {
+                this.activeSubControls--;
+            }
+        }
+    },
+    watch: {
+        active: function (active) {
+            this.$parent.$emit('control-button-active', active);
+        },
+    },
+    created: function () {
+        this.$on('control-button-active', this.updateActiveSubControls);
     },
 });
