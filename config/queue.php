24fd11b38fcad68c24b0fd3998c5a95308db33a4
Martin Zurowietz <martin@zurowietz.de>
2017-06-06 14:40:37 +0200

    Add a 'high' priority queue to the database driver
    Closes BiodataMiningGroup/biigle-core#49

--- config/queue.php
+++ config/queue.php
@@ -37,7 +37,7 @@ return [
         'database' => [
             'driver' => 'database',
             'table' => 'jobs',
-            'queue' => 'default',
+            'queue' => 'high,default',
             'retry_after' => 90,
         ],
 
