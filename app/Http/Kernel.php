c1556e94a679685b9d7b03c497f6d1ffde24a8a8
Martin Zurowietz <martin@zurowietz.de>
2017-07-03 10:04:36 +0200

    Add api middleware group again

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -33,10 +33,10 @@ class Kernel extends HttpKernel
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
 
-        // 'api' => [
-        //     'throttle:60,1',
-        //     'bindings',
-        // ],
+        'api' => [
+            'throttle:60,1',
+            'bindings',
+        ],
     ];
 
     /**
