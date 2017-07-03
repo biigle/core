3f2271c9ca62ba5ca7a8fe285375d33c039ade5d
Martin Zurowietz <martin@zurowietz.de>
2017-07-03 09:57:44 +0200

    Add test for UpdateUserActivity middleware

---
+++ tests/php/Http/Middleware/UpdateUserActivityTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Biigle\Tests\Http\Middleware;
+
+use ApiTestCase;
+
+class UpdateUserActivityTest extends ApiTestCase
+{
+    public function testTerminate()
+    {
+        $this->assertNull($this->user()->login_at);
+        $this->be($this->user());
+        $this->visit('/');
+        $this->assertNotNull($this->user()->fresh()->login_at);
+    }
+}
