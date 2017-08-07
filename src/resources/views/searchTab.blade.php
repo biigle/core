307d9c37f8e30dfd68fd29353d4e0f437ca37e07
Martin Zurowietz <martin@zurowietz.de>
2017-08-07 13:50:58 +0200

    Use the readable_number helper for search result count

--- src/resources/views/searchTab.blade.php
+++ src/resources/views/searchTab.blade.php
@@ -1,3 +1,3 @@
 <li role="presentation" @if(!$type || $type === 'projects') class="active"@endif>
-    <a href="{{route('search', ['q' => $query, 't' => 'projects'])}}">Projects <span class="badge">{{$projectResultCount}}</span></a>
+    <a href="{{route('search', ['q' => $query, 't' => 'projects'])}}">Projects <span class="badge">{{readable_number($projectResultCount)}}</span></a>
 </li>
