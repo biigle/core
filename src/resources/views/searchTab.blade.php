91f643f132dfcb95fea451a5b72eee2cb5151479
Martin Zurowietz <martin@zurowietz.de>
2017-08-07 13:50:23 +0200

    Use the readable_number helper for search result count

--- src/resources/views/searchTab.blade.php
+++ src/resources/views/searchTab.blade.php
@@ -1,3 +1,3 @@
 <li role="presentation" @if($type === 'label-trees') class="active"@endif>
-    <a href="{{route('search', ['q' => $query, 't' => 'label-trees'])}}">Label Trees <span class="badge">{{$labelTreeResultCount}}</span></a>
+    <a href="{{route('search', ['q' => $query, 't' => 'label-trees'])}}">Label Trees <span class="badge">{{readable_number($labelTreeResultCount)}}</span></a>
 </li>
