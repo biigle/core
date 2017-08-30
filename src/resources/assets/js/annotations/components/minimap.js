9a36d38ab5ea887ccbdd82770c97b364763158a9
Martin Zurowietz <martin@zurowietz.de>
2017-08-30 14:49:32 +0200

    Fix minimap component for tiled images

--- src/resources/assets/js/annotations/components/minimap.js
+++ src/resources/assets/js/annotations/components/minimap.js
@@ -28,15 +28,6 @@ biigle.$component('annotations.components.minimap', function () {
                 required: true,
             },
         },
-        methods: {
-            // Move the viewport rectangle on the minimap.
-            refreshViewport: function () {
-                viewport.setGeometry(ol.geom.Polygon.fromExtent(mapView.calculateExtent(mapSize)));
-            },
-            dragViewport: function (e) {
-                mapView.setCenter(e.coordinate);
-            },
-        },
         computed: {
             // Width and height are only evaluated once on initialization. They will be
             // used to calculate the actual minimap size based on the image aspect ratio.
@@ -47,18 +38,39 @@ biigle.$component('annotations.components.minimap', function () {
                 return this.$el.clientHeight;
             },
         },
+        methods: {
+            // Move the viewport rectangle on the minimap.
+            refreshViewport: function () {
+                viewport.setGeometry(ol.geom.Polygon.fromExtent(mapView.calculateExtent(mapSize)));
+            },
+            dragViewport: function (e) {
+                mapView.setCenter(e.coordinate);
+            },
+            refreshImageLayer: function (e) {
+                // Set or refresh the layer that displays the image. This is done after
+                // the minimap element was created. The annotationCanvas can display
+                // either a regular image or a tiled image. If the type changes we have
+                // to update the layer here, too.
+                var layers = minimap.getLayers();
+                if (layers.getLength() > 1) {
+                    layers.setAt(0, e.target.item(e.target.getLength() - 1));
+                } else {
+                    layers.insertAt(0, e.target.item(e.target.getLength() - 1));
+                }
+            },
+        },
         created: function () {
             var map = biigle.$require('annotations.stores.map');
             mapSize = map.getSize();
             mapView = map.getView();
 
-            // Todo maybe listen to change:layerGroup? Image layer is not present
-            // from the start any more.
-            minimap.addLayer(map.getLayers().item(0));
+            // Add the viewport layer now. Add the image layer later when it was added
+            // to the map.
             minimap.addLayer(new ol.layer.Vector({
                 source: viewportSource,
                 style: biigle.$require('annotations.stores.styles').viewport
             }));
+            map.getLayers().on('add', this.refreshImageLayer);
 
             map.on('postcompose', this.refreshViewport);
             map.on('change:size', function () {
@@ -74,10 +86,16 @@ biigle.$component('annotations.components.minimap', function () {
         watch: {
             // Refresh the view if the extent (i.e. image size) changed.
             extent: function (extent) {
+                var imageWidth = extent[2];
+                // If extent[3] is 0 then a tiled image is displayed. For this the image
+                // height is -extent[1]. This is due to the differences between a Zoomify
+                // and an Image source.
+                var imageHeight = extent[3] || -extent[1];
+
                 // Calculate resolution that fits the image into the minimap element.
                 var resolution = Math.max(
-                    extent[2] / this.intendedWidth,
-                    extent[3] / this.intendedHeight
+                    imageWidth / this.intendedWidth,
+                    imageHeight / this.intendedHeight
                 );
                 minimap.setView(new ol.View({
                     projection: this.projection,
@@ -87,8 +105,8 @@ biigle.$component('annotations.components.minimap', function () {
 
                 // Update the minimap element size so it has the same dimensions than the
                 // image displayed by OpenLayers.
-                this.$el.style.width = Math.round(extent[2] / resolution) + 'px';
-                this.$el.style.height = Math.round(extent[3] / resolution) + 'px';
+                this.$el.style.width = Math.round(imageWidth / resolution) + 'px';
+                this.$el.style.height = Math.round(imageHeight / resolution) + 'px';
                 minimap.updateSize();
             },
         },
