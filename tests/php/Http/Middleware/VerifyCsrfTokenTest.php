2ce54f953f15dd55d3d05637827e9093e0810db0
Martin Zurowietz <martin@zurowietz.de>
2017-09-22 11:33:41 +0200

    Implement test for VerifyCsrfToken middleware
    Closes BiodataMiningGroup/biigle-core#107

---
+++ tests/php/Http/Middleware/VerifyCsrfTokenTest.php
@@ -0,0 +1,60 @@
+<?php
+
+namespace Biigle\Tests\Http\Middleware;
+
+use App;
+use ApiTestCase;
+use Biigle\Tests\ApiTokenTest;
+use Biigle\Http\Middleware\VerifyCsrfToken;
+
+class VerifyCsrfTokenTest extends ApiTestCase
+{
+    public function setUp()
+    {
+        parent::setUp();
+
+        // We need this because it is disabled in unit tests by default.
+        App::bind(VerifyCsrfToken::class, VerifyCsrfTokenStub::class);
+
+        $this->token = ApiTokenTest::create([
+            // 'test_token'
+            'hash' => '$2y$10$.rR7YrU9K2ZR4xgPbKs1x.AGUUKIA733CT72eC6I2piTiPY59V7.O',
+            'owner_id' => $this->globalAdmin()->id,
+        ]);
+    }
+
+    public function testHandleWrongToken()
+    {
+        $this->call('PUT', '/api/v1/users/'.$this->guest()->id, [], [], [], [
+                'PHP_AUTH_USER' => $this->globalAdmin()->email,
+                'PHP_AUTH_PW' => 'wrong_token',
+            ])
+            ->assertStatus(403);
+    }
+
+    public function testHandleWrongEmail()
+    {
+        $this->call('PUT', '/api/v1/users/'.$this->guest()->id, [], [], [], [
+                'PHP_AUTH_USER' => 'wrong@email.com',
+                'PHP_AUTH_PW' => 'test_token',
+            ])
+            ->assertStatus(403);
+    }
+
+    public function testHandleCorrect()
+    {
+        $this->call('PUT', '/api/v1/users/'.$this->guest()->id, [], [], [], [
+                'PHP_AUTH_USER' => $this->globalAdmin()->email,
+                'PHP_AUTH_PW' => 'test_token',
+            ])
+            ->assertStatus(200);
+    }
+}
+
+class VerifyCsrfTokenStub extends VerifyCsrfToken
+{
+    public function runningUnitTests()
+    {
+        return false;
+    }
+}
