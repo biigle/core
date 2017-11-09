fb6da39c32ee2a9317e65a37daa3cbf1f863367c
Martin Zurowietz <martin@zurowietz.de>
2017-11-09 13:10:44 +0100

    Keep users logged in by default
    Closes BiodataMiningGroup/biigle-core#110

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -21,6 +21,7 @@ class LoginController extends Controller
 
     use AuthenticatesUsers {
         credentials as protected baseCredentials;
+        attemptLogin as protected baseAttemptLogin;
     }
 
     /**
@@ -54,5 +55,19 @@ class LoginController extends Controller
         $request->merge([$username => strtolower($request->input($username))]);
 
         return $this->baseCredentials($request);
+    }
+
+    /**
+     * Attempt to log the user into the application.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return bool
+     */
+    protected function attemptLogin(Request $request)
+    {
+        // Users should be "remembered" by default until they log out.
+        $request->merge(['remember' => true]);
+
+        return $this->baseAttemptLogin($request);
     }
 }
