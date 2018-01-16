314da90b263466856eb801533df7e45deec4625c
Martin Zurowietz <martin@zurowietz.de>
2018-01-16 10:27:03 +0100

    Extend user admin show view with more detailed project information
    Closes #23

--- src/resources/views/adminShowUser.blade.php
+++ src/resources/views/adminShowUser.blade.php
@@ -1,9 +1,32 @@
 <div class="col-xs-12">
-    <p>
-        @if ($projectCount > 0)
-            Created <strong>{{$projectCount}}</strong> {{$projectCount === 1 ? 'project' : 'projects'}} ({{ $projectPercent}} %).
-        @else
-            Created no projects yet.
-        @endif
-    </p>
+    <div class="row">
+        <div class="col-xs-6">
+            <p>
+                @if ($creatorCount > 0)
+                    Created <strong>{{$creatorCount}}</strong> {{$creatorCount === 1 ? 'project' : 'projects'}} ({{$creatorPercent}} %).
+                @else
+                    Created no projects yet.
+                @endif
+            </p>
+            <ul>
+                @foreach ($creatorProjects as $project)
+                    <li><a href="{{route('project', $project->id)}}">{{$project->name}}</a></li>
+                @endforeach
+            </ul>
+        </div>
+        <div class="col-xs-6">
+            <p>
+                @if ($memberCount > 0)
+                    Is member in <strong>{{$memberCount}}</strong> {{$memberCount === 1 ? 'project' : 'projects'}} ({{$memberPercent}} %).
+                @else
+                    Is member on no projects.
+                @endif
+            </p>
+            <ul>
+                @foreach ($memberProjects as $project)
+                    <li><a href="{{route('project', $project->id)}}">{{$project->name}}</a></li>
+                @endforeach
+            </ul>
+        </div>
+    </div>
 </div>
