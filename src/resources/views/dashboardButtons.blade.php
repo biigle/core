625c6ea08f010ce47122b33ebbbe61417e785691
Martin Zurowietz <martin@zurowietz.de>
2018-01-17 13:52:59 +0100

    Improve dashboard mixins

--- src/resources/views/dashboardButtons.blade.php
+++ src/resources/views/dashboardButtons.blade.php
@@ -1,3 +1,3 @@
 <a href="{{route('label-trees-create')}}" class="btn btn-default btn-block" title="Create a new label tree">
-    <i class="fa fa-tags"></i> New Label Tree
+    <i class="fa fa-tags"></i> Create Label Tree
 </a>
