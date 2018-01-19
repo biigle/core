47af3aff9221124f5519187c60e88f00da546c28
Martin Zurowietz <martin@zurowietz.de>
2018-01-19 15:38:40 +0100

    WIP Add linkt to annotation catalog to label trees show view
    References #37

--- src/LargoServiceProvider.php
+++ src/LargoServiceProvider.php
@@ -50,6 +50,7 @@ class LargoServiceProvider extends ServiceProvider
                 'projectsShowToolbar',
                 'annotationsManualSidebarSettings',
                 'annotationsManualSidebarLabelTrees',
+                'labelTreesShowToolbar',
                 'manualTutorial',
             ],
         ]);
---
+++ src/resources/views/labelTreesShowToolbar.blade.php
@@ -0,0 +1,1 @@
+<a class="btn btn-default" href="#" title="Browse all annotations with labels of this label tree"><i class="fa fa-book"></i> Annotation catalog</a>
