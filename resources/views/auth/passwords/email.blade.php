480fab1c7f71b67234c72d29dcd8b970a6ce2ed8
Martin Zurowietz <martin@zurowietz.de>
2018-03-15 10:04:16 +0100

    Disable password resets in offline mode
    Closes #145

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -8,7 +8,11 @@
     <div class="row center-form">
         <div class="col-md-4 col-sm-6">
             <h1 class="logo  logo--standalone"><a href="{{ route('home') }}" class="logo__biigle">BIIGLE</a></h1>
-        @if (session('status'))
+        @if (config('biigle.offline_mode'))
+            <div class="alert alert-danger">
+                BIIGLE is in offline mode and cannot send password reset emails. Please contact a BIIGLE administrator to reset your password.
+            </div>
+        @elseif (session('status'))
             <div class="alert alert-success">
                 {{ session('status') }}
             </div>
