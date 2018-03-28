d1f13915565f33b492837551ac04a2abf44a717c
Martin Zurowietz <martin@zurowietz.de>
2018-03-28 10:06:40 +0200

    Implement LabelTree::accessibleBy scope

--- app/LabelTree.php
+++ app/LabelTree.php
@@ -120,6 +120,44 @@ class LabelTree extends Model
     }
 
     /**
+     * Scope a query to all trees that are accessible by a user.
+     *
+     * @param \Illuminate\Database\Eloquent\Builder $query
+     * @param User $user
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function scopeAccessibleBy($query, User $user)
+    {
+        if ($user->isAdmin) {
+            return $query;
+        }
+
+        return $query->where(function ($query) use ($user) {
+            $query->where('label_trees.visibility_id', Visibility::$public->id)
+                // Do it like this instead of a join with label_tree_user because
+                // there can be global label trees without any members, too!
+                ->orWhere(function ($query) use ($user) {
+                    $query->whereIn('id', function ($query) use ($user) {
+                        $query->select('label_tree_id')
+                            ->from('label_tree_user')
+                            ->where('user_id', $user->id);
+                    });
+                })
+                // If the user is member of a project where the label tree is
+                // used, they may access it as well, even if they are no member
+                // of the label tree.
+                ->orWhere(function ($query) use ($user) {
+                    $query->whereIn('id', function ($query) use ($user) {
+                        $query->select('label_tree_project.label_tree_id')
+                            ->from('label_tree_project')
+                            ->join('project_user', 'project_user.project_id', '=', 'label_tree_project.project_id')
+                            ->where('project_user.user_id', $user->id);
+                    });
+                });
+        });
+    }
+
+    /**
      * The visibility of the label tree.
      *
      * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
