cbf7f2fa9d8fe2b1837a7c64101164f483b5a4f5
Martin Zurowietz <martin@zurowietz.de>
2018-03-28 10:20:48 +0200

    Use new LabelTree::accessiblyBy scope

--- src/Http/Controllers/Mixins/Views/SearchControllerMixin.php
+++ src/Http/Controllers/Mixins/Views/SearchControllerMixin.php
@@ -19,34 +19,7 @@ class SearchControllerMixin
      */
     public function index(User $user, $query, $type)
     {
-        if ($user->isAdmin) {
-            $queryBuilder = LabelTree::query();
-        } else {
-            $queryBuilder = LabelTree::where(function ($query) use ($user) {
-                    $query->where('label_trees.visibility_id', Visibility::$public->id)
-                        // Do it like this instead of a join with label_tree_user because
-                        // there can be global label trees without any members, too!
-                        ->orWhere(function ($query) use ($user) {
-                            $query->whereIn('id', function ($query) use ($user) {
-                                $query->select('label_tree_id')
-                                    ->from('label_tree_user')
-                                    ->where('user_id', $user->id);
-                            });
-                        })
-                        // If the user is member of a project where the label tree is
-                        // used, they may access it as well, even if they are no member
-                        // of the label tree.
-                        ->orWhere(function ($query) use ($user) {
-                            $query->whereIn('id', function ($query) use ($user) {
-                                $query->select('label_tree_project.label_tree_id')
-                                    ->from('label_tree_project')
-                                    ->join('project_user', 'project_user.project_id', '=', 'label_tree_project.project_id')
-                                    ->where('project_user.user_id', $user->id);
-                            });
-                        });
-                })
-                ->select('label_trees.*');
-        }
+        $queryBuilder = LabelTree::accessibleBy($user);
 
         if ($query) {
             if (\DB::connection() instanceof \Illuminate\Database\PostgresConnection) {
@@ -64,7 +37,8 @@ class SearchControllerMixin
         $values = [];
 
         if ($type === 'label-trees') {
-            $values['results'] = $queryBuilder->orderBy('label_trees.updated_at', 'desc')
+            $values['results'] = $queryBuilder
+                ->orderBy('label_trees.updated_at', 'desc')
                 ->paginate(10);
 
             $values['labelTreeResultCount'] = $values['results']->total();
