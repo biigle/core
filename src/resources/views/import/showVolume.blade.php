740894ccc568b0fa8c5defd24f3a5c7d38f32995
Martin Zurowietz <martin@zurowietz.de>
2018-04-17 15:58:24 +0200

    Add loading spinner to import views

--- src/resources/views/import/showVolume.blade.php
+++ src/resources/views/import/showVolume.blade.php
@@ -125,6 +125,7 @@
                     <input type="hidden" name="_method" value="DELETE">
                     <input type="hidden" name="_token" value="{{ csrf_token() }}">
                     <div class="form-group">
+                        <loader v-bind:active="loading" v-cloak></loader>
                         <button type="submit" class="btn btn-default" title="Delete the uploaded import files" v-bind:disabled="loading">Discard import</button>
                         <button type="button" class="btn btn-success" v-on:click.prevent="performImport" v-bind:disabled="cantDoImport" v-bind:title="submitTitle">Perform import</button>
                     </div>
