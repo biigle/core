b745cc3018c7f83e511e23c720cb41ce38ee207c
Martin Zurowietz <martin@zurowietz.de>
2018-11-08 10:23:35 +0100

    Replace DispatchesJob trait with use of static dispatch function

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -3,14 +3,13 @@
 namespace Biigle\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 
 class Controller extends BaseController
 {
-    use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+    use AuthorizesRequests, ValidatesRequests;
 
     /**
      * Determines if the request was done by an automated script (with API
