080c2d1674e9f6b260d07595a2445d0da89d2547
Martin Zurowietz <martin@zurowietz.de>
2019-02-04 10:21:54 +0100

    Implement text overflow for long label names in track headers

--- src/resources/js/components/trackHeaders.js
+++ src/resources/js/components/trackHeaders.js
@@ -1,7 +1,11 @@
 biigle.$component('videos.components.trackHeaders', {
     template: '<div class="track-headers">' +
         '<div class="track-header" v-for="track in tracks">'+
-            '<div class="label-name" v-text="track.label.name"></div>' +
+            '<div' +
+                ' class="label-name"' +
+                ' v-text="track.label.name"' +
+                ' :title="track.label.name"' +
+                '></div>' +
             '<div class="lane-dummy" v-for="lane in track.lanes"></div>' +
         '</div>' +
     '</div>',
