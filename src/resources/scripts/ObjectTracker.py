978462622822f3172f8704f8174440443c570cd5
Martin Zurowietz <martin@zurowietz.de>
2019-02-04 14:18:50 +0100

    Implement CSRT tracker
    References #6

--- src/resources/scripts/ObjectTracker.py
+++ src/resources/scripts/ObjectTracker.py
@@ -5,35 +5,20 @@ import json
 
 class ObjectTracker(object):
     def __init__(self, params):
-        self.video_path = params['video_path']
-        self.start_time = params['start_time']
-        self.track_window = tuple(map(int, params['start_window']))
-
-        self.video = cv2.VideoCapture(self.video_path)
+        self.video = cv2.VideoCapture(params['video_path'])
         self.fps = self.video.get(cv2.CAP_PROP_FPS)
-        self.width = self.video.get(cv2.CAP_PROP_FRAME_WIDTH)
-        self.height = self.video.get(cv2.CAP_PROP_FRAME_HEIGHT)
+        # self.width = self.video.get(cv2.CAP_PROP_FRAME_WIDTH)
+        # self.height = self.video.get(cv2.CAP_PROP_FRAME_HEIGHT)
 
-        start_frame = round(self.start_time * self.fps)
+        start_frame = round(params['start_time'] * self.fps)
         self.video.set(cv2.CAP_PROP_POS_FRAMES, start_frame)
 
-        self.roi_hist = self.get_roi_hist(self.track_window)
-        self.term_criteria = (cv2.TERM_CRITERIA_EPS | cv2.TERM_CRITERIA_COUNT, 0, 1)
+        self.tracker = cv2.TrackerCSRT_create()
+        _, frame = self.video.read()
+        track_window = tuple(map(int, params['start_window']))
+        self.tracker.init(frame, track_window)
 
         self.debug = False
-
-    def get_roi_hist(self, window):
-        x, y, w, h = window
-        _, frame = self.video.read()
-        roi = frame[y:(y + h), x:(x + w)]
-        hsv_roi = cv2.cvtColor(roi, cv2.COLOR_BGR2HSV)
-
-        return cv2.calcHist([hsv_roi], [0], None, [180], [0, 180])
-
-    def track_window_stop_condition(self, window):
-        x, y, w, h = window
-
-        return x == 0 or y == 0 or (x + w) == self.width or (y + h) == self.height
 
     def __iter__(self):
         return self
@@ -49,28 +34,22 @@ class ObjectTracker(object):
         success, frame = self.video.read()
         if not success:
             raise StopIteration
-        hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-        mask = cv2.calcBackProject([hsv], [0], self.roi_hist, [0, 180], 1)
-        ret, track_window = cv2.meanShift(mask, self.track_window, self.term_criteria)
-        self.track_window = track_window
 
-        if self.track_window_stop_condition(self.track_window):
+        (success, box) = self.tracker.update(frame)
+
+        if not success:
             raise StopIteration
 
         current_frame = self.video.get(cv2.CAP_PROP_POS_FRAMES)
         current_time = current_frame / self.fps
 
         if self.debug:
-            x, y, w, h = self.track_window
+            x, y, w, h = map(int, box)
             cv2.rectangle(frame, (x, y), (x + w, y + h), 255, 2)
-            # for CamShift:
-            # pts = cv2.boxPoints(ret)
-            # pts = np.int0(pts)
-            # cv2.polylines(frame, [pts], True, (255, 0, 0), 2)
             cv2.imshow("frame", frame)
             cv2.waitKey(1)
 
-        return (current_time,) + self.track_window
+        return (current_time,) + box
 
     def __next__(self):
         return self._next()
