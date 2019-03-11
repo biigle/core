6935e0e1ed668671c540451b81eebd0b3161b8e5
Martin Zurowietz <martin@zurowietz.de>
2019-03-11 11:28:20 +0100

    Fix display of admin dashboard view mixin

--- src/resources/views/adminIndex.blade.php
+++ src/resources/views/adminIndex.blade.php
@@ -1,16 +1,20 @@
-<div class="panel panel-default">
-    <div class="panel-heading">
-        <a href="{{route('admin-volumes')}}" title="Volumes"><h3 class="panel-title">Volumes</h3></a>
-    </div>
-    <div class="panel-body">
-        <p class="h1 text-center">{{ Biigle\Volume::count() }}</p>
+<div class="col-sm-6">
+    <div class="panel panel-default">
+        <div class="panel-heading">
+            <a href="{{route('admin-volumes')}}" title="Volumes"><h3 class="panel-title">Volumes</h3></a>
+        </div>
+        <div class="panel-body">
+            <p class="h1 text-center">{{ Biigle\Volume::count() }}</p>
+        </div>
     </div>
 </div>
-<div class="panel panel-default">
-    <div class="panel-heading">
-        <h3 class="panel-title">Images</h3>
-    </div>
-    <div class="panel-body">
-        <p class="h1 text-center">{{ number_format(Biigle\Image::count()) }}</p>
+<div class="col-sm-6">
+    <div class="panel panel-default">
+        <div class="panel-heading">
+            <h3 class="panel-title">Images</h3>
+        </div>
+        <div class="panel-body">
+            <p class="h1 text-center">{{ number_format(Biigle\Image::count()) }}</p>
+        </div>
     </div>
 </div>
