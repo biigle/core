771ea9b4e248e28f88fda788711ba4beba40bb74
Martin Zurowietz <martin@zurowietz.de>
2019-05-28 10:11:24 +0200

    Finalize dashboard activity item

--- src/resources/views/dashboardActivityItem.blade.php
+++ src/resources/views/dashboardActivityItem.blade.php
@@ -1,6 +1,6 @@
 <a class="activity-item" href="{{route('video', $item->id)}}" title="Show video {{$item->name}}">
     <figure class="activity-item-image">
-        <span class="label label-default">Video</span>
+        <i class="icon fas fa-video fa-lg"></i>
         <img src="{{ thumbnail_url($item->thumbnail, config('videos.thumbnail_storage_disk')) }}" onerror="this.src='{{ asset(config('thumbnails.empty_url')) }}'">
         <figcaption>{{$item->name}}</figcaption>
     </figure>
