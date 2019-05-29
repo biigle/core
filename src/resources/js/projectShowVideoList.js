3b3dc54141914654c21651a785c32c24f588e239
Martin Zurowietz <martin@zurowietz.de>
2019-05-29 16:26:41 +0200

    Implement video panel filtering
    Resolves #48

--- src/resources/js/projectShowVideoList.js
+++ src/resources/js/projectShowVideoList.js
@@ -17,10 +17,41 @@ biigle.$viewModel('projects-show-video-list', function (element) {
         data: {
             project: biigle.$require('projects.project'),
             videos: biigle.$require('projects.videos'),
+            filterString: '',
+            fullHeight: 0,
         },
         computed: {
-            hasNoVideos: function () {
-                return this.videos.length === 0;
+            filteredVideos: function () {
+                if (this.hasFiltering) {
+                    var filterString = this.filterString.toLowerCase();
+
+                    return this.videos.filter(function (video) {
+                        return video.name.toLowerCase().indexOf(filterString) !== -1;
+                    });
+                }
+
+                return this.videos;
+            },
+            hasFiltering: function () {
+                return this.filterString.length > 0;
+            },
+            filterInputClass: function () {
+                return this.hasFiltering ? 'panel-filter--active' : '';
+            },
+            hasVideos: function () {
+                return this.videos.length > 0;
+            },
+            panelStyle: function () {
+                if (this.hasFiltering) {
+                    return {
+                        height: this.fullHeight + 'px',
+                    };
+                }
+
+                return {};
+            },
+            hasNoMatchingVideos: function () {
+                return this.hasVideos && this.filteredVideos.length === 0;
             },
         },
         methods: {
@@ -47,8 +78,18 @@ biigle.$viewModel('projects-show-video-list', function (element) {
                     if (index !== -1) {
                         this.videos.splice(index, 1);
                     }
+                    this.$nextTick(this.updateFullHeight);
                 }).bind(this);
             },
+            clearFiltering: function () {
+                this.filterString = '';
+            },
+            updateFullHeight: function () {
+                this.fullHeight = this.$el.offsetHeight;
+            },
+        },
+        mounted: function () {
+            this.updateFullHeight();
         },
     });
 });
--- src/resources/views/projectsShowLeft.blade.php
+++ src/resources/views/projectsShowLeft.blade.php
@@ -1,17 +1,25 @@
-<div id="projects-show-video-list" class="panel panel-default" v-bind:class="{'panel-warning':editing}">
+<div id="projects-show-video-list" class="panel panel-default" v-bind:class="{'panel-warning':editing}"  v-bind:style="panelStyle">
     <div class="panel-heading">
         Videos
-        @can('update', $project)
-            <span class="pull-right">
+        <span class="pull-right">
+            @can('update', $project)
                 <loader :active="loading"></loader>
+            @endcan
+            <form v-cloak v-if="hasVideos" class="panel-filter" :class="filterInputClass">
+                <span class="form-group has-feedback">
+                    <input class="form-control input-sm" type="text" name="filter" placeholder="Filter videos" v-model="filterString" v-on:keyup.esc="clearFiltering">
+                    <span v-cloak v-show="hasFiltering" v-on:click="clearFiltering" title="Clear filtering" class="form-control-feedback" aria-hidden="true"><i class="fas fa-times fa-sm"></i></span>
+                </span>
+            </form>
+            @can('update', $project)
                 <a href="{{ route('create-video') }}?project={{ $project->id }}" class="btn btn-default btn-xs" title="Create a new video"><span class="fa fa-plus" aria-hidden="true"></span></a>
                 <button class="btn btn-default btn-xs" title="Edit videos" v-on:click="toggleEditing"><span class="fa fa-pencil-alt" aria-hidden="true"></span></button>
-            </span>
-        @endcan
+            @endcan
+        </span>
     </div>
     <div class="panel-body container-fluid volumes-grid">
         <div class="row">
-            <div class="col-sm-6" v-for="video in videos" v-bind:key="video.id" v-cloak>
+            <div class="col-sm-6" v-for="video in filteredVideos" v-bind:key="video.id" v-cloak>
                 <a v-bind:href="'{{route('video', '')}}/'+video.id" v-bind:title="'Show video '+video.name">
                     <preview-thumbnail class="preview-thumbnail preview-thumbnail--projects" v-bind:id="video.id" :thumb-uris="video.thumbnailsUrl" @can('update', $project) v-bind:removable="editing" v-bind:remove-title="'Delete video '+video.name" @endcan v-on:remove="deleteVideo(video)">
                         <img v-bind:src="video.thumbnailUrl" onerror="this.src='{{ asset(config('thumbnails.empty_url')) }}'">
@@ -20,7 +28,8 @@
                 </a>
             </div>
         </div>
-        <span class="text-muted" v-if="!videos.length" v-cloak>This project has no videos.</span>
+        <span class="text-muted" v-if="hasNoMatchingVideos" v-cloak>No video matches this query.</span>
+        <span class="text-muted" v-if="!hasVideos" v-cloak>This project has no videos.</span>
     </div>
 </div>
 
