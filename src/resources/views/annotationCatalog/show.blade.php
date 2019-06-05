33f85f6ecac201e4a53c541cf9e39dfadd8da93e
Martin Zurowietz <martin@zurowietz.de>
2019-06-05 13:11:39 +0200

    Fix selecting of a label in the annotation catalog

--- src/resources/views/annotationCatalog/show.blade.php
+++ src/resources/views/annotationCatalog/show.blade.php
@@ -30,7 +30,7 @@
 @section('content')
 <div id="annotation-catalog-container" class="sidebar-container">
     <div class="sidebar-container__content">
-        <catalog-image-grid :images="annotations" empty-url="{{ asset(config('thumbnails.empty_url')) }}" :width="{{config('thumbnails.width')}}" :height="{{config('thumbnails.height')}}"></catalog-image-grid>
+        <catalog-image-grid :images="annotations" ref="dismissGrid" empty-url="{{ asset(config('thumbnails.empty_url')) }}" :width="{{config('thumbnails.width')}}" :height="{{config('thumbnails.height')}}"></catalog-image-grid>
         <div class="largo-images__alerts" :class="{block: loading}">
             <div v-cloak v-if="loading">
                 <loader :active="true" :fancy="true"></loader>
