465dd4f63b0be20c96e2b29e78cf13277b8615f5
Martin Zurowietz <martin@zurowietz.de>
2019-08-21 11:02:33 +0200

    Directly use tags for GitHub Docker registry

--- build.sh
+++ build.sh
@@ -13,13 +13,13 @@ read -p "Publish the images to GitHub? [y/N]" -n 1 -r
 git describe --tags --exact-match &> /dev/null
 if [ $? -eq 0 ]; then
     docker tag \
-        biigle/app:latest \
+        docker.pkg.github.com/biigle/core/app:latest \
         docker.pkg.github.com/biigle/core/app:$VERSION
     docker tag \
-        biigle/worker:latest \
+        docker.pkg.github.com/biigle/core/worker:latest \
         docker.pkg.github.com/biigle/core/worker:$VERSION
     docker tag \
-        biigle/web:latest \
+        docker.pkg.github.com/biigle/core/web:latest \
         docker.pkg.github.com/biigle/core/web:$VERSION
 
     if [[ $REPLY =~ ^[Yy]$ ]]; then
@@ -31,10 +31,6 @@ fi
 
 # Update the "latest" images if the current HEAD is on master.
 if [ "$(git rev-parse --abbrev-ref HEAD)" == "master" ]; then
-    docker tag biigle/app:latest docker.pkg.github.com/biigle/core/app:latest
-    docker tag biigle/worker:latest docker.pkg.github.com/biigle/core/worker:latest
-    docker tag biigle/web:latest docker.pkg.github.com/biigle/core/web:latest
-
     if [[ $REPLY =~ ^[Yy]$ ]]; then
         docker push docker.pkg.github.com/biigle/core/app:latest
         docker push docker.pkg.github.com/biigle/core/worker:latest
