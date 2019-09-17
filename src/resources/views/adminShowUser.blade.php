28e8ffc961646ba4a3c75d79ded85109e06a42b8
Martin Zurowietz <martin@zurowietz.de>
2019-09-17 13:53:59 +0200

    Improve display of user stats in admin panel

--- src/resources/views/adminShowUser.blade.php
+++ src/resources/views/adminShowUser.blade.php
@@ -1,19 +1,21 @@
-<div class="col-xs-12">
-    <p>
-        @if ($volumesCount > 0)
-            Created <strong>{{$volumesCount}}</strong> {{$volumesCount === 1 ? 'volume' : 'volumes'}} ({{ $volumesPercent}} %)
-            @if ($imagesCount > 0)
-                which {{$volumesCount === 1 ? 'contains' : 'contain'}} <strong>{{$imagesCount}}</strong> {{$imagesCount === 1 ? 'image' : 'images'}} ({{ $imagesPercent }} %).
+<div class="col-xs-6">
+    <div class="panel panel-default">
+        <div class="panel-body">
+            @if ($volumesCount > 0)
+                Created <strong>{{$volumesCount}}</strong> {{$volumesCount === 1 ? 'volume' : 'volumes'}} ({{ $volumesPercent}} %)
+                @if ($imagesCount > 0)
+                    which {{$volumesCount === 1 ? 'contains' : 'contain'}} <strong>{{$imagesCount}}</strong> {{$imagesCount === 1 ? 'image' : 'images'}} ({{ $imagesPercent }} %).
+                @else
+                    which {{$volumesCount === 1 ? 'contains' : 'contain'}} no images.
+                @endif
             @else
-                which {{$volumesCount === 1 ? 'contains' : 'contain'}} no images.
+                Created no volumes yet.
             @endif
-        @else
-            Created no volumes yet.
-        @endif
-    </p>
-    <ul>
-        @foreach ($volumes as $volume)
-            <li><a href="{{route('volume', $volume->id)}}">{{$volume->name}}</a></li>
-        @endforeach
-    </ul>
+        </div>
+        <ul class="list-group user-stats-list-group">
+            @foreach ($volumes as $volume)
+                <li class="list-group-item"><a href="{{route('volume', $volume->id)}}">{{$volume->name}}</a></li>
+            @endforeach
+        </ul>
+    </div>
 </div>
