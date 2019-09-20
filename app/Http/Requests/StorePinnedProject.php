0154683956a9640ee29df0644586a1f6fcb4066e
Martin Zurowietz <martin@zurowietz.de>
2019-09-20 18:39:40 +0200

    Implement pinned projects

---
+++ app/Http/Requests/StorePinnedProject.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Biigle\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StorePinnedProject extends FormRequest
+{
+    /**
+     * The project to pin.
+     *
+     * @var Project
+     */
+    public $project;
+
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        $this->project = $this->user()->projects()->findOrFail($this->route('id'));
+
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            //
+        ];
+    }
+
+    /**
+     * Configure the validator instance.
+     *
+     * @param  \Illuminate\Validation\Validator  $validator
+     * @return void
+     */
+    public function withValidator($validator)
+    {
+        $validator->after(function ($validator) {
+            $pinnedCount = $this->user()->projects()->where('pinned', true)->count();
+
+            if ($pinnedCount === 3 && !$this->project->pivot->pinned) {
+                $validator->errors()->add('id', 'You cannot pin more than three projects.');
+            }
+        });
+    }
+}
--- app/User.php
+++ app/User.php
@@ -56,7 +56,7 @@ class User extends Authenticatable
      */
     public function projects()
     {
-        return $this->belongsToMany(Project::class);
+        return $this->belongsToMany(Project::class)->withPivot('pinned');
     }
 
     /**
