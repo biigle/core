ff678af2a1ee697aae44babe71b748f49bf73e32
Martin Zurowietz <martin@zurowietz.de>
2019-11-26 13:19:24 +0100

    Fix str_plural in user admin view

--- src/resources/views/adminShowUser.blade.php
+++ src/resources/views/adminShowUser.blade.php
@@ -2,7 +2,7 @@
     <div class="panel panel-default">
         <div class="panel-body">
             @if ($videosCount > 0)
-                Created <strong>{{ $videosCount }}</strong> {{ str_plural('video', $videosCount) }} ({{ $videosPercent }}&nbsp;%) which have a total duration of {{ round($duration / 3600, 2) }}&nbsp;h ({{ $durationPercent }}&nbsp;%).
+                Created <strong>{{ $videosCount }}</strong> {{ Str::plural('video', $videosCount) }} ({{ $videosPercent }}&nbsp;%) which have a total duration of {{ round($duration / 3600, 2) }}&nbsp;h ({{ $durationPercent }}&nbsp;%).
             @else
                 Created no videos yet.
             @endif
@@ -19,7 +19,7 @@
     <div class="panel panel-default">
         <div class="panel-body">
             @if ($totalVideoAnnotationLabels > 0)
-                Attached <strong>{{ $totalVideoAnnotationLabels }}</strong> {{ str_plural('label', $totalVideoAnnotationLabels) }} ({{ round($relativeVideoAnnotationLabels * 100, 2)}}&nbsp;%) to <strong>{{ $totalVideoAnnotations }}</strong> video {{ str_plural('annotation', $totalVideoAnnotations) }} ({{ round($relativeVideoAnnotations * 100, 2) }}&nbsp;%).
+                Attached <strong>{{ $totalVideoAnnotationLabels }}</strong> {{ Str::plural('label', $totalVideoAnnotationLabels) }} ({{ round($relativeVideoAnnotationLabels * 100, 2)}}&nbsp;%) to <strong>{{ $totalVideoAnnotations }}</strong> video {{ Str::plural('annotation', $totalVideoAnnotations) }} ({{ round($relativeVideoAnnotations * 100, 2) }}&nbsp;%).
             @else
                 Created no video annotations yet.
             @endif
