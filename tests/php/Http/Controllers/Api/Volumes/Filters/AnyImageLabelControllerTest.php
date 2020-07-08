90572dcc648d8a80a4a56c4580b2f95b13edc584
Martin Zurowietz <martin@zurowietz.de>
2020-07-08 16:15:51 +0200

    Prepare module to be merged into biigle/core

---
+++ tests/php/Http/Controllers/Api/Volumes/Filters/AnyImageLabelControllerTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Biigle\Tests\Http\Controllers\Api\Volumes\Filters;
+
+use ApiTestCase;
+use Biigle\Tests\ImageTest;
+use Biigle\Tests\ImageLabelTest;
+
+class AnyImageLabelControllerTest extends ApiTestCase
+{
+    public function testIndex()
+    {
+        $id = $this->volume()->id;
+
+        $image = ImageTest::create(['volume_id' => $id]);
+        ImageLabelTest::create(['image_id' => $image->id]);
+        // this image shouldn't appear
+        ImageTest::create(['volume_id' => $id, 'filename' => 'b.jpg']);
+
+        $this->doTestApiRoute('GET', "/api/v1/volumes/{$id}/images/filter/labels");
+
+        $this->beUser();
+        $response = $this->get("/api/v1/volumes/{$id}/images/filter/labels");
+        $response->assertStatus(403);
+
+        $this->beGuest();
+        $this->get("/api/v1/volumes/{$id}/images/filter/labels")
+            ->assertStatus(200)
+            ->assertExactJson([$image->id]);
+    }
+}
---
+++ tests/php/Http/Controllers/Api/Volumes/UserControllerTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Biigle\Tests\Http\Controllers\Api\Volumes;
+
+use ApiTestCase;
+
+class UserControllerTest extends ApiTestCase
+{
+    public function testIndex()
+    {
+        $id = $this->volume()->id;
+
+        $this->doTestApiRoute('GET', "/api/v1/volumes/{$id}/users");
+
+        $this->beUser();
+        $response = $this->get("/api/v1/volumes/{$id}/users");
+        $response->assertStatus(403);
+
+        $this->beGuest();
+        $response = $this->get("/api/v1/volumes/{$id}/users")
+            ->assertStatus(200);
+        $response->assertExactJson(
+            $this->project()->users()
+                ->select('id', 'firstname', 'lastname', 'affiliation')
+                ->get()
+                ->map(function ($item) {
+                    unset($item->project_role_id);
+
+                    return $item;
+                })
+                ->toArray()
+        );
+    }
+}
