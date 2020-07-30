c530360dc511941f4ed1109385e85fa3c8279838
Martin Zurowietz <martin@zurowietz.de>
2020-07-30 09:48:01 +0200

    Remove unnecessary JS exports

--- resources/assets/js/volumes/export.js
+++ resources/assets/js/volumes/export.js
@@ -2,19 +2,14 @@ import FilterListComponent from './components/filterListComponent';
 import FilterStore from './stores/filters';
 import ImageGrid from './components/imageGrid';
 import ImageGridImage from './components/imageGridImage';
-import ImageLabelsApi from './api/imageLabels';
-import LabelList from './components/fileLabelList';
 import SortComponent from './components/sortComponent';
 import SorterStore from './stores/sorters';
 import VolumesApi from './api/volumes';
 
 biigle.$declare('annotations.api.volumes', VolumesApi); // Legacy support
-biigle.$declare('api.imageLabels', ImageLabelsApi);
 biigle.$declare('volumes.components.filterListComponent', FilterListComponent);
 biigle.$declare('volumes.components.imageGrid', ImageGrid);
 biigle.$declare('volumes.components.imageGridImage', ImageGridImage);
-biigle.$declare('volumes.components.imageLabelList', LabelList);
-biigle.$declare('volumes.components.sortComponent', SortComponent);
 biigle.$declare('volumes.mixins.sortComponent', SortComponent); // Legacy support.
 biigle.$declare('volumes.stores.filters', FilterStore);
 biigle.$declare('volumes.stores.sorters', SorterStore);
