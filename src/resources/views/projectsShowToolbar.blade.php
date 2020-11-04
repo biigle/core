c59bcba11e4d25fe00417b8ae2c087bd9461f3a6
Martin Zurowietz <martin@zurowietz.de>
2020-11-04 14:22:00 +0100

    Update for the default new project overview

--- src/resources/views/projectsShowToolbar.blade.php
+++
@@ -1,5 +0,0 @@
-@can('edit-in', $project)
-    @if ($project->imageVolumes()->exists())
-        <a href="{{route('projectsLargo', $project->id)}}" class="btn btn-default" title="Perform Largo re-evaluation of image annotations for this project"><span class="fa fa-check-square" aria-hidden="true"></span> Largo re-evaluation</a>
-    @endif
-@endcan
