c6ce992606a12f9e3e736ce2a25871e44c795e92
Martin Zurowietz <martin@zurowietz.de>
2020-11-26 16:52:12 +0100

    Implement support for new label tree overview

---
+++ src/resources/views/labelTreesShowTabs.blade.php
@@ -0,0 +1,3 @@
+<li role="presentation">
+    <a href="{{route('annotation-catalog', $tree->id)}}" title="Browse all annotations with labels of this label tree"><i class="fa fa-book"></i> Annotation catalog</a>
+</li>
--- src/resources/views/labelTreesShowToolbar.blade.php
+++
@@ -1,1 +0,0 @@
-<a class="btn btn-default" href="{{route('annotation-catalog', $tree->id)}}" title="Browse all annotations with labels of this label tree"><i class="fa fa-book"></i> Annotation catalog</a>
