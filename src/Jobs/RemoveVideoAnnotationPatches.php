fd543790ac110e333b66f6e443b13c4d88bf35f1
Martin Zurowietz <martin@zurowietz.de>
2020-12-07 14:13:13 +0100

    Improve remove video annotation patch job

--- src/Jobs/RemoveVideoAnnotationPatches.php
+++ src/Jobs/RemoveVideoAnnotationPatches.php
@@ -12,14 +12,10 @@ class RemoveVideoAnnotationPatches extends RemoveAnnotationPatches
     protected function deletePatches()
     {
         $disk = Storage::disk(config('largo.patch_storage_disk'));
-        $format = config('largo.patch_format');
-        $count = config('largo.video_patch_count');
 
         foreach ($this->annotationIds as $id => $uuid) {
             $prefix = fragment_uuid_path($uuid);
-            for ($i = 0; $i < $count; $i++) {
-                $disk->delete("{$prefix}/{$id}/{$i}.{$format}");
-            }
+            $disk->deleteDirectory("{$prefix}/{$id}");
         }
     }
 }
