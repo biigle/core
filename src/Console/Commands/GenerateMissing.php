6112003380bfc2f2da5c1eb7305353fa0a3cb93c
Martin Zurowietz <martin@zurowietz.de>
2020-12-10 14:53:35 +0100

    Extend largo_generate-missing command for video annotations

--- src/Console/Commands/GenerateMissing.php
+++ src/Console/Commands/GenerateMissing.php
@@ -3,7 +3,9 @@
 namespace Biigle\Modules\Largo\Console\Commands;
 
 use Biigle\ImageAnnotation;
+use Biigle\VideoAnnotation;
 use Biigle\Modules\Largo\Jobs\GenerateImageAnnotationPatch;
+use Biigle\Modules\Largo\Jobs\GenerateVideoAnnotationPatch;
 use File;
 use Illuminate\Console\Command;
 use Storage;
@@ -15,7 +17,7 @@ class GenerateMissing extends Command
      *
      * @var string
      */
-    protected $signature = 'largo:generate-missing {--dry-run} {--volume=}';
+    protected $signature = 'largo:generate-missing {--dry-run} {--volume=} {--no-image-annotations} {--no-video-annotations}';
 
     /**
      * The console command description.
@@ -58,6 +60,23 @@ class GenerateMissing extends Command
         $pushToQueue = !$this->option('dry-run');
         $storage = Storage::disk(config('largo.patch_storage_disk'));
 
+        if (!$this->option('no-image-annotations')) {
+            $this->handleImageAnnotations($storage, $pushToQueue);
+        }
+
+        if (!$this->option('no-video-annotations')) {
+            $this->handleVideoAnnotations($storage, $pushToQueue);
+        }
+    }
+
+    /**
+     * Check image annnotation patches
+     *
+     * @param \Illuminate\Filesystem\FilesystemAdapter $storage
+     * @param bool $pushToQueue
+     */
+    protected function handleImageAnnotations($storage, $pushToQueue)
+    {
         $annotations = ImageAnnotation::join('images', 'images.id', '=', 'image_annotations.image_id')
             ->when($this->option('volume'), function ($query) {
                 $query->where('images.volume_id', $this->option('volume'));
@@ -66,27 +85,67 @@ class GenerateMissing extends Command
 
         $total = $annotations->count();
         $progress = $this->output->createProgressBar($total);
-        $this->info("Checking {$total} annotations...");
+        $this->info("Checking {$total} image annotations...");
 
-        $handleChunk = function ($chunk) use ($progress, $pushToQueue, $storage) {
-            foreach ($chunk as $annotation) {
-                $prefix = fragment_uuid_path($annotation->uuid);
-                if (!$storage->exists("{$prefix}/{$annotation->id}.{$this->format}")) {
-                    $this->count++;
-                    if ($pushToQueue) {
-                        GenerateImageAnnotationPatch::dispatch($annotation)
-                            ->onQueue(config('largo.generate_annotation_patch_queue'));
-                    }
+        $handleAnnotation = function ($annotation) use ($progress, $pushToQueue, $storage) {
+            $prefix = fragment_uuid_path($annotation->uuid);
+            if (!$storage->exists("{$prefix}/{$annotation->id}.{$this->format}")) {
+                $this->count++;
+                if ($pushToQueue) {
+                    GenerateImageAnnotationPatch::dispatch($annotation)
+                        ->onQueue(config('largo.generate_annotation_patch_queue'));
                 }
             }
-            $progress->advance($chunk->count());
+            $progress->advance();
         };
 
-        $annotations->chunkById(10000, $handleChunk, 'image_annotations.id', 'id');
+        $annotations->eachById($handleAnnotation, 10000, 'image_annotations.id', 'id');
+
         $progress->finish();
 
         $percent = round($this->count / $total * 100, 2);
-        $this->info("\nFound {$this->count} annotations with missing patches ({$percent} %).");
+        $this->info("\nFound {$this->count} image annotations with missing patches ({$percent} %).");
+        if ($pushToQueue) {
+            $this->info("Pushed {$this->count} jobs to the queue.");
+        }
+    }
+
+    /**
+     * Check video annnotation patches
+     *
+     * @param \Illuminate\Filesystem\FilesystemAdapter $storage
+     * @param bool $pushToQueue
+     */
+    protected function handleVideoAnnotations($storage, $pushToQueue)
+    {
+        $annotations = VideoAnnotation::join('videos', 'videos.id', '=', 'video_annotations.video_id')
+            ->when($this->option('volume'), function ($query) {
+                $query->where('videos.volume_id', $this->option('volume'));
+            })
+            ->select('video_annotations.id', 'videos.uuid as uuid');
+
+        $total = $annotations->count();
+        $progress = $this->output->createProgressBar($total);
+        $this->info("Checking {$total} video annotations...");
+
+        $handleAnnotation = function ($annotation) use ($progress, $pushToQueue, $storage) {
+            $prefix = fragment_uuid_path($annotation->uuid);
+            if (!$storage->exists("{$prefix}/v-{$annotation->id}.{$this->format}")) {
+                $this->count++;
+                if ($pushToQueue) {
+                    GenerateVideoAnnotationPatch::dispatch($annotation)
+                        ->onQueue(config('largo.generate_annotation_patch_queue'));
+                }
+            }
+            $progress->advance();
+        };
+
+        $annotations->eachById($handleAnnotation, 10000, 'video_annotations.id', 'id');
+
+        $progress->finish();
+
+        $percent = round($this->count / $total * 100, 2);
+        $this->info("\nFound {$this->count} video annotations with missing patches ({$percent} %).");
         if ($pushToQueue) {
             $this->info("Pushed {$this->count} jobs to the queue.");
         }
