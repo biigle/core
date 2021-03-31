3113db66cc25a965ca4571b5b75cc086187c4fcb
Martin Zurowietz <martin@zurowietz.de>
2021-03-31 15:56:22 +0200

    Upgrade to Laravel Mix 6

--- webpack.mix.js
+++ webpack.mix.js
@@ -17,7 +17,7 @@ mix.options({processCssUrls: false});
 
 mix.setPublicPath('src/public');
 
-mix.js('src/resources/assets/js/main.js', 'assets/scripts')
+mix.js('src/resources/assets/js/main.js', 'assets/scripts').vue()
     .sass('src/resources/assets/sass/main.scss', 'assets/styles')
     .publish({
         provider: 'Biigle\\Modules\\Largo\\LargoServiceProvider',
