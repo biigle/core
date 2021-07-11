d87142202ba14cb9c05988b75d9f65e086060c72
jLeonBi <jobicker@techfak.uni-bielefeld.de>
2021-07-11 15:30:24 +0200

    remove old token

---
+++ app/Http/Controllers/Api/Volumes/SimilarityIndicesController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Biigle\Http\Controllers\Api\Volumes;
+
+use Biigle\Http\Controllers\Api\Controller;
+use Biigle\Volume;
+
+class SimilarityIndicesController extends Controller
+{
+    /**
+     * Get all similarity indices of a volume.
+     *
+     * @api {get} volumes/:id/similarityIndices Get similarity indices
+     * @apiGroup Volumes
+     * @apiName VolumeSimilarityIndex
+     * @apiPermission projectMember
+     * @apiDescription Returns a map of image IDs to their similarity indices.
+     *
+     * @apiParam {Number} id The volume ID
+     *
+     * @apiSuccessExample {json} Success response:
+     * {
+     *    "123": 0,
+     *    "321": 1
+     * }
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function index($id)
+    {
+        $volume = Volume::findOrFail($id);
+        if ($volume->isImageVolume()) {
+            $this->authorize('access', $volume);
+
+            return $volume->files()->pluck('hash', 'id');
+        }
+        // check right return statement
+        return;
+
+    }
+}
