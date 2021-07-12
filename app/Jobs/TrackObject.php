3932686223e2ba6b9c1997ea83e0f20a74aa5fbf
jLeonBi <jobicker@techfak.uni-bielefeld.de>
2021-07-12 08:51:46 +0200

    remove unneccessary dd

--- app/Jobs/TrackObject.php
+++ app/Jobs/TrackObject.php
@@ -110,7 +110,6 @@ class TrackObject extends Job implements ShouldQueue
                 $outputPath = $this->getOutputJsonPath($annotation);
                 $output = $this->python("{$script} {$inputPath} {$outputPath}");
                 $keyframes = json_decode(File::get($outputPath), true);
-                dd($output);
             } catch (Exception $e) {
                 $input = File::get($inputPath);
                 throw new Exception("Input: {$input}\n".$e->getMessage());
