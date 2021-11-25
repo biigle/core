d3b1906a49494d5f5278bfb9352f774718e32992
Martin Zurowietz <martin@zurowietz.de>
2021-11-25 16:20:20 +0100

    Update manual article on image metadata upload

--- resources/views/manual/tutorials/volumes/image-metadata.blade.php
+++ resources/views/manual/tutorials/volumes/image-metadata.blade.php
@@ -117,7 +117,7 @@ image_1.png,2016-12-19 17:09:00,52.112,28.001,-1500.5,30.25,2.6
 image_2.png,2016-12-19 17:09:31,52.215,28.501,-1502.5,28.25,2.1
 </pre>
         <p>
-            The image metadata CSV file can be uploaded by volume admins on the volume edit page that you can reach with the <button class="btn btn-default btn-xs"><span class="fa fa-pencil-alt" aria-hidden="true"></span></button> button of the volume overview.
+            The image metadata CSV file can be uploaded when a new volume is created. For existing volumes, image metadata can be uploaded by volume admins on the volume edit page that you can reach with the <button class="btn btn-default btn-xs"><span class="fa fa-pencil-alt" aria-hidden="true"></span></button> button of the volume overview.
         </p>
     </div>
     <div class="row">
