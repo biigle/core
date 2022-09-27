9cf0a4176aae0966b0e147968388ddbdb1621099
Max Tiessen <max.tiessen@uni-bielefeld.de>
2022-09-27 20:16:09 +0200

    Changed the timeline plot such that it shows the total contribution of each user (across all years) on default.

--- resources/assets/js/projects/components/statisticsTabTimeline.vue
+++ resources/assets/js/projects/components/statisticsTabTimeline.vue
@@ -59,12 +59,12 @@ export default {
                     // formatter: function (params){
                     //     return `${params.name}: ${params.value[1]} (${params.percent}%)`;
                     // }
-                    formatter: '{b}: {@[1]} ({d}%)'
+                    formatter: '{b}: {@[0]} ({d}%)'
                 },
                 encode: {
                 itemName: 'year',
-                tooltip:  1,
-                value: 1
+                tooltip:  0,
+                value: 0
                 }
             }
         }
@@ -73,36 +73,39 @@ export default {
         handleUpdate(event) {
             const xAxisInfo = event.axesInfo[0];
             if (xAxisInfo) {
-                const dimension = xAxisInfo.value + 1;
+                // if mouse hovered on the x-axis of timeline
+                // skip first two entries (+2) of sourcedata annotation-array-user (see sourcedata-property)
+                const dimension = xAxisInfo.value + 2;
                 // console.log('DIMENSION: ', dimension);
                 this.pieObj.label.formatter = '{b}: {@[' + dimension + ']} ({d}%)';
                 this.pieObj.encode.value = dimension;
                 this.pieObj.encode.tooltip = dimension;
+            } else {
+                // if mouse not over any x-axis line, use the first entry (0) of user-series from sourcedata (the total contribution)
+                this.pieObj.label.formatter = '{b}: {@[' + 0 + ']} ({d}%)';
+                this.pieObj.encode.value = 0;
+                this.pieObj.encode.tooltip = 0;
             }
-        }
-    },
-    created() {
-        // console.log("Image=", this.showImageVolumes, "\n Volume=", this.showVideoVolumes);
-        // console.log(JSON.stringify(this.annotationTimeSeries));
-        // this.sourcedata = this.transformData(this.annotationTimeSeries);
-        // console.log('sourcedata: ', JSON.stringify(this.sourcedata));
-        // console.log("showImageVolumes: ", this.showImageVolumes);
-    },
-    mounted() {
-    },
-    computed: {
-        sourcedata() {
-            let dat = this.annotationTimeSeries;
-            let chartdata = [];
-
-            // get all X-Axis data
-            let xAxis = dat.map((entry) => {
+        },
+        extractYear() {
+            // helper-function to get all X-Axis data (years)
+            let xAxis = this.annotationTimeSeries.map((entry) => {
                 return entry.year.toString();
             });
             // filter duplicated years
             xAxis = [...new Set(xAxis)];
              // sort the years (increasing)
-            xAxis.sort();
+            return xAxis.sort();
+        }
+    },
+    computed: {
+        sourcedata() {
+            // provides special information specifically to pie-chart, namely total contribution of each user 
+            // returns array of type [xAxis-array, annotation-array-user1, annotation-array-user2, etc.]:
+            // [['all', 'year', 2020, 2021, 2022], [1195,"Name1",1195,0,0], [6,"Name2",0,2,4], [...]]
+            let dat = this.annotationTimeSeries;
+            let xAxis = this.extractYear();
+            let chartdata = [];
 
             // get all unique User-names
             let id = dat.map((entry) => {
@@ -137,13 +140,21 @@ export default {
             // setup of whole chartdata object
             // include axis-name in front
             xAxis.unshift('year');
+            // include an entry of the sum over all years
+            xAxis.unshift('all');
             chartdata.push(xAxis);
             // reduce user-timeseries to values only
             Object.entries(idDict).forEach(entry => {
+                // calculate the sum over all years and include in the array on position 0
+                let sum = 0;
+                Object.values(entry[1]).forEach(val => {
+                    sum += val;
+                })
+                // case of deleted account
                 if(entry[0] === " ") {
-                    chartdata.push([ 'Deleted Account', ...Object.values(entry[1]) ]);
-                } else {
-                    chartdata.push([ entry[0], ...Object.values(entry[1]) ]);
+                    chartdata.push([sum, 'Deleted Account', ...Object.values(entry[1]) ]);
+                } else { // case of existing user
+                    chartdata.push([sum, entry[0], ...Object.values(entry[1]) ]);
                 }
             });
 
@@ -151,16 +162,31 @@ export default {
             // console.log('FINAL: ', JSON.stringify(chartdata));
             return [...chartdata];
         },
-        option() {
-            // create a series Array
-            let snippet = {
+        createTimelineSeries() {
+            // create a series Array with entries for each user, used for the timeline-plot
+            let series = [];
+            let end = this.sourcedata[0].length;
+
+            // create a series of data which is specific to each user
+            // skip first entry (idx=1), as it is an array of x-axis names and not user-data
+            // sourcedata-structure: [['all', 'year', 2020, 2021, 2022], [1195,"Name1",1195,0,0], [6,"Name2",0,2,4]]
+            for(let idx=1; idx < this.sourcedata.length; idx++) {
+                let snippet = {
+                    name: this.sourcedata[idx][1],
                     type: 'line',
                     smooth: true,
                     seriesLayoutBy: 'row',
-                    emphasis: { focus: 'series' }
+                    emphasis: { focus: 'series' },
+                    // skip first two entries as they are irrelevant for the timeline-data
+                    data: this.sourcedata[idx].slice(2,end)
                 };
-            // repeat as often as number of users in the project
-            let seriesObj = Array( (this.sourcedata.length-1) ).fill(snippet);
+                series.push(snippet)
+            }
+            // console.log('series: ', JSON.stringify(series));
+            return series;
+        },
+        option() {
+            let seriesObj = this.createTimelineSeries;
             // append the pie-chart specific snippet
             seriesObj.push(this.pieObj);
 
@@ -195,7 +221,10 @@ export default {
                 dataset: {
                     source: this.sourcedata
                 },
-                xAxis: { type: 'category' },
+                xAxis: { 
+                    type: 'category',
+                    data: this.extractYear()
+                },
                 yAxis: { 
                     gridIndex: 0,
                     name: "annotations",
