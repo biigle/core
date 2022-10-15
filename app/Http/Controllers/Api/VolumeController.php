88ee1cfdfd7e04728f8975b6fe60e3b19e4238ba
Leane Hecht <lhecht@techfak.uni-bielefeld.de>
2022-10-15 17:48:21 +0200

    added clone method for volumes (not finished yet)

--- app/Http/Controllers/Api/VolumeController.php
+++ app/Http/Controllers/Api/VolumeController.php
@@ -3,17 +3,32 @@
 namespace Biigle\Http\Controllers\Api;
 
 use Biigle\Http\Requests\UpdateVolume;
+use Biigle\Image;
+use Biigle\Jobs\CreateNewImagesOrVideos;
 use Biigle\Jobs\ProcessNewVolumeFiles;
+use Biigle\Project;
 use Biigle\Volume;
+use Carbon\Carbon;
 use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Queue;
 use Illuminate\Validation\ValidationException;
 
 class VolumeController extends Controller
 {
     /**
+     * Limit for the number of files above which volume files are created asynchronously.
+     *
+     * @var int
+     */
+    const CREATE_SYNC_LIMIT = 10000;
+
+    /**
      * Shows all volumes the user has access to.
      *
+     * @param Request $request
+     * @return \Illuminate\Http\Response
      * @api {get} volumes Get accessible volumes
      * @apiGroup Volumes
      * @apiName IndexVolumes
@@ -39,8 +54,6 @@ class VolumeController extends Controller
      *    }
      * ]
      *
-     * @param Request $request
-     * @return \Illuminate\Http\Response
      */
     public function index(Request $request)
     {
@@ -52,7 +65,7 @@ class VolumeController extends Controller
                     return $query->join('project_user', 'project_user.project_id', '=', 'projects.id')
                         ->where('project_user.user_id', $user->id);
                 })
-                ->select('projects.id', 'projects.name', 'projects.description');
+                    ->select('projects.id', 'projects.name', 'projects.description');
             }])
             ->orderByDesc('id')
             ->select('id', 'name', 'created_at', 'updated_at', 'media_type_id')
@@ -62,6 +75,9 @@ class VolumeController extends Controller
     /**
      * Displays the specified volume.
      *
+     * @param Request $request
+     * @param int $id
+     * @return Volume
      * @api {get} volumes/:id Get a volume
      * @apiGroup Volumes
      * @apiName ShowVolumes
@@ -87,9 +103,6 @@ class VolumeController extends Controller
      *    ]
      * }
      *
-     * @param Request $request
-     * @param  int  $id
-     * @return Volume
      */
     public function show(Request $request, $id)
     {
@@ -107,6 +120,8 @@ class VolumeController extends Controller
     /**
      * Updates the attributes of the specified volume.
      *
+     * @param UpdateVolume $request
+     * @return \Illuminate\Http\Response
      * @api {put} volumes/:id Update a volume
      * @apiGroup Volumes
      * @apiName UpdateVolumes
@@ -118,8 +133,6 @@ class VolumeController extends Controller
      * @apiParam (Attributes that can be updated) {String} url The base URL of the files. Can be a path to a storage disk like `local://volumes/1` or a remote path like `https://example.com/volumes/1`. Updating the URL will trigger a re-generation of all volume thumbnails.
      * @apiParam (Attributes that can be updated) {String} handle Handle or DOI of the dataset that is represented by the new volume.
      *
-     * @param UpdateVolume $request
-     * @return \Illuminate\Http\Response
      */
     public function update(UpdateVolume $request)
     {
@@ -143,5 +156,28 @@ class VolumeController extends Controller
                 ->with('saved', $isDirty)
                 ->with('reread', $shouldReread);
         }
+    }
+
+    public function clone($volumeId, $destProjectId)
+    {
+        $project = Project::findOrFail($destProjectId);
+        $this->authorize('update', $project);
+        $volume = Volume::findOrFail($volumeId);
+        $copy = $volume->replicate();
+        $copy->created_at = Carbon::now();
+
+        foreach ($volume->getRelations() as $relation => $items) {
+            foreach ($items as $item) {
+                unset($item->id);
+                $copy->{$relation}()->create($item->toArray());
+            }
+        }
+
+        $copy->save();
+        $project->addVolumeId($copy->id);
+        $project->push();
+
+        return redirect()->back()->with(['copy' => $copy]);
+
     }
 }
