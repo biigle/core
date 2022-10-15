c291c4fd816d0aacf86b66442d45fc5b51ba4ef8
Leane Hecht <lhecht@techfak.uni-bielefeld.de>
2022-10-15 17:50:20 +0200

    added test method for cloning image volume

--- tests/php/Http/Controllers/Api/VolumeControllerTest.php
+++ tests/php/Http/Controllers/Api/VolumeControllerTest.php
@@ -3,13 +3,33 @@
 namespace Biigle\Tests\Http\Controllers\Api;
 
 use ApiTestCase;
+use Biigle\Image;
 use Biigle\Jobs\ProcessNewVolumeFiles;
 use Biigle\MediaType;
+use Biigle\Role;
+use Biigle\Tests\ImageTest;
 use Biigle\Tests\ProjectTest;
-use Storage;
+use Biigle\Tests\UserTest;
+use Biigle\Tests\VolumeTest;
+use Biigle\Volume;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Storage;
+use function PHPUnit\Framework\assertTrue;
 
 class VolumeControllerTest extends ApiTestCase
 {
+    private $volume;
+
+    public function setUp(): void
+    {
+        parent::setUp();
+        $this->volume = VolumeTest::create();
+        $this->project()->volumes()->attach($this->volume);
+        Storage::fake('test');
+        config(['volumes.editor_storage_disks' => ['test']]);
+    }
+
+
     public function testIndex()
     {
         $project = ProjectTest::create();
@@ -72,19 +92,19 @@ class VolumeControllerTest extends ApiTestCase
         $this->doesntExpectJobs(ProcessNewVolumeFiles::class);
 
         $id = $this->volume(['media_type_id' => MediaType::imageId()])->id;
-        $this->doTestApiRoute('PUT', '/api/v1/volumes/'.$id);
+        $this->doTestApiRoute('PUT', '/api/v1/volumes/' . $id);
 
         $this->beGuest();
-        $response = $this->put('/api/v1/volumes/'.$id);
+        $response = $this->put('/api/v1/volumes/' . $id);
         $response->assertStatus(403);
 
         $this->beEditor();
-        $response = $this->put('/api/v1/volumes/'.$id);
+        $response = $this->put('/api/v1/volumes/' . $id);
         $response->assertStatus(403);
 
         $this->beAdmin();
         $this->assertNotEquals('the new volume', $this->volume()->fresh()->name);
-        $response = $this->json('PUT', '/api/v1/volumes/'.$id, [
+        $response = $this->json('PUT', '/api/v1/volumes/' . $id, [
             'name' => 'the new volume',
             'media_type_id' => MediaType::videoId(),
         ]);
@@ -141,28 +161,28 @@ class VolumeControllerTest extends ApiTestCase
 
         $this->beAdmin();
         $this->expectsJobs(ProcessNewVolumeFiles::class);
-        $this->json('PUT', '/api/v1/volumes/'.$this->volume()->id, [
-                'url' => 'admin-test://volumes',
-            ])->assertStatus(422);
-        $this->json('PUT', '/api/v1/volumes/'.$this->volume()->id, [
-                'url' => 'editor-test://volumes',
-            ])->assertStatus(200);
+        $this->json('PUT', '/api/v1/volumes/' . $this->volume()->id, [
+            'url' => 'admin-test://volumes',
+        ])->assertStatus(422);
+        $this->json('PUT', '/api/v1/volumes/' . $this->volume()->id, [
+            'url' => 'editor-test://volumes',
+        ])->assertStatus(200);
         $this->assertEquals('editor-test://volumes', $this->volume()->fresh()->url);
 
         $this->beGlobalAdmin();
-        $this->json('PUT', '/api/v1/volumes/'.$this->volume()->id, [
-                'url' => 'editor-test://volumes',
-            ])->assertStatus(422);
-        $this->json('PUT', '/api/v1/volumes/'.$this->volume()->id, [
-                'url' => 'admin-test://volumes',
-            ])->assertStatus(200);
+        $this->json('PUT', '/api/v1/volumes/' . $this->volume()->id, [
+            'url' => 'editor-test://volumes',
+        ])->assertStatus(422);
+        $this->json('PUT', '/api/v1/volumes/' . $this->volume()->id, [
+            'url' => 'admin-test://volumes',
+        ])->assertStatus(200);
         $this->assertEquals('admin-test://volumes', $this->volume()->fresh()->url);
     }
 
     public function testUpdateUrlProviderDenylist()
     {
         $this->beAdmin();
-        $this->json('PUT', '/api/v1/volumes/'.$this->volume()->id, [
+        $this->json('PUT', '/api/v1/volumes/' . $this->volume()->id, [
             'url' => 'https://dropbox.com',
         ])->assertStatus(422);
     }
@@ -173,7 +193,7 @@ class VolumeControllerTest extends ApiTestCase
         // A request that changes no attributes performed by a global admin triggers
         // a reread.
         $this->expectsJobs(ProcessNewVolumeFiles::class);
-        $this->json('PUT', '/api/v1/volumes/'.$this->volume()->id)
+        $this->json('PUT', '/api/v1/volumes/' . $this->volume()->id)
             ->assertStatus(200);
     }
 
@@ -198,17 +218,51 @@ class VolumeControllerTest extends ApiTestCase
     public function testUpdateRedirect()
     {
         $this->beAdmin();
-        $response = $this->put('/api/v1/volumes/'.$this->volume()->id, [
+        $response = $this->put('/api/v1/volumes/' . $this->volume()->id, [
             '_redirect' => 'settings/profile',
         ]);
         $response->assertRedirect('settings/profile');
         $response->assertSessionHas('saved', false);
 
         $this->get('/');
-        $response = $this->put('/api/v1/volumes/'.$this->volume()->id, [
+        $response = $this->put('/api/v1/volumes/' . $this->volume()->id, [
             'name' => 'abc',
         ]);
         $response->assertRedirect('/');
         $response->assertSessionHas('saved', true);
+    }
+
+    public function testCloneImageVolume()
+    {
+        $this->volume = VolumeTest::create(['media_type_id' => MediaType::imageId()]);
+        $id = $this->volume->id;
+
+        $project = ProjectTest::create();
+        $id2 = $project->id;
+
+        $this->doTestApiRoute('POST', "/api/v1/volumes/{$id}/project/{$id2}");
+
+        ImageTest::create([
+            'filename' => 'a.jpg',
+            'volume_id' => $this->volume->id,
+        ]);
+        ImageTest::create([
+            'filename' => 'b.jpg',
+            'volume_id' => $this->volume->id,
+        ]);
+        ImageTest::create([
+            'filename' => 'c.jpg',
+            'volume_id' => $this->volume->id,
+        ]);
+
+        $this->beAdmin();
+        $project->addUserId($this->admin()->id, Role::adminId());
+        Cache::flush();
+
+        $response = $this->post("/api/v1/volumes/{$id}/project/{$id2}");
+        $response->assertStatus(302);
+
+        $copy = $response->getSession()->get('copy');
+
     }
 }
