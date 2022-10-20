50a710832ecc54b8b1e432748b27362fccc5306a
Martin Zurowietz <martin@zurowietz.de>
2022-10-20 17:03:56 +0200

    WIP Implement dummy for announcement in navbar

---
+++ resources/assets/js/core/announcements/container.vue
@@ -0,0 +1,24 @@
+<script>
+import Popover from 'uiv/dist/Popover';
+
+export default {
+    components: {
+        popover: Popover,
+    },
+    data() {
+        return {
+            expandAnnouncement: true,
+        };
+    },
+    methods: {
+        handleHide() {
+            this.expandAnnouncement = false;
+            // Store ID in local storage to keep collapsed.
+        },
+    },
+    created() {
+        // Collapse announcement if the ID matches local storage.
+        // Delete local storage if the ID is not the one of the current announcement.
+    },
+};
+</script>
