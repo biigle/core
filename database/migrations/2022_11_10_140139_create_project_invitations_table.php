923db9be09db4cd70f5c434fe9df8b8cbe6e0e64
Martin Zurowietz <martin@zurowietz.de>
2022-11-10 14:52:08 +0100

    Implement the ProjectInvitation model

---
+++ database/migrations/2022_11_10_140139_create_project_invitations_table.php
@@ -0,0 +1,51 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('project_invitations', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->timestamp('expires_at');
+            $table->uuid('uuid');
+            // How many users have used the invitation to add themselves to the project.
+            $table->unsignedInteger('current_uses')->default(0);
+            // How many uses are allowed before the invitation expires.
+            $table->unsignedInteger('max_uses')->nullable();
+
+            $table->integer('project_id')->unsigned();
+            $table->foreign('project_id')
+                  ->references('id')
+                  ->on('projects')
+                  ->onDelete('cascade');
+
+            $table->integer('role_id')->unsigned();
+            $table->foreign('role_id')
+                  ->references('id')
+                  ->on('roles')
+                  ->onDelete('restrict');
+        });
+
+        DB::statement('ALTER TABLE project_invitations ADD CONSTRAINT check_max_uses CHECK ("max_uses" IS NULL OR "current_uses" <= "max_uses")');
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('project_invitations');
+    }
+};
---
+++ tests/php/ProjectInvitationTest.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Biigle\Tests;
+
+use Biigle\Project;
+use Biigle\ProjectInvitation;
+use Biigle\Role;
+use Illuminate\Database\QueryException;
+use ModelTestCase;
+
+class ProjectInvitationTest extends ModelTestCase
+{
+    /**
+     * The model class this class will test.
+     */
+    protected static $modelClass = ProjectInvitation::class;
+
+    public function testAttributes()
+    {
+        $this->assertNotNull($this->model->uuid);
+        $this->assertNotNull($this->model->expires_at);
+        $this->assertNotNull($this->model->project_id);
+        $this->assertNotNull($this->model->role_id);
+        $this->assertNotNull($this->model->current_uses);
+        $this->assertNull($this->model->max_uses);
+    }
+
+    public function testMaxUsesConstraint()
+    {
+        $this->model->increment('current_uses');
+        $this->model->update(['max_uses' => 1]);
+
+        $this->expectException(QueryException::class);
+        $this->model->increment('current_uses');
+    }
+
+    public function testIsOpenUses()
+    {
+        $this->assertTrue($this->model->isOpen());
+        $this->model->max_uses = 2;
+        $this->model->current_uses = 1;
+        $this->assertTrue($this->model->isOpen());
+        $this->model->current_uses = 2;
+        $this->assertFalse($this->model->isOpen());
+    }
+
+    public function testIsOpenDate()
+    {
+        $this->assertTrue($this->model->isOpen());
+        $this->model->expires_at = '2022-11-09 14:51:00';
+        $this->assertFalse($this->model->isOpen());
+    }
+}
