0a06ce2c945595616e85cb65f822b6838b203294
Daniel Langenkämper <dlangenk@gmail.com>
2022-12-09 10:14:38 +0100

    Added graphics to the 404 error page

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -5,5 +5,8 @@
 @section('type', 'warning')
 
 @section('message')
-We could not find what you are looking for, sorry. <a href="{{URL::previous()}}">Go back.</a>
+The content you are trying to access has been eaten.<br><br>
+<img src="{{asset('assets/images/404.webp')}}"></img>
+<br>
+<a href="{{URL::previous()}}">Go back.</a>
 @endsection
