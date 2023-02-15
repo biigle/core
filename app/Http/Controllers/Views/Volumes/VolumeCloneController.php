c64cecfde18782d0222e615bfb668e768809d2e1
Leane Hecht <lhecht@techfak.uni-bielefeld.de>
2023-02-15 18:04:00 +0100

    create volume clone controller for view

---
+++ app/Http/Controllers/Views/Volumes/VolumeCloneController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Biigle\Http\Controllers\Views\Volumes;
+
+use Biigle\Http\Controllers\Views\Controller;
+use Biigle\Project;
+use Biigle\Volume;
+use Illuminate\Http\Request;
+use function Amp\Iterator\map;
+
+class VolumeCloneController extends Controller
+{
+
+    function clone(Request $request, $id)
+    {
+        $volume = Volume::findOrFail($id);
+        $this->authorize('update', $volume);
+
+        $user = $request->user();
+        $destinationProjects = $user->projects()->get()->map(function ($project) use ($user) {
+            if ($user->can('sudo') || $user->can('update', $project)) {
+                return $project;
+            }
+        });
+
+        $filesWithAllLabels = $volume->files()->with(['Labels','Annotations.labels'])->get();
+        dd($filesWithAllLabels->toArray());
+
+        return view('clone', [
+            'filename' => $volume->filename,
+            'destProjects' => $destinationProjects,
+            'files' => $filesWithAllLabels
+        ]);
+    }
+
+}
