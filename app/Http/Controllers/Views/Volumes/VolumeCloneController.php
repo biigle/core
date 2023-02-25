1e907b474f1c3ec0b85b503f5a60200c14be61e0
Leane Hecht <lhecht@techfak.uni-bielefeld.de>
2023-02-25 19:37:21 +0100

    return label tree

--- app/Http/Controllers/Views/Volumes/VolumeCloneController.php
+++ app/Http/Controllers/Views/Volumes/VolumeCloneController.php
@@ -3,6 +3,7 @@
 namespace Biigle\Http\Controllers\Views\Volumes;
 
 use Biigle\Http\Controllers\Views\Controller;
+use Biigle\LabelTree;
 use Biigle\Project;
 use Biigle\Volume;
 use Illuminate\Http\Request;
@@ -18,11 +19,22 @@ class VolumeCloneController extends Controller
         $user = $request->user();
         $destProjects = $user->projects()->where('project_role_id', \Biigle\Role::adminId())->get();
 
+        $labelTrees = LabelTree::select('id', 'name', 'version_id')
+            ->with('labels', 'version')
+            ->whereIn('id', function ($query) use ($user) {
+                $query->select('label_tree_id')
+                    ->from('label_tree_user')
+                    ->where('user_id', $user->id);
+            })
+            ->get();
+
+
         return view('volumes.clone', [
             'volume' => $volume,
             'name' => $volume->name,
             'destinationProjects' => collect($destProjects)->values(),
             'files' => collect($volume->files()->get())->values(),
+            'labelTrees' => $labelTrees
         ]);
     }
 
