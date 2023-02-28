6c5060daf9f54c6757bb8135cf323e25916ccb2e
Leane Hecht <lhecht@techfak.uni-bielefeld.de>
2023-02-28 18:16:28 +0100

    add label tree to annotaion label section

--- resources/views/volumes/clone.blade.php
+++ resources/views/volumes/clone.blade.php
@@ -50,39 +50,50 @@
                 <label>clone files</label>
 
                 <div v-if="cloneFiles">
-                <div id="file-panel" class="panel panel-default volume-files-panel">
-                    <div class="panel-heading">
-                        <div class="form-group">
-                            <label>Filename(s):&nbsp;</label>
-                            @if ($volume->isImageVolume())
-                                <input type="text" class="form-control" id="files"
-                                       placeholder="img*.jpg" v-model="filePattern" required v-on:keydown.enter="loadFilesMatchingPattern">
-                            @else
-                                <input type="text" class="form-control" id="files"
-                                       placeholder="video*.mp4" v-model="filePattern" required>
-                            @endif
+                    <div id="file-panel" class="panel panel-default volume-files-panel">
+                        <div class="panel-heading">
+                            <div class="form-group">
+                                <label>Filename(s):&nbsp;</label>
+                                @if ($volume->isImageVolume())
+                                    <input type="text" class="form-control" id="files"
+                                           placeholder="img*.jpg" v-model="filePattern" required
+                                           v-on:keydown.enter="loadFilesMatchingPattern">
+                                @else
+                                    <input type="text" class="form-control" id="files"
+                                           placeholder="video*.mp4" v-model="filePattern" required>
+                                @endif
+                            </div>
+                        </div>
+                        <div class="panel-body">
+                            <ul class="list-group files-list" v-cloak>
+                                <li v-for="file in selectedFiles" class="list-group-item"><span
+                                        class="text-muted">#<span v-text="file.id"></span></span> <span
+                                        v-text="file.filename"></span></li>
+                            </ul>
                         </div>
                     </div>
-                    <div class="panel-body">
-                        <ul class="list-group files-list" v-cloak>
-                            <li v-for="file in selectedFiles" class="list-group-item"><span class="text-muted">#<span v-text="file.id"></span></span> <span v-text="file.filename"></span></li>
-                        </ul>
+
+
+                    <div>
+                    <input type="checkbox" id="fileLabels" v-model="cloneFileLabels">
+                    <label>clone file Labels</label>
+                    <label-trees v-if="cloneFileLabels" :trees='{{$labelTrees}}' :multiselect="true"
+                                 :allow-select-siblings="true" :allow-select-children="true"
+                                 class="request-labels-well well well-sm"></label-trees>
                     </div>
-                </div>
 
+                    <input type="checkbox" id="annotations" v-model="cloneAnnotations">
+                    <label>clone annotations</label>
 
-                <BR>
-                <input type="checkbox" id="fileLabels" v-model="cloneFileLabels">
-                <label v-if="cloneFiles">clone file Labels</label>
-                <label-trees v-if="cloneFileLabels" :trees='{{$labelTrees}}' :multiselect="true" :allow-select-siblings="true" :allow-select-children="true"></label-trees>
+                    <div>
+                    <input type="checkbox" v-if="cloneAnnotations" id="annotationLabel"
+                           v-model="cloneAnnotationLabels">
+                    <label v-if="cloneAnnotations">clone annotation labels</label>
+                    <label-trees v-if="cloneAnnotations && cloneAnnotationLabels" :trees='{{$labelTrees}}' :multiselect="true"
+                                 :allow-select-siblings="true" :allow-select-children="true"
+                                 class="request-labels-well well well-sm"></label-trees>
+                    </div>
 
-                <BR>
-                <input type="checkbox" id="annotations" v-model="cloneAnnotations">
-                <label v-if="cloneFiles">clone annotations</label>
-                <BR>
-                <input type="checkbox" v-if="cloneAnnotations" id="annotationLabel"
-                       v-model="cloneAnnotationLabels">
-                <label v-if="cloneFiles && cloneAnnotations">clone annotation labels</label>
 
                 </div>
 
