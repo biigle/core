e44e748f0a62e5b105294fb20521525cd2a2e13a
Leane Hecht <lhecht@techfak.uni-bielefeld.de>
2023-02-28 17:24:52 +0100

    use labeltrees from common projects

--- app/Http/Controllers/Views/Volumes/VolumeCloneController.php
+++ app/Http/Controllers/Views/Volumes/VolumeCloneController.php
@@ -17,14 +17,24 @@ class VolumeCloneController extends Controller
         $this->authorize('update', $volume);
 
         $user = $request->user();
+
+        if ($user->can('sudo')) {
+            // Global admins have no restrictions.
+            $projectIds = $volume->projects()->pluck('id');
+        } else {
+            // Array of all project IDs that the user and the volume have in common.
+            $projectIds = Project::inCommon($user, $volume->id)->pluck('id');
+        }
+
+        // Collection of projects where cloned volume can be copied to.
         $destProjects = $user->projects()->where('project_role_id', \Biigle\Role::adminId())->get();
 
         $labelTrees = LabelTree::select('id', 'name', 'version_id')
             ->with('labels', 'version')
-            ->whereIn('id', function ($query) use ($user) {
+            ->whereIn('id', function ($query) use ($projectIds) {
                 $query->select('label_tree_id')
-                    ->from('label_tree_user')
-                    ->where('user_id', $user->id);
+                    ->from('label_tree_project')
+                    ->whereIn('project_id', $projectIds);
             })
             ->get();
 
