b0b559073265248f044af4c97039929e11e3b05e
Leane Hecht <lhecht@techfak.uni-bielefeld.de>
2023-03-14 14:43:17 +0100

    change types back to bool

--- app/Http/Requests/CloneVolume.php
+++ app/Http/Requests/CloneVolume.php
@@ -47,10 +47,10 @@ class CloneVolume extends FormRequest
             'name' => 'max:512',
             'only_files.*' => 'int|gt:0',
             'only_files' => 'array',
-            'clone_annotations' => 'int|digits_between: 0,1',
+            'clone_annotations' => 'bool',
             'only_annotation_labels.*' => 'int|gt:0',
             'only_annotation_labels' => 'array',
-            'clone_file_labels' => 'int|digits_between: 0,1',
+            'clone_file_labels' => 'bool',
             'only_file_labels.*' => 'int|gt:0',
             'only_file_labels' => 'array'
         ];
