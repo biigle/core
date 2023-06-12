ee707b199624ab5be37eebb4053b95d9a67d7628
Leane Hecht <lhecht@techfak.de>
2023-06-12 09:54:56 +0200

    Add new test case for LabelSourceController
    Check, if SoapFault exception is caught and
    HTTP code 503 is returned to the frontend instead.

--- tests/php/Http/Controllers/Api/LabelSourceControllerTest.php
+++ tests/php/Http/Controllers/Api/LabelSourceControllerTest.php
@@ -6,6 +6,7 @@ use ApiTestCase;
 use App;
 use Biigle\Tests\LabelSourceTest;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 use Mockery;
 
 class LabelSourceControllerTest extends ApiTestCase
@@ -37,5 +38,29 @@ class LabelSourceControllerTest extends ApiTestCase
         ]);
         $response->assertStatus(200);
         $response->assertExactJson([['name' => 'My Query Label']]);
+    }
+
+    public function testFindWithException()
+    {
+        $source = LabelSourceTest::create(['name' => 'my_source']);
+
+        $mock = Mockery::mock();
+        $mock->shouldReceive('find')
+            ->once()
+            ->andThrow(new \SoapFault('test', 'test')); // faultcode and faultstring parameter must to be set
+
+        App::singleton('Biigle\Services\LabelSourceAdapters\MySourceAdapter', function () use ($mock) {
+            return $mock;
+        });
+
+        $this->beGuest();
+
+        $response = $this->json('GET', "/api/v1/label-sources/{$source->id}/find", [
+            'query' => 'my query',
+        ]);
+        $response->assertStatus(Response::HTTP_SERVICE_UNAVAILABLE);
+
+
+
     }
 }
