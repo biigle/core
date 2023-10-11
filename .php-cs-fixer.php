5dc354ad67270e0be31abcffb89f26fd926ee837
Martin Zurowietz <martin@zurowietz.de>
2023-10-11 15:11:40 +0200

    Disable CS Fixer for vendor/biigle/

--- .php-cs-fixer.php
+++ .php-cs-fixer.php
@@ -2,7 +2,13 @@
 
 $finder = (new PhpCsFixer\Finder())
     ->files()
-    ->in([__DIR__.'/app', __DIR__.'/tests', __DIR__.'/config', __DIR__.'/database', __DIR__.'/vendor/biigle'])
+    ->in([
+        __DIR__.'/app',
+        __DIR__.'/tests',
+        __DIR__.'/config',
+        __DIR__.'/database',
+        // __DIR__.'/vendor/biigle',
+    ])
     ->name('*.php')
     ->ignoreDotFiles(true)
     ->ignoreVCS(true);
