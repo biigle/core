c7c73c8f16b198d4cb6ec62dc708044db8fa3780
Martin Zurowietz <martin@zurowietz.de>
2023-12-06 16:08:31 +0100

    Fix ApiTokenFactory

--- database/factories/ApiTokenFactory.php
+++ database/factories/ApiTokenFactory.php
@@ -18,7 +18,7 @@ class ApiTokenFactory extends Factory
             'owner_id' => User::factory(),
             'purpose' => $this->faker->sentence(),
             // 'password', hashed with 4 rounds as defined in phpunit.xml
-            'password' => '$2y$04$aqV2XBF34eexL9ezbQZs1eM872NWgH5MhvrmD0SC9qUbhmg9EoxJq',
+            'hash' => '$2y$04$aqV2XBF34eexL9ezbQZs1eM872NWgH5MhvrmD0SC9qUbhmg9EoxJq',
         ];
     }
 }
