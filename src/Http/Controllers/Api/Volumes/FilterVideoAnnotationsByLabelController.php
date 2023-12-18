225d0ed5804c706159dc260b39df844a7d1ff986
Leane Schlundt <lhecht@techfak.de>
2023-12-18 10:08:26 +0100

    Revert "Add annotation svgs to volume filter controller"
    This reverts commit 82413ae6b8b01f315028bfb6d770e5720be5d424.

--- src/Http/Controllers/Api/Volumes/FilterVideoAnnotationsByLabelController.php
+++ src/Http/Controllers/Api/Volumes/FilterVideoAnnotationsByLabelController.php
@@ -2,13 +2,10 @@
 
 namespace Biigle\Modules\Largo\Http\Controllers\Api\Volumes;
 
-use Biigle\Volume;
-use Biigle\VideoAnnotation;
-use Illuminate\Support\Arr;
-use Illuminate\Support\Str;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Storage;
 use Biigle\Http\Controllers\Api\Controller;
+use Biigle\VideoAnnotation;
+use Biigle\Volume;
+use Illuminate\Http\Request;
 
 class FilterVideoAnnotationsByLabelController extends Controller
 {
@@ -44,7 +41,7 @@ class FilterVideoAnnotationsByLabelController extends Controller
             $query = VideoAnnotation::query();
         }
 
-        $res = $query->join('video_annotation_labels', 'video_annotations.id', '=', 'video_annotation_labels.annotation_id')
+        return $query->join('video_annotation_labels', 'video_annotations.id', '=', 'video_annotation_labels.annotation_id')
             ->join('videos', 'video_annotations.video_id', '=', 'videos.id')
             ->where('videos.volume_id', $vid)
             ->where('video_annotation_labels.label_id', $lid)
@@ -60,31 +57,5 @@ class FilterVideoAnnotationsByLabelController extends Controller
             ->distinct()
             ->orderBy('video_annotations.id', 'desc')
             ->pluck('videos.uuid', 'video_annotations.id');
-
-            $uuids = $res->values()->unique()->all();
-            $annotationIds = $res->keys();
-    
-        foreach($uuids as $uuid){
-            $dir = fragment_uuid_path($uuid);
-            $allFiles = Storage::disk(config('largo.patch_storage_disk'))->files($dir);
-            $files = Arr::where($allFiles, fn($path, $id) => Str::endsWith($path, config('largo.annotation_format')));
-    
-            if(sizeof($files) > 0){
-                $files = collect(Arr::flatten($files));
-                foreach ($files as $file) {
-                    $id = (Str::of($file))->match('/([0-9]*).'.config('largo.annotation_format').'/')->toInteger();
-                    if($annotationIds->contains($id)){
-                        $xml = Storage::disk(config('largo.patch_storage_disk'))->get($file);
-                        $res[$id] = [$uuid,$xml];
-                    }
-                };
-            } else {
-                foreach($res as $id => $uuid) {
-                    $res[$id] = [$uuid];
-                }
-            }
-        }
-
-        return $res;
     }
 }
