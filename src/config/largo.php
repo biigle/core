cb666b9641af0d0ea631addd2456610007807ea0
Leane Schlundt <lhecht@techfak.de>
2023-12-18 10:08:27 +0100

    Revert "Add svg format to constants"
    This reverts commit b748bc03c04d8a775b534654049fc926a4fc54dc.

--- src/config/largo.php
+++ src/config/largo.php
@@ -14,11 +14,6 @@ return [
     'patch_format' => 'jpg',
 
     /*
-    | Annotation shape file format
-    */
-    'annotation_format' => 'svg',
-
-    /*
     | Padding to add to each patch in x and y direction (in px).
     */
     'patch_padding' => 10,
