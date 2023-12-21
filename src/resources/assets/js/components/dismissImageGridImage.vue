3f9f349da7fb6eed35de69f425fdee45865b72de
Leane Schlundt <lhecht@techfak.de>
2023-12-21 10:25:37 +0100

    Format code

--- src/resources/assets/js/components/dismissImageGridImage.vue
+++ src/resources/assets/js/components/dismissImageGridImage.vue
@@ -6,8 +6,8 @@
         <div class="overlay svg">
             <img @click="toggleSelect" :src="srcUrl" @error="showEmptyImage">
             <span v-html="this.svg" class="stroke" :style="this.getSVGStyle(true)"></span>
-            <span v-if="this.isPoint" v-html="this.svg" class="stroke fill" :style="this.getSVGStyle(false)"></span>        
-            <span v-else v-html="this.svg" class="stroke" :style="this.getSVGStyle(false)"></span>        
+            <span v-if="this.isPoint" v-html="this.svg" class="stroke fill" :style="this.getSVGStyle(false)"></span>
+            <span v-else v-html="this.svg" class="stroke" :style="this.getSVGStyle(false)"></span>
         </div>
         <div v-if="showAnnotationLink" class="image-buttons">
             <a :href="showAnnotationLink" target="_blank" class="image-button"
@@ -62,38 +62,38 @@ export default {
         title() {
             return this.selected ? 'Undo dismissing this annotation' : 'Dismiss this annotation';
         },
-        svg(){
+        svg() {
             return this.xml !== null ? new XMLSerializer().serializeToString(this.xml) : '';
-        }
+        },
     },
     methods: {
-        svgSrcUrl(){
-            return this.srcUrl.replace(/.[A-Za-z]*$/,'.svg');
+        svgSrcUrl() {
+            return this.srcUrl.replace(/.[A-Za-z]*$/, '.svg');
         },
         async fetchSVG() {
             await fetch(this.svgSrcUrl()).then(res => res.text()).then(data => {
                 this.setXML(data);
                 // prevent stroke to scale
-                this.addAttributeToShape("vector-effect","non-scaling-stroke");
+                this.addAttributeToShape("vector-effect", "non-scaling-stroke");
                 this.identifyPoints(); // needed for styling
-            
+
             });
         },
-        setXML(xml){
-            this.xml = new DOMParser().parseFromString(xml,"image/svg+xml");
+        setXML(xml) {
+            this.xml = new DOMParser().parseFromString(xml, "image/svg+xml");
         },
-        identifyPoints(){
+        identifyPoints() {
             this.isPoint = this.svg.includes('r="1"');
         },
-        addAttributeToShape(attribute,value){
+        addAttributeToShape(attribute, value) {
             Object.keys(this.xml.childNodes[0].childNodes).forEach((key) => {
-                this.xml.childNodes[0].childNodes[key].setAttribute(attribute,value);
+                this.xml.childNodes[0].childNodes[key].setAttribute(attribute, value);
             })
         },
-        getSVGStyle(isOutline){
-            return isOutline ? '--color: white; --width: 5px;' 
-                             : '--color: #' + this.image.label_color + '; --width: 3px;';
-        }
+        getSVGStyle(isOutline) {
+            return isOutline ? '--color: white; --width: 5px;'
+                : '--color: #' + this.image.label_color + '; --width: 3px;';
+        },
     },
     created() {
         this.fetchSVG();
