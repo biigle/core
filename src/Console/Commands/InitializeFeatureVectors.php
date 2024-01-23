40e2d7f527144ba867db44ab30d0c0a37690fe48
Martin Zurowietz <martin@zurowietz.de>
2024-01-23 15:36:58 +0100

    Update initialize feature vector command to process only one volume

--- src/Console/Commands/InitializeFeatureVectors.php
+++ src/Console/Commands/InitializeFeatureVectors.php
@@ -5,6 +5,7 @@ namespace Biigle\Modules\Largo\Console\Commands;
 use Biigle\ImageAnnotation;
 use Biigle\Modules\Largo\Jobs\InitializeFeatureVectorChunk;
 use Biigle\VideoAnnotation;
+use Biigle\Volume;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\Queue;
 
@@ -16,6 +17,7 @@ class InitializeFeatureVectors extends Command
      * @var string
      */
     protected $signature = 'largo:initialize-feature-vectors
+        {volume : ID of the volume of which the annotations should be processed}
         {--dry-run : Do not submit jobs to generate feature vectors}
         {--queue=default : Queue name to push jobs to generate feature vectors to}
         {--chunk-size=1000 : Number of annotations to process in a single job}';
@@ -34,14 +36,27 @@ class InitializeFeatureVectors extends Command
      */
     public function handle()
     {
+        $volume = Volume::findOrFail($this->argument('volume'));
+        if ($volume->isImageVolume()) {
+            $this->processImages($volume);
+        } else {
+            $this->processVideos($volume);
+        }
+    }
+
+    protected function processImages(Volume $volume)
+    {
         $chunkSize = intval($this->option('chunk-size'));
         $loopChunkSize = max($chunkSize, 10000);
 
-        $count = ImageAnnotation::count();
+        $query = ImageAnnotation::join('images', 'images.id', '=', 'image_annotations.image_id')
+            ->where('images.volume_id', $volume->id);
+
+        $count = $query->count();
         $this->info("Processing {$count} image annotations.");
         $p = $this->output->createProgressBar($count);
 
-        ImageAnnotation::select('id')
+        $query->select('image_annotations.id')
             ->chunkById($loopChunkSize, function ($chunk) use ($p, $chunkSize) {
                 $chunk->chunk($chunkSize)->each(function ($c) use ($p) {
                     $job = new InitializeFeatureVectorChunk($c->pluck('id')->all(), []);
@@ -54,12 +69,21 @@ class InitializeFeatureVectors extends Command
 
         $p->finish();
         $this->line('');
+    }
 
-        $count = VideoAnnotation::count();
+    protected function processVideos(Volume $volume)
+    {
+        $chunkSize = intval($this->option('chunk-size'));
+        $loopChunkSize = max($chunkSize, 10000);
+
+        $query = VideoAnnotation::join('videos', 'videos.id', '=', 'video_annotations.video_id')
+            ->where('videos.volume_id', $volume->id);
+
+        $count = $query->count();
         $this->info("Processing {$count} video annotations.");
         $p = $this->output->createProgressBar($count);
 
-        VideoAnnotation::select('id')
+        $query->select('video_annotations.id')
             ->chunkById($loopChunkSize, function ($chunk) use ($p, $chunkSize) {
                 $chunk->chunk($chunkSize)->each(function ($c) use ($p) {
                     $job = new InitializeFeatureVectorChunk([], $c->pluck('id')->all());
