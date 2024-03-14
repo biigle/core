2b44bae5553b883b93c1986716d477953141ef19
Martin Zurowietz <martin@zurowietz.de>
2024-03-14 14:35:22 +0100

    Implement a way to extend metadata parsers from a module

--- app/Services/MetadataParsing/ParserFactory.php
+++ app/Services/MetadataParsing/ParserFactory.php
@@ -3,6 +3,7 @@
 namespace Biigle\Services\MetadataParsing;
 
 use SplFileInfo;
+use Exception;
 
 class ParserFactory
 {
@@ -15,6 +16,9 @@ class ParserFactory
         ],
     ];
 
+    /**
+     * Get the first parser that recognizes the file.
+     */
     public static function getParserForFile(SplFileInfo $file, string $type): ?MetadataParser
     {
         $parsers = self::$parsers[$type] ?? [];
@@ -26,5 +30,17 @@ class ParserFactory
         }
 
         return null;
+    }
+
+    /**
+     * Add a new metadata parser to the list of known parsers.
+     */
+    public static function extend(string $parserClass, string $type): void
+    {
+        if (!in_array(MetadataParser::class, class_parents($parserClass))) {
+            throw new Exception("A metadata parser must extend ".MetadataParser::class);
+        }
+
+        self::$parsers[$type][] = $parserClass;
     }
 }
--- tests/php/Services/MetadataParsing/ParserFactoryTest.php
+++ tests/php/Services/MetadataParsing/ParserFactoryTest.php
@@ -3,8 +3,10 @@
 namespace Biigle\Tests\Services\MetadataParsing;
 
 use Biigle\Services\MetadataParsing\ImageCsvParser;
+use Biigle\Services\MetadataParsing\MetadataParser;
 use Biigle\Services\MetadataParsing\ParserFactory;
 use Biigle\Services\MetadataParsing\VideoCsvParser;
+use Biigle\Services\MetadataParsing\VolumeMetadata;
 use Symfony\Component\HttpFoundation\File\File;
 use TestCase;
 
@@ -36,5 +38,38 @@ class ParserFactoryTest extends TestCase
         $file = new File(__DIR__."/../../../files/image-metadata.csv");
         $parser = ParserFactory::getParserForFile($file, 'test');
         $this->assertNull($parser);
+    }
+
+    public function testExtend()
+    {
+        ParserFactory::extend(TestParser::class, 'image');
+        $this->assertContains(TestParser::class, ParserFactory::$parsers['image']);
+
+        $this->expectException(\Exception::class);
+        ParserFactory::extend(TestParser2::class, 'image');
+    }
+}
+
+class TestParser extends MetadataParser {
+    public function recognizesFile(): bool
+    {
+        return false;
+    }
+
+    public function getMetadata(): VolumeMetadata
+    {
+        return new VolumeMetadata;
+    }
+}
+
+class TestParser2 {
+    public function recognizesFile(): bool
+    {
+        return false;
+    }
+
+    public function getMetadata(): VolumeMetadata
+    {
+        return new VolumeMetadata;
     }
 }
