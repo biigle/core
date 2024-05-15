13cbf4f768380fa5c2cb923d667546d6b3f08f0f
Martin Zurowietz <martin@zurowietz.de>
2024-05-15 10:56:53 +0200

    Implement more advanced spam protection in the sign up form
    If we still need more protection, we can switch to https://github.com/spatie/laravel-honeypot
    and use it's even more advanced features.

---
+++ resources/assets/js/core/registerForm.vue
@@ -0,0 +1,21 @@
+<script>
+export default {
+    data() {
+        return {
+            form: false,
+            token: '',
+        };
+    },
+    methods: {
+        showForm() {
+            this.form = true;
+        },
+    },
+    created() {
+        this.token = document.querySelector('meta[name="csrf-token"]').content;
+    },
+    mounted() {
+        this.$refs.ht.appendChild(this.$refs.hs.firstChild);
+    },
+};
+</script>
