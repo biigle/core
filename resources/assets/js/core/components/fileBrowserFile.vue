03c556929b1629a40c30781af06540efbf936644
Leane Schlundt <lhecht@techfak.de>
2024-06-10 13:49:20 +0200

    Fix file browser view
    Prevent displaying file names twice on review page.

--- resources/assets/js/core/components/fileBrowserFile.vue
+++ resources/assets/js/core/components/fileBrowserFile.vue
@@ -4,22 +4,21 @@
     :class="classObject"
     @click="handleClick($event)"
     >
-    <a
-        v-if="file.url"
-        :href="file.url"
-        :title="viewTitle"
-        >
+    <div v-if="file.url">
+        <a :href="file.url" :title="viewTitle">
         <i class="fa fa-file"></i> {{file.name}}
-    </a>
-
-    <span v-if="hasError" :title="file.name" class="text-warning">
-        <i class="fa fa-exclamation-triangle"></i> {{file.name}}
-    </span>
-    <span v-else :title="file.name">
-        <i v-if="hasInfo" class="fa fa-info-circle"></i>
-        <i v-else class="fa fa-file"></i>
-        {{file.name}}
-    </span>
+        </a>
+    </div>
+    <div v-else>
+        <span v-if="hasError" :title="file.name" class="text-warning">
+            <i class="fa fa-exclamation-triangle"></i> {{file.name}}
+        </span>
+        <span v-else :title="file.name">
+            <i v-if="hasInfo" class="fa fa-info-circle"></i>
+            <i v-else class="fa fa-file"></i>
+            {{file.name}}
+        </span>
+    </div>
 
     <button
         v-if="removable"
