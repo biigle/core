4be4cc37d314db09ae872a23475441ac667867aa
Leane Schlundt <lhecht@techfak.de>
2024-06-18 09:46:10 +0200

    Disable button on loading

--- resources/views/notifications/index.blade.php
+++ resources/views/notifications/index.blade.php
@@ -21,7 +21,7 @@
         <div id="notifications-list" class="col-sm-9 col-md-7 col-md-offset-1" v-cloak>
             @if (!$all)
                 <p v-cloak v-if="hasUnreadNotifications" class="clearfix">
-                    <button class="btn btn-default btn-s pull-right" v-on:click="markAllAsRead">Mark all as read</button>
+                    <button class="btn btn-default btn-s pull-right" v-on:click="markAllAsRead" v-bind:disabled="isLoading">Mark all as read</button>
                 </p>
             @endif
             <notification v-for="item in notifications" v-bind:key="item.id" v-bind:item="item" v-bind:remove-item="{{$all ? 'false': 'true'}}" inline-template>
