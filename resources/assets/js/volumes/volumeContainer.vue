034347200af522c9caa39f2af17032e7d95fe7bd
Leane Schlundt <lhecht@techfak.de>
2024-07-04 12:48:18 +0200

    Update filename of video thumbnails

--- resources/assets/js/volumes/volumeContainer.vue
+++ resources/assets/js/volumes/volumeContainer.vue
@@ -273,7 +273,9 @@ export default {
             let thumbnailUrl;
             if (thumbCount > 1) {
                 thumbnailUrl = Array.from(Array(thumbCount).keys()).map(function (i) {
-                    return thumbUri.replace(':uuid', transformUuid(fileUuids[id]) + '/' + i);
+                    let idx = (i+1).toString();
+                    let filename = "frame" + "0".repeat(4-idx.length) + idx;
+                    return thumbUri.replace(':uuid', transformUuid(fileUuids[id]) + '/' + filename);
                 });
             } else {
                 thumbnailUrl = thumbUri.replace(':uuid', transformUuid(fileUuids[id]));
