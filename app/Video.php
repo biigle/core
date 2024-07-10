cd6082a91ef532d57561b3e6e7150029d11eea0c
Leane Schlundt <lhecht@techfak.de>
2024-07-10 09:20:05 +0200

    Revert "Update thumbnaile file names to prevent sorting"
    This reverts commit 5888d015d02cbae24211fce0d22e93206e53680e.

--- app/Video.php
+++ app/Video.php
@@ -141,9 +141,8 @@ class Video extends VolumeFile
      */
     public function getThumbnailsAttribute()
     {
-        $zeros = fn ($i) => str_repeat("0", 4-strlen(strval($i)));
         return collect(range(0, config('videos.thumbnail_count') - 1))
-            ->map(fn ($i) => "{$this->uuid}/".$zeros($i)."{$i}");
+            ->map(fn ($i) => "{$this->uuid}/{$i}");
     }
 
     /**
