414dcc430ea238dce9856bc8b7e9eb6524dd9ef7
Leane Schlundt <lhecht@techfak.de>
2024-07-17 08:10:44 +0200

    Revert changes in test class from f50e916b

--- tests/php/Http/Controllers/Views/Videos/VideoControllerTest.php
+++ tests/php/Http/Controllers/Views/Videos/VideoControllerTest.php
@@ -26,19 +26,4 @@ class VideoControllerTest extends ApiTestCase
         $this->beUser();
         $this->get('videos/999')->assertRedirect('/videos/999/annotations');
     }
-
-    public function testVideoWithoutDimensions()
-    {
-        $this->beGuest();
-        $id = $this->volume(['media_type_id' => MediaType::videoId()])->id;
-
-        $video = VideoTest::create(['volume_id' => $id, 'height' => NULL, 'width' => NULL]);
-        $this->get("videos/{$video->id}/annotations")->assertStatus(200);
-
-        $video = VideoTest::create(['volume_id' => $id, 'height' => 0, 'width' => 0]);
-        $this->get("videos/{$video->id}/annotations")->assertStatus(200);
-
-        $video = VideoTest::create(['volume_id' => $id]);
-        $this->get("videos/{$video->id}/annotations")->assertStatus(200);
-    }
 }
