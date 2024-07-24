841d7c53d1fc21e8cbbdbda85a6e6fe6a7e67d04
Daniel Langenkämper <dlangenk@gmail.com>
2024-07-24 11:04:23 +0200

    use css instead of php for truncating

--- resources/views/volumes/images/index.blade.php
+++ resources/views/volumes/images/index.blade.php
@@ -1,16 +1,17 @@
 @extends('app')
-@section('title', $image->filename)
+@section('full-navbar', true)
+@section('title', "$image->filename")
 
 @section('content')
 <div class="container">
     <div class="row">
-        <h2 class="col-lg-12 clearfix">
-            {{ mb_strimwidth($image->filename, 0, 63, '...')}}
-            <span class="pull-right">
+        <h2 class="col-lg-12 clearfix annotations-breadcrumb">
+            {{ $image->filename }}
+        </h2>
+        <span class="pull-right">
                 <a href="{{route('volume', $volume->id)}}" title="Back to {{ $volume->name }}" class="btn btn-default">back</a>
                 @mixin('imagesIndexButtons')
             </span>
-        </h2>
     </div>
     <div class="row">
         <div class="col-sm-6 col-lg-4">
@@ -29,8 +30,14 @@
 @endsection
 
 @section('navbar')
-<div class="navbar-text navbar-volumes-breadcrumbs">
-    @include('volumes.partials.projectsBreadcrumb', ['projects' => $volume->projects])/ <a href="{{route('volume', $volume->id)}}" class="navbar-link" title="Show volume {{$volume->name}}">{{$volume->name}}</a>
-    / <strong title="{{mb_strimwidth($image->filename, 0, 63, '...')}}">{{mb_strimwidth($image->filename, 0, 63, '...')}}</strong> @include('volumes.partials.annotationSessionIndicator')
+<div class="navbar-text">
+    <div class="annotations-project-dd">
+        @include('volumes.partials.projectsBreadcrumb', ['projects' => $volume->projects]) /
+    </div>
+    <div class="annotations-breadcrumb">
+    <a href="{{route('volume', $volume->id)}}" class="navbar-link" title="Show volume {{$volume->name}}">{{$volume->name}}</a> /
+            <strong>{{$image->filename}}</strong>
+    </div>
+    @include('volumes.partials.annotationSessionIndicator')
 </div>
 @endsection
