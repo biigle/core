f83ca91718e503805abaca12a2ab72897c624828
Martin Zurowietz <martin@zurowietz.de>
2024-07-24 12:54:16 +0200

    Polish handling of long image filenames

--- resources/views/volumes/images/index.blade.php
+++ resources/views/volumes/images/index.blade.php
@@ -1,17 +1,16 @@
 @extends('app')
-@section('full-navbar', true)
-@section('title', "$image->filename")
+@section('title', $image->filename)
 
 @section('content')
 <div class="container">
     <div class="row">
-        <h2 class="col-lg-12 clearfix annotations-breadcrumb">
+        <h2 class="col-lg-12 clearfix file-info-title" title="{{ $image->filename }}">
             {{ $image->filename }}
-        </h2>
-        <span class="pull-right">
+            <span class="pull-right">
                 <a href="{{route('volume', $volume->id)}}" title="Back to {{ $volume->name }}" class="btn btn-default">back</a>
                 @mixin('imagesIndexButtons')
             </span>
+        </h2>
     </div>
     <div class="row">
         <div class="col-sm-6 col-lg-4">
@@ -30,13 +29,13 @@
 @endsection
 
 @section('navbar')
-<div class="navbar-text">
+<div class="navbar-text navbar-volumes-breadcrumbs">
     <div class="annotations-project-dd">
         @include('volumes.partials.projectsBreadcrumb', ['projects' => $volume->projects]) /
     </div>
     <div class="annotations-breadcrumb">
-    <a href="{{route('volume', $volume->id)}}" class="navbar-link" title="Show volume {{$volume->name}}">{{$volume->name}}</a> /
-            <strong>{{$image->filename}}</strong>
+        <a href="{{route('volume', $volume->id)}}" class="navbar-link" title="Show volume {{$volume->name}}">{{$volume->name}}</a> /
+        <strong title="{{$image->filename}}">{{$image->filename}}</strong>
     </div>
     @include('volumes.partials.annotationSessionIndicator')
 </div>
