981b0881e76fedfcac7c2e1f6194b7fc920c0194
Daniel Langenkämper <dlangenk@gmail.com>
2024-07-25 15:46:45 +0200

    change timeline to show month instead of years

--- app/Http/Controllers/Views/Projects/ProjectStatisticsController.php
+++ app/Http/Controllers/Views/Projects/ProjectStatisticsController.php
@@ -112,8 +112,8 @@ class ProjectStatisticsController extends Controller
 
         $annotationTimeSeries = $baseQuery->clone()
             ->leftJoin('users', 'users.id', '=', "{$type}_annotation_labels.user_id")
-            ->selectRaw("{$type}_annotation_labels.user_id, concat(users.firstname, ' ', users.lastname) as fullname, count({$type}_annotation_labels.id), EXTRACT(YEAR from {$type}_annotations.created_at)::integer as year")
-            ->groupBy("{$type}_annotation_labels.user_id", 'fullname', 'year')
+            ->selectRaw("{$type}_annotation_labels.user_id, concat(users.firstname, ' ', users.lastname) as fullname, count({$type}_annotation_labels.id), concat(EXTRACT(YEAR from {$type}_annotations.created_at)::integer,'-',EXTRACT(MONTH from {$type}_annotations.created_at)::integer) as yearmonth")
+            ->groupBy("{$type}_annotation_labels.user_id", 'fullname', 'yearmonth')
             ->orderBy("{$type}_annotation_labels.user_id")
             ->get();
 
