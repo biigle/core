c6bc7943929bc56f1f021c78d8ad4a14683bcfcf
Leane Schlundt <lhecht@techfak.de>
2024-10-09 11:43:41 +0200

    Set feature style to null to get faster feedback

--- resources/assets/js/annotations/ol/MagicWandInteraction.js
+++ resources/assets/js/annotations/ol/MagicWandInteraction.js
@@ -183,14 +183,14 @@ class MagicWandInteraction extends PointerInteraction {
     handleUpEvent() {
         this.currentThreshold = this.colorThreshold;
 
-        if (this.isShowingCross) {
-            this.sketchSource.removeFeature(this.sketchFeature);
-        } else {
-            this.dispatchEvent({type: 'drawend', feature: this.sketchFeature});
+        if (!this.isShowingCross) {
+            this.sketchFeature.setStyle(null);
+            this.dispatchEvent({ type: 'drawend', feature: this.sketchFeature });
             // Add feature to annotation source to prevent flickering feature
             this.source.addFeature(this.sketchFeature);
-            this.sketchSource.removeFeature(this.sketchFeature);
         }
+        
+        this.sketchSource.removeFeature(this.sketchFeature);
 
         this.sketchFeature = null;
 
