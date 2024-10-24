8da99fe9eb85e3b62242ca0e79452ca703f6c9c0
Leane Schlundt <lhecht@techfak.de>
2024-10-24 09:52:58 +0200

    Add test case for fuzzy search

--- tests/php/Http/Controllers/Api/ProjectsAttachableVolumesControllerTest.php
+++ tests/php/Http/Controllers/Api/ProjectsAttachableVolumesControllerTest.php
@@ -46,4 +46,42 @@ class ProjectsAttachableVolumesControllerTest extends ApiTestCase
             'thumbnailsUrl' => [],
         ]]);
     }
+
+    public function testIndexFuzzySearch()
+    {
+        $validVolume = VolumeTest::create(['name' => 'my test']);
+        $validProject = ProjectTest::create();
+        $validProject->addVolumeId($validVolume->id);
+        $validProject->addUserId($this->admin()->id, Role::adminId());
+
+        $invalidVolume = VolumeTest::create(['name' => 'my test']);
+        $invalidProject = ProjectTest::create();
+        $invalidProject->addVolumeId($invalidVolume->id);
+        $invalidProject->addUserId($this->admin()->id, Role::editorId());
+
+        $existingVolume = $this->volume();
+        $validProject->addVolumeId($existingVolume->id); // should not be returned
+        $id = $this->project()->id;
+
+        $this->doTestApiRoute('GET', "/api/v1/projects/{$id}/attachable-volumes/test");
+
+        $this->beEditor();
+        $response = $this->get("/api/v1/projects/{$id}/attachable-volumes/test");
+        $response->assertStatus(403);
+
+        $this->beAdmin();
+        $response = $this->get("/api/v1/projects/{$id}/attachable-volumes/test");
+        $response->assertStatus(200);
+
+        $this->assertCount(1, $response->decodeResponseJson());
+        $response->assertExactJson([[
+            'id' => $validVolume->id,
+            'name' => $validVolume->name,
+            'media_type_id' => $validVolume->media_type_id,
+            'media_type' => $validVolume->mediaType,
+            'updated_at' => $validVolume->updated_at->toJson(),
+            'thumbnailUrl' => null,
+            'thumbnailsUrl' => [],
+        ]]);
+    }
 }
