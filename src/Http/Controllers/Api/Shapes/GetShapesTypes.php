3dae2c3ff0001dd1f65959a1967ced2e2b06dc2e
Davide Brembilla <davide.brembilla98@gmail.com>
2024-10-31 06:55:19 +0100

    Fix the Shapes docstring

--- src/Http/Controllers/Api/Shapes/GetShapesTypes.php
+++ src/Http/Controllers/Api/Shapes/GetShapesTypes.php
@@ -12,14 +12,7 @@ class GetShapesTypes extends Controller
     /**
      * Get all shapes currently available in Biigle
      *
-     * @api {get} projects/:tid/video-annotations/filter/label/:lid Get video annotations with a label
-     * @apiGroup Projects
-     * @apiName ShowProjectsVideoAnnotationsFilterLabels
-     * @apiParam {Number} pid The project ID
-     * @apiParam {Number} lit The Label ID
-     * @apiParam (Optional arguments) {Number} take Number of video annotations to return. If this parameter is present, the most recent annotations will be returned first. Default is unlimited.
-     * @apiPermission projectMember
-     * @apiDescription Returns a map of video annotation IDs to their video UUIDs.
+     * @api {get} shapes
      *
      * @param Request $request
      */
