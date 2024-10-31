c2af2da63cca4bd85680fd4580310c2fd001815a
Davide Brembilla <davide.brembilla98@gmail.com>
2024-10-31 10:45:40 +0100

    Adds tests for filtering annotations

--- tests/Http/Controllers/Api/Projects/FilterImageAnnotationsByLabelControllerTest.php
+++ tests/Http/Controllers/Api/Projects/FilterImageAnnotationsByLabelControllerTest.php
@@ -5,6 +5,8 @@ namespace Biigle\Tests\Modules\Largo\Http\Controllers\Api\Projects;
 use ApiTestCase;
 use Biigle\Tests\ImageAnnotationLabelTest;
 use Biigle\Tests\ImageAnnotationTest;
+use Biigle\Tests\UserTest;
+use Biigle\Tests\ShapeTest;
 use Biigle\Tests\ImageTest;
 
 class FilterImageAnnotationsByLabelControllerTest extends ApiTestCase
@@ -73,5 +75,40 @@ class FilterImageAnnotationsByLabelControllerTest extends ApiTestCase
         $this->get("/api/v1/projects/{$id}/image-annotations/filter/label/{$l1->label_id}")
             ->assertStatus(200)
             ->assertExactJson([$a1->id => $image->uuid]);
+    }
+
+    public function testFilters()
+    {
+        $id = $this->project()->id;
+
+        $image = ImageTest::create(['volume_id' => $this->volume()->id]);
+
+        $u1 = UserTest::create();
+        $u2 = UserTest::create();
+
+        $s1 = ShapeTest::create();
+        $s2 = ShapeTest::create();
+
+        $a1 = ImageAnnotationTest::create(['image_id' => $image->id, 'shape_id' =>$s1->id]);
+        $a2 = ImageAnnotationTest::create(['image_id' => $image->id, 'shape_id' =>$s1->id]);
+        $a3 = ImageAnnotationTest::create(['image_id' => $image->id, 'shape_id' =>$s2->id]);
+
+        $l1 = ImageAnnotationLabelTest::create(['annotation_id' => $a1->id, 'user_id' =>$u1->id]);
+        $l2 = ImageAnnotationLabelTest::create(['annotation_id' => $a2->id, 'label_id' => $l1->label_id, 'user_id' =>$u2->id]);
+        $l3 = ImageAnnotationLabelTest::create(['annotation_id' => $a3->id, 'label_id' => $l1->label_id, 'user_id' =>$u2->id]);
+
+        $this->beEditor();
+
+        //Case 1: filter by shape
+        $this->get("/api/v1/projects/{$id}/image-annotations/filter/label/{$l1->label_id}?shape_id={$s1->id}")
+            ->assertExactJson([$a1->id => $image->uuid, $a2->id => $image->uuid]);
+
+        //Case 2: filter by user
+        $this->get("/api/v1/projects/{$id}/image-annotations/filter/label/{$l1->label_id}?user_id={$u2->id}")
+            ->assertExactJson([$a2->id => $image->uuid, $a3->id => $image->uuid]);
+
+        //Case 3: filter by shape and user
+        $this->get("/api/v1/projects/{$id}/image-annotations/filter/label/{$l1->label_id}?shape_id={$s2->id}&user_id={$u2->id}")
+            ->assertExactJson([$a3->id => $image->uuid]);
     }
 }
