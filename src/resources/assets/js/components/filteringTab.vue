f752fad2d78c50f233b17fc40290fef1a820374a
Davide Brembilla <davide.brembilla98@gmail.com>
2024-11-08 07:52:00 +0100

    Remove console statement left by mistake

--- src/resources/assets/js/components/filteringTab.vue
+++ src/resources/assets/js/components/filteringTab.vue
@@ -35,7 +35,6 @@ export default {
       this.activeFilters.splice(key, 1)
       //If its the first filter to be removed, remove the 'And' or 'Or'
       if (key == 0){
-        console.log(this.activeFilters[0].name.split(' ', 1))
         this.activeFilters[0].name = this.activeFilters[0].name.substring(
           this.activeFilters[0].name.indexOf(' ', 0)
         )
