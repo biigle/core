83579a17105edfd98ed07f563086468d4d880d88
Leane Schlundt <lhecht@techfak.de>
2024-11-20 09:48:01 +0100

    Fix missing merge of image and video labels

--- src/resources/assets/js/mixins/largoContainer.vue
+++ src/resources/assets/js/mixins/largoContainer.vue
@@ -496,7 +496,7 @@ export default {
             let res = responses[0].body.length != 0 ? responses[0] : responses[1];
             let type = responses[0].body.length != 0 ? IMAGE_ANNOTATION : VIDEO_ANNOTATION;
             // Only annotation labels are required for flat label tab
-            this.annotationLabels = res.body.map((al) => al.labels);
+            this.annotationLabels = this.annotationLabels.concat(res.body.map((al) => al.labels));
             // Process annotations to use them later in annotationsChache
             let groupedAnnotation = {};
             res.body.forEach((al) => {
