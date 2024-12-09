62335123c2db11cbf09f70274846c179980375d5
Leane Schlundt <lhecht@techfak.de>
2024-12-09 07:51:39 +0100

    Remove unused code

--- resources/views/videos/show/sidebar-settings.blade.php
+++ resources/views/videos/show/sidebar-settings.blade.php
@@ -7,7 +7,7 @@
             <div class="annotator-tab settings-tab">
                 <div class="sidebar-tab__section">
                     <button v-if="crossOriginError" class="btn btn-default" title="Screenshots are not available for remote videos without cross-origin resource sharing" disabled="disabled" ><span class="fa fa-camera" aria-hidden="true"></span> Capture screenshot</button>
-                    <screenshot-button v-else inline-template :is-image="false">
+                    <screenshot-button v-else inline-template>
                         <button class="btn btn-default" title="Get a screenshot of the visible area 𝗣" v-on:click="capture"><span class="fa fa-camera" aria-hidden="true"></span> Capture screenshot</button>
                     </screenshot-button>
                 </div>
