0a3ae1222a80263d5374607d55097c333e20da03
Aya Bezine <abezine@techfak.de>
2024-12-11 16:47:33 +0100

    Highlight settings tab when annotation opacity is 0 Resolves https://github.com/biigle/core/issues/457

--- resources/assets/js/annotations/annotatorContainer.vue
+++ resources/assets/js/annotations/annotatorContainer.vue
@@ -163,7 +163,11 @@ export default {
         },
         annotationCount() {
             return this.annotations.length;
+        },
+        highlightSettingsTab() {
+            return this.annotationOpacity === 0;
         }
+    
     },
     methods: {
         getImageAndAnnotationsPromises(id) {
