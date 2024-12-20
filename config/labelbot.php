89bc3c8022bb9dde8f696c3fced1eb6f19cd3450
gkourie <g.kourie96@gmail.com>
2024-12-20 17:15:59 +0100

    Fix lint

--- config/labelbot.php
+++ config/labelbot.php
@@ -8,7 +8,7 @@ return [
     |--------------------------------------------------------------------------
     |
     | The value of K determines the number of nearest neighbors to consider
-    | when performing a KNN search. This is used in both Approximate 
+    | when performing a KNN search. This is used in both Approximate
     | Nearest Neighbor (ANN) and Exact KNN searches.
     */
     'K' => 100,
@@ -19,8 +19,8 @@ return [
     |--------------------------------------------------------------------------
     |
     | The value of N specifies how many top labels should be returned from
-    | the search results. After performing a KNN or ANN search, the top N 
-    | labels (based on their distance to the query vector) will be selected 
+    | the search results. After performing a KNN or ANN search, the top N
+    | labels (based on their distance to the query vector) will be selected
     | and returned.
     */
     'N' => 3,
