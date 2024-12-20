602b2538f0a10bb4b57076729f52a88f33c6d8f8
gkourie <g.kourie96@gmail.com>
2024-12-20 17:14:06 +0100

    Expand labelbot config comments

--- config/labelbot.php
+++ config/labelbot.php
@@ -3,12 +3,25 @@
 return [
 
     /*
-    | K for KNN
+    |--------------------------------------------------------------------------
+    | K for KNN (K-Nearest Neighbors)
+    |--------------------------------------------------------------------------
+    |
+    | The value of K determines the number of nearest neighbors to consider
+    | when performing a KNN search. This is used in both Approximate 
+    | Nearest Neighbor (ANN) and Exact KNN searches.
     */
     'K' => 100,
 
     /*
-    | N for top N labels
+    |--------------------------------------------------------------------------
+    | N for Top N Labels
+    |--------------------------------------------------------------------------
+    |
+    | The value of N specifies how many top labels should be returned from
+    | the search results. After performing a KNN or ANN search, the top N 
+    | labels (based on their distance to the query vector) will be selected 
+    | and returned.
     */
     'N' => 3,
 ];
