0398b6c26592829fe4b37aa435139d9518b2b976
Leane Schlundt <lhecht@techfak.de>
2025-01-30 08:54:28 +0100

    Filter labels if annotation session is active

--- src/Http/Controllers/Api/Volumes/VolumeAnnotationLabels.php
+++ src/Http/Controllers/Api/Volumes/VolumeAnnotationLabels.php
@@ -2,22 +2,24 @@
 
 namespace Biigle\Modules\Largo\Http\Controllers\Api\Volumes;
 
-use Biigle\Label;
 use Biigle\Volume;
+use Biigle\ImageAnnotation;
+use Biigle\VideoAnnotation;
+use Illuminate\Http\Request;
 use Biigle\Http\Controllers\Api\Controller;
 
 class VolumeAnnotationLabels extends Controller
 {
     /**
      * Get all annotation labels and annotation count for a given volume
-     * 
+     *
      * @api {get} volumes/:vid/label-count Get annotation labels with a annotation count
      * @apiGroup Volumes
      * @apiName test
      * @apiParam {Number} id The Volume ID
      * @apiPermission projectMember
      * @apiDescription Returns a collection of annotation labels and their counts in the volume
-     * 
+     *
      * @apiSuccessExample {json} Success response:
      * [{"id":1,
      * "name":"a",
@@ -28,26 +30,46 @@ class VolumeAnnotationLabels extends Controller
      * @param int $id Volume ID
      * @return \Illuminate\Database\Eloquent\Collection
      */
-    public function getVolumeAnnotationLabels($id)
+    public function getVolumeAnnotationLabels(Request $request, $id)
     {
         $volume = Volume::findOrFail($id);
         $this->authorize('access', $volume);
+        $this->validate($request, ['take' => 'integer']);
+        $take = $request->input('take');
+        $isImageVolume = $volume->isImageVolume();
 
-        if ($volume->isImageVolume()) {
-            $labelQuery = Label::query()
-                ->join('image_annotation_labels', 'labels.id', '=', 'image_annotation_labels.label_id')
-                ->join('image_annotations', 'image_annotation_labels.annotation_id', '=', 'image_annotations.id')
+        $session = $volume->getActiveAnnotationSession($request->user());
+
+        if ($session) {
+            $query = $isImageVolume ? ImageAnnotation::allowedBySession($session, $request->user()) :
+                VideoAnnotation::allowedBySession($session, $request->user());
+        } else {
+            $query = $isImageVolume ? ImageAnnotation::query() : VideoAnnotation::query();
+        }
+
+        if ($isImageVolume) {
+            $labelQuery = $query
+                ->join('image_annotation_labels', 'image_annotations.id', '=', 'image_annotation_labels.annotation_id')
+                ->join('labels', 'image_annotation_labels.label_id', '=', 'labels.id')
                 ->join('images', 'image_annotations.image_id', '=', 'images.id')
                 ->where('images.volume_id', '=', $id);
         } else {
-            $labelQuery = Label::query()
-                ->join('video_annotation_labels', 'labels.id', '=', 'video_annotation_labels.label_id')
-                ->join('video_annotations', 'video_annotation_labels.annotation_id', '=', 'video_annotations.id')
+            $labelQuery = $query
+                ->join('video_annotation_labels', 'video_annotations.id', '=', 'video_annotation_labels.annotation_id')
+                ->join('labels', 'video_annotation_labels.label_id', '=', 'labels.id')
                 ->join('videos', 'video_annotations.video_id', '=', 'videos.id')
                 ->where('videos.volume_id', '=', $id);
         }
 
         return $labelQuery
+            ->when($session, function ($query) use ($session, $request) {
+                if ($session->hide_other_users_annotations) {
+                    $query->where('image_annotation_labels.user_id', $request->user()->id);
+                }
+            })
+            ->when(!is_null($take), function ($query) use ($take) {
+                return $query->take($take);
+            })
             ->selectRaw('labels.id, labels.name, labels.color, labels.label_tree_id, count(labels.id) as count')
             ->groupBy(['labels.id', 'labels.name', 'labels.color', 'labels.label_tree_id'])
             ->orderBy('labels.name')
