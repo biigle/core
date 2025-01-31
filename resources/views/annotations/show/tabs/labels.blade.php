f5120cba156399000e564e503c730b9b74af6545
gkourie <g.kourie96@gmail.com>
2025-01-31 14:46:41 +0100

    Fix handle LabelBOT method name

--- resources/views/annotations/show/tabs/labels.blade.php
+++ resources/views/annotations/show/tabs/labels.blade.php
@@ -2,7 +2,7 @@
     <labels-tab v-on:select="handleSelectedLabel" v-on:labelbot="handleLabelBOT" v-cloak inline-template>
         <div class="labels-tab">
             <div class="labelBOT-button">
-                <power-toggle :active="labelBOTIsOn" title-off="Activate LabelBOT" title-on="Deactivate LabelBOT" v-on:on="handleLabelBOTOn" v-on:off="handleLabelBOTOff">LabelBOT</power-toggle>
+                <power-toggle :active="labelBOTIsOn" title-off="Activate LabelBOT" title-on="Deactivate LabelBOT" v-on:on="handleLabelBOT" v-on:off="handleLabelBOT">LabelBOT</power-toggle>
             </div>
             <div class="labels-tab__trees">
                 <label-trees :trees="labelTrees" :show-favourites="true" :label-b-o-t-is-on="labelBOTIsOn" v-on:select="handleSelectedLabel" v-on:deselect="handleDeselectedLabel" v-on:clear="handleDeselectedLabel"></label-trees>
