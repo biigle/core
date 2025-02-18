14495554f7949c63938563498d2fbfe749907948
Davide Brembilla <davide.brembilla98@gmail.com>
2025-02-18 11:21:54 +0100

    Improve test comment further

--- tests/Http/Controllers/Api/Projects/FilterVideoAnnotationsByLabelControllerTest.php
+++ tests/Http/Controllers/Api/Projects/FilterVideoAnnotationsByLabelControllerTest.php
@@ -119,7 +119,7 @@ class FilterVideoAnnotationsByLabelControllerTest extends ApiTestCase
         $this->get("/api/v1/projects/{$id}/video-annotations/filter/label/{$l1->label_id}?user_id[]={$u1->id}&user_id[]={$u2->id}&union=1")
             ->assertExactJson([$a1->id => $video->uuid, $a2->id => $video->uuid, $a3->id => $video->uuid]);
 
-        //Case 6: combine uncompatible filters
+        //Case 6: combine incompatible filters: annotations should be of user1 and user2 at the same time
         $this->get("/api/v1/projects/{$id}/video-annotations/filter/label/{$l1->label_id}?user_id[]={$u1->id}&user_id[]={$u2->id}&union=0")
             ->assertExactJson([]);
 
