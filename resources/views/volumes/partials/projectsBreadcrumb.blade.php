84c64b7f22a5061b2f44d83636231fca7bef2e06
Martin Zurowietz <martin@zurowietz.de>
2025-02-21 09:37:34 +0100

    Fix project breadcrumb dropdown for Vue 3

--- resources/views/volumes/partials/projectsBreadcrumb.blade.php
+++ resources/views/volumes/partials/projectsBreadcrumb.blade.php
@@ -1,12 +1,14 @@
 @if ($projects->count() > 1)
-    <dropdown tag="span" id="projects-breadcrumb">
-        <a href="#" onclick="event.preventDefault()" class="dropdown-toggle navbar-link">Projects <span class="caret"></span></a>
-        <template #dropdown>
-            @foreach ($projects as $project)
-                <li><a href="{{route('project', $project->id)}}">{{$project->name}}</a></li>
-            @endforeach
-        </template>
-    </dropdown>
+    <span id="projects-breadcrumb">
+        <dropdown tag="span">
+            <a href="#" v-on:click.prevent class="dropdown-toggle navbar-link">Projects <span class="caret"></span></a>
+            <template #dropdown>
+                @foreach ($projects as $project)
+                    <li><a href="{{route('project', $project->id)}}">{{$project->name}}</a></li>
+                @endforeach
+            </template>
+        </dropdown>
+    </span>
 @else
     <a href="{{route('project', $projects->first()->id)}}" class="navbar-link" title="Show project {{$projects->first()->name}}">{{$projects->first()->name}}</a>
 @endif
