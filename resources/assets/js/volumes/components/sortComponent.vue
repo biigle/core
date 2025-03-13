e33474a6e6269bbca1a4ff37ad077d2f6623876b
Martin Zurowietz <martin@zurowietz.de>
2025-03-13 14:40:11 +0100

    Fix volume sorters promise definition

--- resources/assets/js/volumes/components/sortComponent.vue
+++ resources/assets/js/volumes/components/sortComponent.vue
@@ -28,7 +28,7 @@ export default {
     },
     methods: {
         getSequence() {
-            return new Promise.resolve([]);
+            return Promise.resolve([]);
         },
         handleClick() {
             if (!this.active) {
