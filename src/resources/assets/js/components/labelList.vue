faf400ae187025b8113e4e47ba4c4fa39d73a9a8
Martin Zurowietz <martin@zurowietz.de>
2025-03-20 14:49:04 +0100

    Remove inline-template in Largo view

--- src/resources/assets/js/components/labelList.vue
+++ src/resources/assets/js/components/labelList.vue
@@ -1,3 +1,25 @@
+<template>
+    <div class="annotations-tab--largo">
+        <div class="annotations-tab__header--largo">
+            <div class="text-muted">Total
+                <span
+                    class="pull-right badge"
+                    v-text="annotationBadgeCount"
+                ></span>
+            </div>
+        </div>
+        <ul class="annotations-tab__list--largo list-unstyled" ref="scrollList">
+            <label-item
+                 v-for="item in labels"
+                 :key="item.id"
+                 :label="item"
+                 @select="handleSelectedLabel"
+                 @deselect="handleDeselectedLabel"
+                 ></label-item>
+        </ul>
+    </div>
+</template>
+
 <script>
 import LabelItem from './labelListLabelItem.vue';
 
--- src/resources/views/labelList.php
+++ src/resources/views/labelList.php
@@ -3,26 +3,5 @@
         :labels="labels"
         v-on:select="handleSelectedLabel"
         v-on:deselect="handleDeselectedLabel"
-        inline-template
-        >
-            <div class="annotations-tab--largo">
-                <div class="annotations-tab__header--largo">
-                    <div class="text-muted">Total
-                        <span
-                            class="pull-right badge"
-                            v-text="annotationBadgeCount"
-                        ></span>          
-                    </div> 
-                </div>
-                <ul class="annotations-tab__list--largo list-unstyled" ref="scrollList">
-                    <label-item
-                         v-for="item in labels"
-                         :key="item.id"
-                         :label="item"
-                         v-on:select="handleSelectedLabel"
-                         v-on:deselect="handleDeselectedLabel"
-                         ></label-item>
-                </ul>
-            </div>
-    </label-list>
+        ></label-list>
 </sidebar-tab>
