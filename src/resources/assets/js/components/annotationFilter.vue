407fb94a898c5a8f5aee5e0e23c84442b6231eef
Davide Brembilla <davide.brembilla98@gmail.com>
2025-04-03 16:12:20 +0200

    Align filteringTab and annotationFilter to the rest of the codebase for what regards whitespace and indentation

--- src/resources/assets/js/components/annotationFilter.vue
+++ src/resources/assets/js/components/annotationFilter.vue
@@ -1,83 +1,90 @@
 <template>
-  <div class="annotation-filter">
-    <div class="form-group">
-      <div class="btn-group" role="group">
-        <button
-          type="button"
-          class="btn btn-default"
-          title="Use the 'and' operator for filter rules"
-          :class="{ active: !union }"
-          @click="activateAndOperator"
-        >
-          and
-        </button>
-        <button
-          type="button"
-          class="btn btn-default"
-          title="Use the 'or' operator for filter rules"
-          :class="{ active: union }"
-          @click="activateOrOperator"
-        >
-          or
-        </button>
-      </div>
-      <div class="btn-group pull-right" role="group">
-        <button
-          type="button"
-          class="btn btn-default"
-          title="Clear all filter rules"
-          @click="reset"
-        >
-          <span class="fa fa-times" aria-hidden="true"></span>
-        </button>
-      </div>
+    <div class="annotation-filter">
+        <div class="form-group">
+            <div class="btn-group" role="group">
+                <button
+                    type="button"
+                    class="btn btn-default"
+                    title="Use the 'and' operator for filter rules"
+                    :class="{ active: !union }"
+                    @click="activateAndOperator"
+                >
+                    and
+                </button>
+                <button
+                    type="button"
+                    class="btn btn-default"
+                    title="Use the 'or' operator for filter rules"
+                    :class="{ active: union }"
+                    @click="activateOrOperator"
+                >
+                    or
+                </button>
+            </div>
+            <div class="btn-group pull-right" role="group">
+                <button
+                    type="button"
+                    class="btn btn-default"
+                    title="Clear all filter rules"
+                    @click="reset"
+                >
+                    <span class="fa fa-times" aria-hidden="true"></span>
+                </button>
+            </div>
+        </div>
+        <div class="filter-form__selects">
+            <div class="form-group largo-filter-select">
+                <select
+                    class="form-control"
+                    selected="Shapes"
+                    v-model="selectedFilter"
+                    title="Select attribute for filtering"
+                    @click.once="loadApiFilters"
+                    @change="resetSelectedFilter"
+                >
+                    <option
+                        v-for="value in Object.keys(filterValues)"
+                        :value="value"
+                        v-text="value"
+                    ></option>
+                </select>
+            </div>
+            <div class="form-group largo-filter-select filter-select">
+                <select
+                    class="form-control"
+                    v-model="negate"
+                    selected="true"
+                    required
+                >
+                    <option :value="false">is</option>
+                    <option :value="true">is not</option>
+                </select>
+            </div>
+        </div>
+        <div class="filter-form__selects">
+            <div class="form-group largo-filter-select">
+                <select class="form-control" v-model="selectedFilterValue">
+                    <option
+                        v-for="(filter_name, filter_id) in this
+                            .activeFilterValue"
+                        :value="[filter_name, filter_id]"
+                        v-text="filter_name"
+                    ></option>
+                </select>
+            </div>
+            <div class="form-group filter-select largo-filter-select">
+                <button
+                    type="button"
+                    :disabled="!this.selectedFilterValue"
+                    class="btn btn-default btn-block"
+                    title="Add the selected filter rule"
+                    @click="addFilter"
+                >
+                    Add rule
+                </button>
+            </div>
+        </div>
     </div>
-    <div class="filter-form__selects">
-      <div class="form-group largo-filter-select">
-        <select
-          class="form-control"
-          selected="Shapes"
-          v-model="selectedFilter"
-          title="Select attribute for filtering"
-          @click.once="loadApiFilters"
-          @change="resetSelectedFilter"
-        >
-          <option
-            v-for="value in Object.keys(filterValues)"
-            :value="value"
-            v-text="value"
-          ></option>
-        </select>
-      </div>
-      <div class="form-group largo-filter-select filter-select">
-        <select class="form-control" v-model="negate" selected="true" required>
-          <option :value="false">is</option>
-          <option :value="true">is not</option>
-        </select>
-      </div>
-    </div>
-    <div class="filter-form__selects">
-      <div class="form-group largo-filter-select">
-        <select class="form-control" v-model="selectedFilterValue">
-          <option
-            v-for="(filter_name, filter_id) in this.activeFilterValue"
-            :value="[filter_name, filter_id]"
-            v-text="filter_name"
-          ></option>
-        </select>
-      </div>
-      <div class="form-group filter-select largo-filter-select">
-          <button
-              type="button"
-              :disabled="!this.selectedFilterValue"
-              class="btn btn-default btn-block"
-              title="Add the selected filter rule"
-              @click="addFilter"
-           >Add rule
-          </button>
-      </div>
-    </div>
-  </div>
 </template>
 <script>
 import { Messages } from "../import";
@@ -85,106 +92,110 @@ import ProjectsApi from "../api/projects";
 import VolumesApi from "../api/volumes";
 
 export default {
-  data() {
-    //TODO: add more filters here. See https://github.com/biigle/largo/issues/66
-    let availableShapes = biigle.$require("largo.availableShapes");
+    data() {
+        //TODO: add more filters here. See https://github.com/biigle/largo/issues/66
+        let availableShapes = biigle.$require("largo.availableShapes");
 
-    return {
-      filterValues: {
-        Shape: availableShapes,
-        User: {},
-      },
-      filterToKeyMapping: {
-        Shape: "shape_id",
-        User: "user_id",
-      },
-      selectedFilter: "Shape",
-      selectedFilterValue: null,
-      negate: false,
-      union: false,
-    };
-  },
-
-  computed: {
-      activeFilterValue() {
-        return this.filterValues[this.selectedFilter];
-      }
-  },
-
-  methods: {
-    activateAndOperator() {
-      this.union = false;
-      this.$emit('set-union-logic', 0);
+        return {
+            filterValues: {
+                Shape: availableShapes,
+                User: {}
+            },
+            filterToKeyMapping: {
+                Shape: "shape_id",
+                User: "user_id"
+            },
+            selectedFilter: "Shape",
+            selectedFilterValue: null,
+            negate: false,
+            union: false
+        };
     },
-    activateOrOperator() {
-      this.union = true;
-      this.$emit('set-union-logic', 1);
-    },
-    reset() {
-      this.$emit("reset-filters");
-    },
-    resetSelectedFilter() {
-        this.selectedFilterValue = null;
-    },
-    loadApiFilters() {
-      //Load here filters that should be loaded AFTER the page is rendered
-      let volumeId = biigle.$require("largo.volumeId");
-      let usersWithAnnotationsPromise;
 
-      if (typeof volumeId === "number") {
-        usersWithAnnotationsPromise = VolumesApi.getUsersWithAnnotations({
-          id: volumeId,
-        });
-      } else {
-        let projectId = biigle.$require("largo.projectId");
-        usersWithAnnotationsPromise = ProjectsApi.getUsersWithAnnotations({
-          id: projectId,
-        });
-      }
-
-      usersWithAnnotationsPromise.then((response) =>
-        response.data.forEach(
-          (user) =>
-            (this.filterValues.User[user.user_id] =
-              user.firstname + " " + user.lastname),
-        ), Messages.handleErrorResponse
-      );
-    },
-    addFilter() {
-      if (!this.selectedFilterValue) {
-        return;
-      }
-
-      let logicalString;
-
-      //Avoid changing directly the value of selectedFilterValue
-      //This can cause weird bugs on the frontend otherwise
-      let selectedFilterValue = [...this.selectedFilterValue];
-
-      //convert to integer
-      selectedFilterValue[1] = +selectedFilterValue[1];
-
-      if (this.negate) {
-        logicalString = "is not";
-        if (selectedFilterValue[1] > 0){
-          selectedFilterValue[1] = -selectedFilterValue[1];
+    computed: {
+        activeFilterValue() {
+            return this.filterValues[this.selectedFilter];
         }
-      } else {
-        logicalString = "is";
-      }
-
-      let filterToAdd = {
-        name:
-          this.selectedFilter +
-          " " +
-          logicalString +
-          " " +
-          selectedFilterValue[0],
-        filter: this.filterToKeyMapping[this.selectedFilter],
-        value: selectedFilterValue[1],
-      };
-      this.$emit("add-filter", filterToAdd);
     },
-  },
+
+    methods: {
+        activateAndOperator() {
+            this.union = false;
+            this.$emit("set-union-logic", 0);
+        },
+        activateOrOperator() {
+            this.union = true;
+            this.$emit("set-union-logic", 1);
+        },
+        reset() {
+            this.$emit("reset-filters");
+        },
+        resetSelectedFilter() {
+            this.selectedFilterValue = null;
+        },
+        loadApiFilters() {
+            //Load here filters that should be loaded AFTER the page is rendered
+            let volumeId = biigle.$require("largo.volumeId");
+            let usersWithAnnotationsPromise;
+
+            if (typeof volumeId === "number") {
+                usersWithAnnotationsPromise =
+                    VolumesApi.getUsersWithAnnotations({
+                        id: volumeId
+                    });
+            } else {
+                let projectId = biigle.$require("largo.projectId");
+                usersWithAnnotationsPromise =
+                    ProjectsApi.getUsersWithAnnotations({
+                        id: projectId
+                    });
+            }
+
+            usersWithAnnotationsPromise.then(
+                (response) =>
+                    response.data.forEach(
+                        (user) =>
+                            (this.filterValues.User[user.user_id] =
+                                user.firstname + " " + user.lastname)
+                    ),
+                Messages.handleErrorResponse
+            );
+        },
+        addFilter() {
+            if (!this.selectedFilterValue) {
+                return;
+            }
+
+            let logicalString;
+
+            //Avoid changing directly the value of selectedFilterValue
+            //This can cause weird bugs on the frontend otherwise
+            let selectedFilterValue = [...this.selectedFilterValue];
+
+            //convert to integer
+            selectedFilterValue[1] = +selectedFilterValue[1];
+
+            if (this.negate) {
+                logicalString = "is not";
+                if (selectedFilterValue[1] > 0) {
+                    selectedFilterValue[1] = -selectedFilterValue[1];
+                }
+            } else {
+                logicalString = "is";
+            }
+
+            let filterToAdd = {
+                name:
+                    this.selectedFilter +
+                    " " +
+                    logicalString +
+                    " " +
+                    selectedFilterValue[0],
+                filter: this.filterToKeyMapping[this.selectedFilter],
+                value: selectedFilterValue[1]
+            };
+            this.$emit("add-filter", filterToAdd);
+        }
+    }
 };
 </script>
--- src/resources/assets/js/components/filteringTab.vue
+++ src/resources/assets/js/components/filteringTab.vue
@@ -1,70 +1,89 @@
 <template>
-  <div class="filtering-tab">
-    <form class="form clearfix" v-on:submit.prevent>
-      <annotation-filter
-        @reset-filters="resetFilters"
-        @add-filter="addNewFilter"
-        @set-union-logic="setUnionLogic"
-      >
-      </annotation-filter>
-    </form>
-    <ul class="list-group">
-      <li class="list-group-item text-muted" v-if="activeFilters.length == 0">
-        No filter rules
-      </li>
-      <li class="list-group-item" v-for="(filter, k) in activeFilters">
-        <span v-if="k > 0"> {{ logicString }} </span> <span>{{ filter.name }}</span>
-        <button @click="removeFilter(k)" type="button" class="close" title="Remove this rule">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </li>
-    </ul>
-  </div>
+    <div class="filtering-tab">
+        <form class="form clearfix" v-on:submit.prevent>
+            <annotation-filter
+                @reset-filters="resetFilters"
+                @add-filter="addNewFilter"
+                @set-union-logic="setUnionLogic"
+            >
+            </annotation-filter>
+        </form>
+        <ul class="list-group">
+            <li
+                class="list-group-item text-muted"
+                v-if="activeFilters.length == 0"
+            >
+                No filter rules
+            </li>
+            <li class="list-group-item" v-for="(filter, k) in activeFilters">
+                <span v-if="k > 0"> {{ logicString }} </span>
+                <span>{{ filter.name }}</span>
+                <button
+                    @click="removeFilter(k)"
+                    type="button"
+                    class="close"
+                    title="Remove this rule"
+                >
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </li>
+        </ul>
+    </div>
 </template>
 <script>
 import AnnotationFilter from "../components/annotationFilter.vue";
 
 export default {
-  components: {
-    AnnotationFilter,
-  },
-  computed: {
-      logicString() {
-        return this.union ? 'or' : 'and';
-      }
-  },
-  data() {
-    return {
-      activeFilters: [],
-      union: false,
-    };
-  },
-  methods: {
-    resetFilters() {
-      this.activeFilters = [];
-      this.filterAnnotations();
+    components: {
+        AnnotationFilter
     },
-    removeFilter(key) {
-      this.activeFilters.splice(key, 1);
-      this.filterAnnotations();
-    },
-    setUnionLogic(union) {
-      this.union = union;
-      this.filterAnnotations();
-    },
-    addNewFilter(filter) {
-      if (this.activeFilters.length > 0) {
-        if (this.activeFilters.some(f => f.filter === filter.filter && f.value === filter.value)) {
-          return;
+    computed: {
+        logicString() {
+            return this.union ? "or" : "and";
         }
-      }
-      this.activeFilters.push(filter);
-      this.filterAnnotations();
     },
+    data() {
+        return {
+            activeFilters: [],
+            union: false
+        };
+    },
+    methods: {
+        resetFilters() {
+            this.activeFilters = [];
+            this.filterAnnotations();
+        },
+        removeFilter(key) {
+            this.activeFilters.splice(key, 1);
+            this.filterAnnotations();
+        },
+        setUnionLogic(union) {
+            this.union = union;
+            this.filterAnnotations();
+        },
+        addNewFilter(filter) {
+            if (this.activeFilters.length > 0) {
+                if (
+                    this.activeFilters.some(
+                        (f) =>
+                            f.filter === filter.filter &&
+                            f.value === filter.value
+                    )
+                ) {
+                    return;
+                }
+            }
+            this.activeFilters.push(filter);
+            this.filterAnnotations();
+        },
 
-    filterAnnotations() {
-      this.$emit("handle-selected-filters", this.activeFilters, this.union);
-    },
-  },
+        filterAnnotations() {
+            this.$emit(
+                "handle-selected-filters",
+                this.activeFilters,
+                this.union
+            );
+        }
+    }
 };
 </script>
