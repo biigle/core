c47780b9a1bf4658167a9c90aec28c492cf9bbe9
Leane Schlundt <lhecht@techfak.de>
2025-05-05 09:05:47 +0200

    Remove unneccessary line

--- app/Services/Reports/Volumes/ImageAnnotations/AbundanceReportGenerator.php
+++ app/Services/Reports/Volumes/ImageAnnotations/AbundanceReportGenerator.php
@@ -189,9 +189,7 @@ class AbundanceReportGenerator extends AnnotationReportGenerator
      */
     protected function createCsv($rows, $title, $labels, $emptyImagesQuery)
     {
-        $rows = $rows
-            ->groupBy('filename')
-            ->sortBy(fn ($annotation, $file) => $file, SORT_NATURAL);
+        $rows = $rows->groupBy('filename');
 
         if ($this->shouldAggregateChildLabels()) {
             [$rows, $labels] = $this->aggregateChildLabels($rows, $labels);
