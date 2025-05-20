3114712515fea12f494d8ac766e7034c4d519854
Leane Schlundt <lhecht@techfak.de>
2025-05-20 09:01:19 +0200

    Add tests

--- tests/php/Jobs/TileSingleImageTest.php
+++ tests/php/Jobs/TileSingleImageTest.php
@@ -6,19 +6,17 @@ use File;
 use Mockery;
 use Storage;
 use TestCase;
+use Exception;
 use Aws\Result;
-use ArrayObject;
 use Aws\Command;
+use ArrayIterator;
 use Aws\MockHandler;
 use Aws\S3\S3Client;
 use Jcupitt\Vips\Image;
-use Aws\CommandInterface;
 use Biigle\Tests\ImageTest;
 use GuzzleHttp\Psr7\Response;
-use Aws\Exception\AwsException;
 use Biigle\Jobs\TileSingleImage;
 use Aws\S3\Exception\S3Exception;
-use Illuminate\Support\Facades\Log;
 
 class TileSingleImageTest extends TestCase
 {
@@ -68,33 +66,72 @@ class TileSingleImageTest extends TestCase
         $mock->append(
             new Result(),
             new Result(),
-            // new Result(),
-            new S3Exception("test", new Command("mockCommand"), [
-                'code' => 'mockCode',
-                'response' => new Response(400)
-            ])
+            new Result(),
         );
 
         $client = new S3Client([
             'region' => 'test',
             'handler' => $mock,
             'credentials' => [
-            'key' => 'fake-key',
-            'secret' => 'fake-secret',
+                'key' => 'fake-key',
+                'secret' => 'fake-secret',
             ],
         ]);
 
-        $files = ['test-image.jpg', 'test-image.png', 'exif-test.jpg'];
+        // Treat images as tiles for Biigle\Image
+        $tiles = ['test-image.jpg', 'test-image.png', 'exif-test.jpg'];
 
         $image = ImageTest::create();
         $job = new TileSingleImageStub($image);
         $job->client = $client;
-        $job->files = $files;
+        $job->files = $tiles;
         $job->useParentGetIterator = false;
 
         $job->uploadToS3Storage();
 
-        // $this->assertEquals($files, $job->uploadedFiles);
+        $this->assertEquals($tiles, $job->uploadedFiles);
+    }
+
+    public function testUploadToS3StorageThrowException()
+    {
+        config(['image.tiles.nbr_concurrent_requests' => 2]);
+
+        $mock = new MockHandler();
+        $mock->append(
+            new Result(),
+            new Result(),
+            new S3Exception("test", new Command("mockCommand"), [
+                'code' => 'mockCode',
+                'response' => new Response(400)
+            ]),
+        );
+
+        $client = new S3Client([
+            'region' => 'test',
+            'handler' => $mock,
+            'credentials' => [
+                'key' => 'fake-key',
+                'secret' => 'fake-secret',
+            ],
+        ]);
+
+        // Treat images as tiles for Biigle\Image
+        $tiles = ['test-image.jpg', 'test-image.png', 'exif-test.jpg'];
+
+        $image = ImageTest::create();
+        $job = new TileSingleImageStub($image);
+        $job->client = $client;
+        $job->files = $tiles;
+        $job->useParentGetIterator = false;
+
+        $fails = false;
+        try {
+            $job->uploadToS3Storage();
+        } catch (Exception $e) {
+            $fails = true;
+        }
+
+        $this->assertTrue($fails);
     }
 
     public function testQueue()
@@ -122,33 +159,42 @@ class TileSingleImageStub extends TileSingleImage
         return $this->mock;
     }
 
-    protected function getClient($disk) {
+    protected function getClient($disk)
+    {
         return $this->client;
     }
 
-    protected function getBucket($disk) {
+    protected function getBucket($disk)
+    {
         return "test-bucket";
     }
 
-    protected function getIterator($path){
-        if ($this->useParentGetIterator){
+    protected function getIterator($path)
+    {
+        if ($this->useParentGetIterator) {
             return parent::getIterator($path);
         }
 
         $files = [];
-        foreach($this->files as $file){
+        foreach ($this->files as $file) {
             $files[] = "tests/files/{$file}";
         }
 
-        return (new ArrayObject($files))->getIterator();
+        return new ArrayIterator($files);
     }
 
-    protected function sendRequests($files, $onFullfill, $onReject){
-        $onFullfill2 = function($res, $index) use ($onFullfill) {
+    protected function sendRequests($files, $onFullfill, $onReject)
+    {
+        $onFullfill2 = function ($res, $index) use ($onFullfill) {
             $onFullfill($res, $index);
             $this->uploadedFiles[$index] = basename($res->get('ObjectURL'));
         };
         parent::sendRequests($files, $onFullfill2, $onReject);
+    }
+
+    public function uploadToS3Storage($retry = 3)
+    {
+        parent::uploadToS3Storage(1);
     }
 
 }
