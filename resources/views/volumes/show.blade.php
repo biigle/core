05de84a99723634b710f9e2a350014cfb3352f8a
Daniel Langenkämper <dlangenk@gmail.com>
2025-07-08 23:57:46 +0200

    show modal "without" opening sidebar

--- resources/views/volumes/show.blade.php
+++ resources/views/volumes/show.blade.php
@@ -42,7 +42,7 @@
 
 @section('content')
 <div id="volume-container" class="sidebar-container">
-    <sidebar direction="left" v-on:open="handleSidebarOpen" v-on:close="handleSidebarClose">
+    <sidebar ref="sidebar" direction="left" v-on:open="handleSidebarOpen" v-on:close="handleSidebarClose">
         @can ('update', $volume)
             <sidebar-tab name="edit" icon="pencil-alt" title="Edit this volume" href="{{ route('volume-edit', $volume->id) }}"></sidebar-tab>
         @endcan
@@ -57,7 +57,9 @@
         <sidebar-tab name="sorting" icon="exchange-alt fa-rotate-90" title="Sort files" :highlight="sortingActive">
             @include('volumes.show.sorting')
         </sidebar-tab>
-        <sidebar-tab name="charts" icon="chart-bar" title="Show volume charts" @click.native.prevent="showCharts"></sidebar-tab>
+        <sidebar-tab name="charts" icon="chart-bar" title="Show volume charts">
+            <div style="display: none;"></div>
+        </sidebar-tab>
         <sidebar-tab name="reports" icon="file" title="Request reports for this volume" href="{{route('volume-reports', $volume->id)}}"></sidebar-tab>
         @canany (['edit-in', 'sudo'], $volume)
             <sidebar-tab name="largo" icon="check-square" title="Perform Largo re-evaluation of annotations for this volume" href="{{ route('largo', $volume->id) }}"></sidebar-tab>
