3ab71c8e3a429a1c31b8c33c232c58fd8a3c5b5c
Martin Zurowietz <martin@zurowietz.de>
2025-09-18 15:20:13 +0200

    Add tests for other largo filter controllers

--- tests/php/Http/Controllers/Api/Volumes/FilterVideoAnnotationsByLabelControllerTest.php
+++ tests/php/Http/Controllers/Api/Volumes/FilterVideoAnnotationsByLabelControllerTest.php
@@ -252,4 +252,43 @@ class FilterVideoAnnotationsByLabelControllerTest extends ApiTestCase
         $this->get("/api/v1/volumes/{$id}/video-annotations/filter/label/{$l1->label_id}?user_id[]={$u1->id}&user_id[]=-{$u2->id}&union=0")
             ->assertExactJson([$a1->id => $video->uuid]);
     }
+
+    public function testIndexFilenameFilter()
+    {
+        $id = $this->volume()->id;
+
+        $video1 = VideoTest::create(['volume_id' => $id, 'filename' => 'test_video_001.mp4']);
+        $video2 = VideoTest::create(['volume_id' => $id, 'filename' => 'test_video_002.mp4']);
+        $video3 = VideoTest::create(['volume_id' => $id, 'filename' => 'another_file.webm']);
+
+        $a1 = VideoAnnotationTest::create(['video_id' => $video1->id]);
+        $a2 = VideoAnnotationTest::create(['video_id' => $video2->id]);
+        $a3 = VideoAnnotationTest::create(['video_id' => $video3->id]);
+
+        $l1 = VideoAnnotationLabelTest::create(['annotation_id' => $a1->id]);
+        $l2 = VideoAnnotationLabelTest::create(['annotation_id' => $a2->id, 'label_id' => $l1->label_id]);
+        $l3 = VideoAnnotationLabelTest::create(['annotation_id' => $a3->id, 'label_id' => $l1->label_id]);
+
+        $this->beEditor();
+
+        // Test filename pattern matching with wildcards
+        $this->get("/api/v1/volumes/{$id}/video-annotations/filter/label/{$l1->label_id}?filename[]=test_video_*")
+            ->assertExactJson([$a2->id => $video2->uuid, $a1->id => $video1->uuid]);
+
+        // Test exact filename matching
+        $this->get("/api/v1/volumes/{$id}/video-annotations/filter/label/{$l1->label_id}?filename[]=test_video_001.mp4")
+            ->assertExactJson([$a1->id => $video1->uuid]);
+
+        // Test filename extension matching
+        $this->get("/api/v1/volumes/{$id}/video-annotations/filter/label/{$l1->label_id}?filename[]=*.mp4")
+            ->assertExactJson([$a2->id => $video2->uuid, $a1->id => $video1->uuid]);
+
+        // Test negative filename filtering
+        $this->get("/api/v1/volumes/{$id}/video-annotations/filter/label/{$l1->label_id}?filename[]=-test_video_*")
+            ->assertExactJson([$a3->id => $video3->uuid]);
+
+        // Test union mode with filename patterns
+        $this->get("/api/v1/volumes/{$id}/video-annotations/filter/label/{$l1->label_id}?filename[]=test_video_001.mp4&filename[]=another_file.webm&union=1")
+            ->assertExactJson([$a3->id => $video3->uuid, $a1->id => $video1->uuid]);
+    }
 }
