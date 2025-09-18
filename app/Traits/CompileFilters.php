6f13d82316cd0a2cc5ebd5470192f1b6db0a4aeb
Martin Zurowietz <martin@zurowietz.de>
2025-09-18 15:14:13 +0200

    Simplify compileFilterConditions method

--- app/Traits/CompileFilters.php
+++ app/Traits/CompileFilters.php
@@ -11,73 +11,27 @@ trait CompileFilters
     * @param bool $union Whether filters are considered inclusive (OR) or exclusive (AND)
     * @param array $filters Array of filters to add to the query in the form `filterName => filterValue`
     */
-    private function compileFilterConditions(Builder $query, bool $union, array $filters): void
+    protected function compileFilterConditions(Builder $query, bool $union, array $filters): void
     {
-        if ($union) {
-            $query->where(function ($q) use ($filters) {
-                foreach ($filters as $filterName => $filterValues) {
-                    if ($filterName === 'filename') {
-                        $this->applyFilenameFilters($q, $filterValues, true);
-                    } else {
-                        $toInclude = array_filter($filterValues, fn ($num) => $num > 0);
-                        $toExclude = array_map('abs', array_filter($filterValues, fn ($num) => $num < 0));
+        $boolean = $union ? 'or' : 'and';
 
-                        foreach ($toInclude as $valueToInclude) {
-                            $q->orWhere($filterName, $valueToInclude);
-                        }
-
-                        foreach ($toExclude as $valueToExclude) {
-                            $q->orWhereNot($filterName, $valueToExclude);
-                        }
-                    }
-                }
-            });
-        } else {
+        $query->where(function ($q) use ($filters, $boolean) {
             foreach ($filters as $filterName => $filterValues) {
                 if ($filterName === 'filename') {
-                    $this->applyFilenameFilters($query, $filterValues, false);
+                    $operator = 'ilike';
+                    $filterValues = array_map(fn ($v) => str_replace('*', '%', $v), $filterValues);
                 } else {
-                    foreach ($filterValues as $value) {
-                        if ($value < 0) {
-                            $query->whereNot($filterName, abs($value));
-                        } else {
-                            $query->where($filterName, $value);
-                        }
+                    $operator = '=';
+                }
+
+                foreach ($filterValues as $value) {
+                    if (str_starts_with($value, '-')) {
+                        $q->whereNot($filterName, $operator, substr($value, 1), $boolean);
+                    } else {
+                        $q->where($filterName, $operator, $value, $boolean);
                     }
                 }
             }
-        }
-    }
-
-    /**
-     * Apply filename filters to the query
-     */
-    private function applyFilenameFilters(Builder $query, array $patterns, bool $union): void
-    {
-        if ($union) {
-            foreach ($patterns as $pattern) {
-                if (str_starts_with($pattern, '-')) {
-                    // Negative pattern (exclude)
-                    $cleanPattern = str_replace('*', '%', substr($pattern, 1));
-                    $query->orWhereNot('filename', 'ilike', $cleanPattern);
-                } else {
-                    // Positive pattern (include)
-                    $cleanPattern = str_replace('*', '%', $pattern);
-                    $query->orWhere('filename', 'ilike', $cleanPattern);
-                }
-            }
-        } else {
-            foreach ($patterns as $pattern) {
-                if (str_starts_with($pattern, '-')) {
-                    // Negative pattern (exclude)
-                    $cleanPattern = str_replace('*', '%', substr($pattern, 1));
-                    $query->whereNot('filename', 'ilike', $cleanPattern);
-                } else {
-                    // Positive pattern (include)
-                    $cleanPattern = str_replace('*', '%', $pattern);
-                    $query->where('filename', 'ilike', $cleanPattern);
-                }
-            }
-        }
+        });
     }
 }
