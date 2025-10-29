2504a6fd4e6aace1813bc0207d8e693150d21bf1
Davide Brembilla <davide.brembilla98@gmail.com>
2025-10-29 09:19:20 +0100

    Change full_report.py and csvs_to_xlsx.py so that xlsx files that are too big are split

--- resources/scripts/reports/csvs_to_xlsx.py
+++ resources/scripts/reports/csvs_to_xlsx.py
@@ -8,6 +8,9 @@ csv.field_size_limit(sys.maxsize)
 target_file = sys.argv[2]
 csvs = sys.argv[3:]
 
+#Arbitrary limit of rows for a single sheet. See https://github.com/biigle/core/issues/1040
+ROW_LIMIT = 5000
+
 workbook = Workbook()
 numSheets = 0
 
@@ -20,14 +23,16 @@ for path in csvs:
     if len(rows) == 2:
         continue
     numSheets += 1
+
     # rows have the content: image_filename, label_name, label_count
+    for i in range(0, len(rows), ROW_LIMIT):
+        sheet_number = i // ROW_LIMIT
+        # Excel does not permit worksheet names longer than 31 characters
+        ws = workbook.new_sheet(f"sheet {str(numSheets)}-{str(sheet_number)}", data=rows[i:i+ROW_LIMIT])
 
-    # Excel does not permit worksheet names longer than 31 characters
-    ws = workbook.new_sheet("sheet " + str(numSheets), data=rows)
-
-    # bold font for titles
-    ws.set_row_style(1, Style(font=Font(bold=True)))
-    ws.set_row_style(2, Style(font=Font(bold=True)))
+        # bold font for titles
+        ws.set_row_style(1, Style(font=Font(bold=True)))
+        ws.set_row_style(2, Style(font=Font(bold=True)))
 
 if not numSheets:
     ws = workbook.new_sheet("No labels found", data=[['No labels found']])
