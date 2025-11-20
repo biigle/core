0ab2df5d87ae3ca08c3d8b1a8435daae1711d06f
yannik131 <yannik131@web.de>
2025-11-20 14:09:13 +0100

    Made labelbot work for tiled images
    Since tiled images are transmitted in parts, no underlying image can be
    directly accessed. Instead, a screenshot of the map is taken and cropped
    according to the selection.

--- resources/assets/js/annotations/utils.js
+++ resources/assets/js/annotations/utils.js
@@ -26,4 +26,57 @@ let isInvalidShape = function (feature) {
     }
 };
 
-export {isInvalidShape};
+/**
+ * Clamps a value in an inclusive interval
+ * @param value The value to clamp
+ * @param min Lower bound
+ * @param max Upper bound
+ * @returns Lower or upper bound if the value is outside of the bounds, otherwise just the value
+ */
+function clamp(value, min, max) {
+    return Math.max(min, Math.min(max, value));
+}
+
+function getBoundingBox(image, points) {
+    let minX = image.width;
+    let minY = image.height;
+    let maxX = 0;
+    let maxY = 0;
+    // Point
+    if (points.length === 2) {
+        // TODO: maybe use SAM or PTP module to convert point to shape
+        const tempRadius = 64; // Same radius than used for Largo thumbnails.
+        const [x, y] = points;
+        minX = Math.max(0, x - tempRadius);
+        minY = Math.max(0, y - tempRadius);
+        maxX = Math.min(image.width, x + tempRadius);
+        maxY = Math.min(image.height, y + tempRadius);
+    } else if (points.length === 3) { // Circle
+        const [centerX, centerY, radius] = points;
+        minX = Math.max(0, centerX - radius);
+        minY = Math.max(0, centerY - radius);
+        maxX = Math.min(image.width, centerX + radius);
+        maxY = Math.min(image.height, centerY + radius);
+    } else {
+        for (let i = 0; i < points.length; i += 2) {
+            const x = points[i];
+            const y = points[i + 1];
+            minX = Math.min(minX, x);
+            minY = Math.min(minY, y);
+            maxX = Math.max(maxX, x);
+            maxY = Math.max(maxY, y);
+        }
+        // Ensure the bounding box is within the image dimensions
+        minX = Math.max(0, minX);
+        minY = Math.max(0, minY);
+        maxX = Math.min(image.width, maxX);
+        maxY = Math.min(image.height, maxY);
+    }
+
+    const width = maxX - minX;
+    const height = maxY - minY;
+
+    return [minX, minY, width, height];
+}
+
+export {isInvalidShape, clamp, getBoundingBox};
