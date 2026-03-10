5afd475c66e8246f5743c37c1e3017488d28f1d1
Davide Brembilla <davide.brembilla98@gmail.com>
2026-03-10 09:30:10 +0100

    Adds column for reference images

--- database/migrations/2026_03_02_103908_create_annotation_strategy_tables.php
+++ database/migrations/2026_03_02_103908_create_annotation_strategy_tables.php
@@ -11,15 +11,15 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('lol', function (Blueprint $table) {
+        Schema::create('annotation_strategies', function (Blueprint $table) {
             $table->id();
             $table->foreignId('project')
                 ->constrained()
                 ->onDelete('cascade');
-            $table->text('description');
+            $table->text('description')
         });
 
-        Schema::create('lol2', function (Blueprint $table) {
+        Schema::create('annotation_strategy_labels', function (Blueprint $table) {
             $table->foreignId('annotation_strategy_id')
                 ->constrained()
                 ->onDelete('cascade');
@@ -29,10 +29,14 @@ return new class extends Migration
                 ->onDelete('cascade');
 
             $table->foreignId('shape_id')
+                ->nullable(true)
                 ->constrained()
-                ->onDelete('cascade');
+                ->onDelete('set null');
 
             $table->text('description');
+
+            $table->string('reference_image')
+                ->nullable(true);
         });
     }
 
