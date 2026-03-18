f4d38d1ece0bd50fa78691f6b032aa06d1899564
Martin Zurowietz <martin@zurowietz.de>
2026-03-18 16:40:08 +0100

    Apply suggestions from code review

--- resources/scripts/ExtractFeaturesWorker.py
+++ resources/scripts/ExtractFeaturesWorker.py
@@ -4,7 +4,7 @@ from queue import Queue, Empty
 from threading import Thread, Event
 from torch import device, no_grad
 from torch.cuda import is_available as cuda_is_available
-from torch.hub import load as hub_load
+from torch.hub import load as hub_load, get_dir as hub_get_dir
 import fcntl
 import io
 import json
@@ -57,9 +57,9 @@ def worker():
     ImageFile.LOAD_TRUNCATED_IMAGES = True
 
     # Use a file lock so parallel workers don't race to init the TorchHub cache.
-    lock_path = os.path.join(os.environ['TORCH_HOME'], 'hub', '.init.lock')
+    lock_path = os.path.join(hub_get_dir(), '.init.lock')
     os.makedirs(os.path.dirname(lock_path), exist_ok=True)
-    with open(lock_path, 'w') as lock_file:
+    with open(lock_path, 'a') as lock_file:
         fcntl.flock(lock_file, fcntl.LOCK_EX)
         try:
             dinov2_vits14 = hub_load('facebookresearch/dinov2:main', 'dinov2_vits14')
