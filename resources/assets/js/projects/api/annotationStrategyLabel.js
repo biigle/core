4e920e37a6919a56dfe01da82ef5b7fdcb822894
Davide Brembilla <davide.brembilla98@gmail.com>
2026-03-24 08:04:09 +0100

    Remove now useless methods from api resource

--- resources/assets/js/projects/api/annotationStrategyLabel.js
+++ resources/assets/js/projects/api/annotationStrategyLabel.js
@@ -6,18 +6,11 @@ import {Resource} from 'vue-resource';
  * Create or update a label in a strategy.
  * resource.save({id: projectId, descriptions: [description], labels: [labels]}, {...}).then(...);
  *
- * Upload a reference image and returns the uploaded filename
- * resource.upload_image({id: projectId, file: file}, {...}).then(...);
- *
  * Delete a reference image
  * resource.delete_image({id: projectId, descriptions: [description], labels: [labels]}, {...}).then(...);
  *
  */
 export default Resource('api/v1/projects{/id}/annotation-strategy-label',{}, {
-    upload_image: {
-        method: 'POST',
-        url: 'api/v1/projects{/id}/annotation-strategy-label/upload-image',
-    },
     delete_image: {
         method: 'DELETE',
         url: 'api/v1/projects{/id}/annotation-strategy-label/delete-image',
