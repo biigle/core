cde258485d30520fae8e2c03dced8e9e9b06025d
Leane Schlundt <lhecht@techfak.de>
2026-03-26 10:16:24 +0100

    Remove unnecessary fail event

--- app/Jobs/CreateNewFilesAndNotifyUser.php
+++ app/Jobs/CreateNewFilesAndNotifyUser.php
@@ -2,17 +2,12 @@
 
 namespace Biigle\Jobs;
 
-use Biigle\Events\VolumeFilesProcessingFailed;
 use Biigle\Events\VolumeImagesProcessed;
 use Biigle\User;
 use Biigle\Volume;
-use Exception;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
-use Illuminate\Support\Facades\App;
-use Illuminate\Support\Facades\Log;
-use Throwable;
 
 class CreateNewFilesAndNotifyUser implements ShouldQueue
 {
@@ -48,7 +43,7 @@ class CreateNewFilesAndNotifyUser implements ShouldQueue
 
     /**
      * Create a new job instance.
-     * 
+     *
      * @param Volume $volume
      * @param array $filenames
      * @param User $user
@@ -65,27 +60,11 @@ class CreateNewFilesAndNotifyUser implements ShouldQueue
      */
     public function handle(): void
     {
-        try {
-            $job = $this->getJob();
-            $job->handle();
-            
-            if ($this->volume->isImageVolume()) {
-                VolumeImagesProcessed::dispatch($this->user);
-            }
-        } catch (Exception $e) {
-            if ($this->volume->creating_async) {
-                $this->volume->creating_async = false;
-                $this->volume->save();
-            }
+        $job = $this->getJob();
+        $job->handle();
 
-            if (App::runningUnitTests()) {
-                $this->fail();
-                $this->failed($e);
-                return;
-            }
-
-            Log::warning("Could not process the new volume with id {$this->volume->id}");
-            throw $e;
+        if ($this->volume->isImageVolume()) {
+            VolumeImagesProcessed::dispatch($this->user);
         }
     }
 
@@ -97,10 +76,5 @@ class CreateNewFilesAndNotifyUser implements ShouldQueue
     public function getJob()
     {
         return new CreateNewImagesOrVideos($this->volume, $this->filenames);
-    }
-
-    public function failed(?Throwable $exception)
-    {
-        VolumeFilesProcessingFailed::dispatch($this->user);
     }
 }
