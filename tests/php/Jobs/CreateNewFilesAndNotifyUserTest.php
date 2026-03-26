7bbf479d47a13424b9710c0a863c8e29437e0344
Leane Schlundt <lhecht@techfak.de>
2026-03-26 09:24:52 +0100

    Add tests

---
+++ tests/php/Jobs/CreateNewFilesAndNotifyUserTest.php
@@ -0,0 +1,88 @@
+<?php
+
+namespace Biigle\Tests\Jobs;
+
+use Biigle\Events\VolumeFilesProcessingFailed;
+use Biigle\Events\VolumeImagesProcessed;
+use Biigle\Jobs\CreateNewFilesAndNotifyUser;
+use Biigle\Jobs\CreateNewImagesOrVideos;
+use Biigle\MediaType;
+use Biigle\Tests\VolumeTest;
+use Biigle\User;
+use Exception;
+use Illuminate\Support\Facades\Event;
+use Mockery;
+use TestCase;
+
+class CreateNewFilesAndNotifyUserTest extends TestCase
+{
+    public function testHandleImages()
+    {
+        Event::fake();
+        $volume = VolumeTest::create([
+            'media_type_id' => MediaType::imageId(),
+        ]);
+        $user = User::find($volume->creator_id);
+        $filenames = ['a.jpg', 'b.jpg'];
+
+        $mock = Mockery::mock(CreateNewImagesOrVideos::class);
+        $job = new CreateNewFilesAndNotifyUserStub($volume, $filenames, $user);
+        $job->subJob = $mock;
+
+        $mock->shouldReceive('handle');
+        $job->handle();
+        Event::assertDispatched(VolumeImagesProcessed::class);
+    }
+
+    public function testHandleVideos()
+    {
+        Event::fake();
+        $volume = VolumeTest::create([
+            'media_type_id' => MediaType::videoId(),
+        ]);
+        $user = User::find($volume->creator_id);
+        $filenames = ['a.mp4', 'b.mp4'];
+
+        $mock = Mockery::mock(CreateNewImagesOrVideos::class);
+        $job = new CreateNewFilesAndNotifyUserStub($volume, $filenames, $user);
+        $job->subJob = $mock;
+
+        $mock->shouldReceive('handle');
+        $job->handle();
+        Event::assertNotDispatched(VolumeImagesProcessed::class);
+    }
+
+    public function testHandleExceptionFromSubJob()
+    {
+        Event::fake();
+        $volume = VolumeTest::create([
+            'media_type_id' => MediaType::imageId(),
+            'creating_async' => true,
+        ]);
+        $user = User::find($volume->creator_id);
+        $filenames = ['a.jpg', 'b.jpg'];
+
+        $mock = Mockery::mock(CreateNewImagesOrVideos::class);
+        $job = new CreateNewFilesAndNotifyUserStub($volume, $filenames, $user);
+        $job->withFakeQueueInteractions();
+        $job->subJob = $mock;
+
+        $mock->shouldReceive('handle')->andThrow(new Exception());
+        $job->handle();
+        $job->assertFailed();
+        $this->assertFalse($volume->fresh()->creating_async);
+        Event::assertNotDispatched(VolumeImagesProcessed::class);
+        Event::assertDispatched(VolumeFilesProcessingFailed::class);
+    }
+}
+
+class CreateNewFilesAndNotifyUserStub extends CreateNewFilesAndNotifyUser
+{
+
+    public $subJob;
+
+    public function getJob()
+    {
+        return $this->subJob;
+    }
+}
