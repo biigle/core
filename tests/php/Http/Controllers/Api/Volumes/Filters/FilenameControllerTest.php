9448a4162a63de5a2c266638059ed7cadcc127da
yannik131 <yannik131@web.de>
2026-05-21 11:38:25 +0200

    Removed trailing whitespace, formatting

--- tests/php/Http/Controllers/Api/Volumes/Filters/FilenameControllerTest.php
+++ tests/php/Http/Controllers/Api/Volumes/Filters/FilenameControllerTest.php
@@ -74,18 +74,18 @@ class FilenameControllerTest extends ApiTestCase
             'volume_id' => $vid,
             'filename' => '%2F.jpg' // encodeURIComponent("/") yields "%2F"
         ]);
-        
+
         $expectedResults = [
             '*cde*%5C' => [],
             '%2F.jpg' => [$image2->id], // "/.jpg"
             '%2F*' => [$image2->id], // "/*"
             '%25252F.jpg' => [$image3->id], // "%2F.jpg"
         ];
-        
+
         $this->beGuest();
         foreach ($expectedResults as $pattern => $expectedIds) {
             $response = $this->json('GET', "/api/v1/volumes/{$vid}/files/filter/filename/{$pattern}")
-            ->assertExactJson($expectedIds);
+                ->assertExactJson($expectedIds);
             $response->assertStatus(200);
         }
     }
