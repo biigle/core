eb9666b69c60898e8a157a37b7e5424f96eed770
Martin Zurowietz <martin@zurowietz.de>
2026-09-21 15:18:17 +0200

    Remove transaction block
    Is unnecessary for a single statement.

--- app/Http/Controllers/Api/PendingVolumeController.php
+++ app/Http/Controllers/Api/PendingVolumeController.php
@@ -56,11 +56,11 @@ class PendingVolumeController extends Controller
     public function store(StorePendingVolume $request)
     {
         try {
-            $pv = DB::transaction(fn () => $request->project->pendingVolumes()->create([
+            $pv = $request->project->pendingVolumes()->create([
                 'media_type_id' => $request->input('media_type_id'),
                 'user_id' => $request->user()->id,
                 'metadata_parser' => $request->input('metadata_parser', null),
-            ]));
+            ]);
         } catch (UniqueConstraintViolationException) {
             throw ValidationException::withMessages([
                 'id' => 'Only a single pending volume can be created at a time for each project and user.',
